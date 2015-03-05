a81c06699e6c399cf1d939681146d693c4f14a0c
David Smith <dsmith@datasync.com>
2015-03-05 00:10:13 -0600

    Add comments for converter class.

--- NetTally/Quest.cs
+++ NetTally/Quest.cs
@@ -75,9 +75,16 @@ namespace NetTally
     }
 
 
-
+    /// <summary>
+    /// Converter class for data binding, to allow conversion between the name of a
+    /// quest and the quest object itself.
+    /// </summary>
     public class QuestConverter : IValueConverter
     {
+        /// <summary>
+        /// Convert from source (object) to target (string).
+        /// </summary>
+        /// <returns>Returns the name of the quest object for display.</returns>
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             var q = value as Quest;
@@ -87,6 +94,10 @@ namespace NetTally
             return string.Empty;
         }
 
+        /// <summary>
+        /// Convert from target (string) to source (object).
+        /// </summary>
+        /// <returns>Returns the object that corresponds to the provide quest name.</returns>
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             var qname = (string)value;
