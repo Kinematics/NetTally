f9b8d3b32779870ba7a4fb1ca18bc656eb358ebd
David Smith <dsmith@datasync.com>
2015-03-06 23:11:14 -0600

    Add documentation comments and tidy up.

--- NetTally/MainWindow.xaml.cs
+++ NetTally/MainWindow.xaml.cs
@@ -19,6 +19,10 @@ namespace NetTally
         Tally tally = new Tally();
         Quests quests = new Quests();
 
+        /// <summary>
+        /// Function that's run when the program first starts.
+        /// Set up the data context links with the local variables.
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
@@ -32,11 +36,21 @@ namespace NetTally
             LoadQuests();
         }
 
+        /// <summary>
+        /// When the program closes, save the current list of quests.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
             SaveQuests();
         }
 
+        /// <summary>
+        /// Start running the tally on the currently selected quest and post range.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private async void tallyButton_Click(object sender, RoutedEventArgs e)
         {
             CleanupEditQuestName();
@@ -62,6 +76,12 @@ namespace NetTally
             }
         }
 
+        /// <summary>
+        /// Clear the page cache so that subsequent tally requests load the pages from the network
+        /// rather than from the cache.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void clearTallyCacheButton_Click(object sender, RoutedEventArgs e)
         {
             CleanupEditQuestName();
@@ -84,13 +104,60 @@ namespace NetTally
             }
         }
 
+        /// <summary>
+        /// Event to cause the program to copy the current contents of the the tally
+        /// results (ie: what's shown in the main text window) to the clipboard.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void copyToClipboardButton_Click(object sender, RoutedEventArgs e)
         {
             CleanupEditQuestName();
             Clipboard.SetText(tally.TallyResults);
         }
 
+        /// <summary>
+        /// If either start post or end post text boxes get focus, select the entire
+        /// contents so that they're easy to replace.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void post_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox tb = (sender as TextBox);
+            if (tb != null)
+                tb.SelectAll();
+        }
+
+        /// <summary>
+        /// If either start post or end post text boxes are clicked on with the mouse,
+        /// and they don't already have focus, explicitly set their focus.  This will
+        /// in turn cause them to select the inner contents.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void post_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBox tb = (sender as TextBox);
+
+            if (tb != null)
+            {
+                if (!tb.IsKeyboardFocusWithin)
+                {
+                    e.Handled = true;
+                    tb.Focus();
+                }
+            }
+        }
+
+
         #region Adding and removing quests
+        /// <summary>
+        /// Event handler for adding a new quest.
+        /// Create a new quest, and make the edit textbox visible so that the user can rename it.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void addQuestButton_Click(object sender, RoutedEventArgs e)
         {
             quests.AddToQuestList(new Quest());
@@ -100,15 +167,9 @@ namespace NetTally
             editQuestName.SelectAll();
         }
 
-        private void removeQuestButton_Click(object sender, RoutedEventArgs e)
-        {
-            quests.RemoveCurrentQuest();
-            CleanupEditQuestName();
-        }
-
         /// <summary>
-        /// Hitting enter will complete the entry.
-        /// Hitting escape will cancel the entry.
+        /// When modifying the quest name, hitting enter will complete the entry,
+        /// and hitting escape will cancel (and delete) the entry.
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
@@ -125,6 +186,21 @@ namespace NetTally
             }
         }
 
+        /// <summary>
+        /// Remove the current quest from the quest list.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void removeQuestButton_Click(object sender, RoutedEventArgs e)
+        {
+            quests.RemoveCurrentQuest();
+            CleanupEditQuestName();
+        }
+
+        /// <summary>
+        /// Any user interaction that ends the use of the text box for editing the quest
+        /// name needs to ensure some cleanup occurs.
+        /// </summary>
         private void CleanupEditQuestName()
         {
             if (editQuestName.Visibility == Visibility.Visible)
@@ -134,12 +210,15 @@ namespace NetTally
                 var cqn = quests.CurrentQuest.Name;
                 quests.Update();
                 quests.SetCurrentQuestByName(cqn);
+                startPost.Focus();
             }
         }
         #endregion
 
-
         #region Serialization
+        /// <summary>
+        /// Load any saved quest information when starting up the program.
+        /// </summary>
         private void LoadQuests()
         {
             string filepath = Path.Combine(System.Windows.Forms.Application.CommonAppDataPath, questFile);
@@ -155,6 +234,9 @@ namespace NetTally
             }
         }
 
+        /// <summary>
+        /// Save any quest information when shutting the program down.
+        /// </summary>
         private void SaveQuests()
         {
             string filepath = Path.Combine(System.Windows.Forms.Application.CommonAppDataPath, questFile);
@@ -170,27 +252,5 @@ namespace NetTally
         }
 
         #endregion
-
-        private void post_GotFocus(object sender, RoutedEventArgs e)
-        {
-            TextBox tb = (sender as TextBox);
-            if (tb != null)
-                tb.SelectAll();
-        }
-
-        private void post_PreviewMouseDown(object sender, MouseButtonEventArgs e)
-        {
-            TextBox tb = (sender as TextBox);
-
-            if (tb != null)
-            {
-                if (!tb.IsKeyboardFocusWithin)
-                {
-                    e.Handled = true;
-                    tb.Focus();
-                }
-            }
-        }
-
     }
 }
