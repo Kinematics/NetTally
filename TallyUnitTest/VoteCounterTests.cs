3d5ebada50cf57de283f0dac67b927c7cc41deda
David Smith <dsmith@datasync.com>
2015-03-08 15:49:32 -0500

    Add test classes for refactored VoteCounter and WebPageProvider classes.

---
+++ TallyUnitTest/VoteCounterTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NetTally;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+
+namespace NetTally.Tests
+{
+    [TestClass()]
+    public class VoteCounterTests
+    {
+        [TestMethod()]
+        public void TallyVotesTest()
+        {
+            Assert.Fail();
+            var a = new VoteCounter();
+            var b = new PrivateObject(a);
+        }
+    }
+}
---
+++ TallyUnitTest/WebPageProviderTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NetTally;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetTally.Tests
+{
+    [TestClass()]
+    public class WebPageProviderTests
+    {
+        [TestMethod()]
+        public void ClearPageCacheTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void LoadPagesTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
