ecc1c2fe2db53ea0123ed2370cdaa3bbe55d6173
David Smith <dsmith@datasync.com>
2015-03-08 16:06:36 -0500

    Create a private object for testing private methods in VoteCounter class.

--- TallyUnitTest/VoteCounterTests.cs
+++ TallyUnitTest/VoteCounterTests.cs
@@ -12,12 +12,29 @@ namespace NetTally.Tests
     [TestClass()]
     public class VoteCounterTests
     {
+        static VoteCounter voteCounter;
+        static PrivateObject privateVote;
+
+        [ClassInitialize()]
+        public static void ClassInit(TestContext context)
+        {
+            voteCounter = new VoteCounter();
+            privateVote = new PrivateObject(voteCounter);
+        }
+
         [TestMethod()]
         public void TallyVotesTest()
         {
-            Assert.Fail();
-            var a = new VoteCounter();
-            var b = new PrivateObject(a);
+        }
+
+        [TestMethod()]
+        public void CleanVoteTest()
+        {
+            string input = "[X] We [i]did[/i] agree to non-lethal. My most [color=blue]powerful[/color] stuff either knocks people out or kills them without having to fight at all. Everything else I've learned to do so far feels like a witch barrier, and I try not to use that since it freaks everyone out.";
+            string expected = "[x]wedidagreetonon-lethalmymostpowerfulstuffeitherknockspeopleoutorkillsthemwithouthavingtofightatalleverythingelsei'velearnedtodosofarfeelslikeawitchbarrier,anditrynottousethatsinceitfreakseveryoneout";
+
+            var results = privateVote.Invoke("CleanVote", input);
+            Assert.AreEqual(expected, results);
         }
     }
 }
