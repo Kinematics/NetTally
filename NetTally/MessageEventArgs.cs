af1eb69782eaf5c8b6a9dd9e01849baba6ff7a1f
David Smith <dsmith@datasync.com>
2015-03-08 12:59:09 -0500

    Add a custom eventargs class to pass a message string.

---
+++ NetTally/MessageEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace NetTally
+{
+    /// <summary>
+    /// EventArgs custom class to pass a message string.
+    /// </summary>
+    public class MessageEventArgs : EventArgs
+    {
+        public string Message { get; }
+
+        public MessageEventArgs(string message)
+        {
+            Message = message;
+        }
+    }
+}
