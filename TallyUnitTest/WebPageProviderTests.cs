4a52b50be2e2f2470ebd0ef7b96a9a6178ff5023
David Smith <dsmith@datasync.com>
2015-03-08 20:25:57 -0500

    Add simple PrivateObject for this test class as well.

--- TallyUnitTest/WebPageProviderTests.cs
+++ TallyUnitTest/WebPageProviderTests.cs
@@ -11,10 +11,27 @@ namespace NetTally.Tests
     [TestClass()]
     public class WebPageProviderTests
     {
+        static WebPageProvider pageProvider;
+        static PrivateObject privateWeb;
+
+        [ClassInitialize()]
+        public static void ClassInit(TestContext context)
+        {
+            pageProvider = new WebPageProvider();
+            privateWeb = new PrivateObject(pageProvider);
+        }
+
+
         [TestMethod()]
         public void ClearPageCacheTest()
         {
-            Assert.Fail();
+            pageProvider.ClearPageCache();
+
+            Dictionary<string, CachedPage> accessPageCache = (Dictionary<string, CachedPage>)privateWeb.GetField("pageCache");
+            Assert.IsTrue(accessPageCache.Count == 0);
+
+            Dictionary<string, int> accessLoadedPages = (Dictionary<string, int>)privateWeb.GetField("lastPageLoaded");
+            Assert.IsTrue(accessLoadedPages.Count == 0);
         }
 
         [TestMethod()]
