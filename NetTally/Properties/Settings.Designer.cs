e7ccac18a1b972545359e74225307d53126b8cef
David Smith <dsmith@datasync.com>
2015-03-12 20:47:33 -0500

    Remove settings from userSettings that are now handled via ConfigurationManager.

--- NetTally/Properties/Settings.Designer.cs
+++ NetTally/Properties/Settings.Designer.cs
@@ -26,36 +26,12 @@ namespace NetTally.Properties {
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("False")]
-        public bool CheckForLastThreadmark {
+        public bool Upgraded {
             get {
-                return ((bool)(this["CheckForLastThreadmark"]));
+                return ((bool)(this["Upgraded"]));
             }
             set {
-                this["CheckForLastThreadmark"] = value;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
-        public bool UseVotePartitions {
-            get {
-                return ((bool)(this["UseVotePartitions"]));
-            }
-            set {
-                this["UseVotePartitions"] = value;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("True")]
-        public bool PartitionByLine {
-            get {
-                return ((bool)(this["PartitionByLine"]));
-            }
-            set {
-                this["PartitionByLine"] = value;
+                this["Upgraded"] = value;
             }
         }
     }
