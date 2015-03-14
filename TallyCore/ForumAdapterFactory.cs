098e1a63d49cb028021301a0b6578d265e7acdc1
Kinematics <dsmith@datasync.com>
2015-03-14 14:42:17 -0500

    Add comments.

--- TallyCore/ForumAdapterFactory.cs
+++ TallyCore/ForumAdapterFactory.cs
@@ -8,6 +8,11 @@ namespace NetTally
 {
     public static class ForumAdapterFactory
     {
+        /// <summary>
+        /// Get a forum adapter for the site a quest is on.
+        /// </summary>
+        /// <param name="quest">The quest that a forum adapter is needed for.</param>
+        /// <returns>Returns a forum adapter for the site the quest is on.</returns>
         public static IForumAdapter GetAdapter(IQuest quest)
         {
             IForumAdapter adapter = GetExplicitAdapter(quest);
@@ -18,13 +23,18 @@ namespace NetTally
             return adapter;
         }
 
+        /// <summary>
+        /// Get adapter for site that we have explicitly built adapters for.
+        /// An empty string gets the default forum adapter.
+        /// </summary>
+        /// <param name="quest">The quest that a forum adapter is needed for.</param>
+        /// <returns>Returns a forum adapter for the site the quest is on.</returns>
         private static IForumAdapter GetExplicitAdapter(IQuest quest)
         {
             switch (quest.Site)
             {
                 case "http://forums.sufficientvelocity.com/":
                 case "":
-                case null:
                     // Sufficient Velocity is the default if no site name is given
                     return new SVForumAdapter();
                 default:
@@ -32,6 +42,12 @@ namespace NetTally
             }
         }
 
+        /// <summary>
+        /// Gets adapter for a site we can only implicitly guess the structure of.
+        /// </summary>
+        /// <param name="quest">The quest that a forum adapter is needed for.</param>
+        /// <returns>Returns a forum adapter for the site the quest is on.
+        /// Throws an exception if it is unable to identify a site's forums.</returns>
         private static IForumAdapter GetImplicitAdapter(IQuest quest)
         {
             throw new NotImplementedException("There is no forum adapter implemented for " + quest.Site);
