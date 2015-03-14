7acd67d6f345ea8904fd83f050d18dae037cafa6
Kinematics <dsmith@datasync.com>
2015-03-14 15:10:39 -0500

    Add a unit test for the factory adapter.

---
+++ TallyUnitTest/ForumAdapterFactoryUnitTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NetTally;
+
+namespace NetTally.Tests
+{
+    [TestClass]
+    public class ForumAdapterFactoryUnitTest
+    {
+        [TestMethod]
+        public void TestDefault()
+        {
+            IQuest quest = new Quest();
+            var adapter = ForumAdapterFactory.GetAdapter(quest);
+
+            Assert.IsInstanceOfType(adapter, typeof(SVForumAdapter));
+        }
+
+        [TestMethod]
+        public void TestSufficientVelocity()
+        {
+            IQuest quest = new Quest();
+            quest.Site = "http://forums.sufficientvelocity.com/";
+            var adapter = ForumAdapterFactory.GetAdapter(quest);
+
+            Assert.IsInstanceOfType(adapter, typeof(SVForumAdapter));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NotImplementedException))]
+        public void TestSpaceBattles()
+        {
+            IQuest quest = new Quest();
+            quest.Site = "http://forums.spacebattles.com/";
+            var adapter = ForumAdapterFactory.GetAdapter(quest);
+
+            //Assert.IsInstanceOfType(adapter, typeof(SBForumAdapter));
+        }
+    }
+}
