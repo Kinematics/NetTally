3f9ed6708084d0d62b8d2f964f2663837ea96a81
Kinematics <dsmith@datasync.com>
2015-03-14 13:38:36 -0500

    Add a forum adapter factory to generate the appropriate forum adapter for each quest.

---
+++ TallyCore/ForumAdapterFactory.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetTally
+{
+    public static class ForumAdapterFactory
+    {
+        public static IForumAdapter GetAdapter(IQuest quest)
+        {
+            IForumAdapter adapter = GetExplicitAdapter(quest);
+
+            if (adapter == null)
+                adapter = GetImplicitAdapter(quest);
+
+            return adapter;
+        }
+
+        private static IForumAdapter GetExplicitAdapter(IQuest quest)
+        {
+            switch (quest.Site)
+            {
+                case "http://forums.sufficientvelocity.com/":
+                case "":
+                case null:
+                    // Sufficient Velocity is the default if no site name is given
+                    return new SVForumAdapter();
+                default:
+                    return null;
+            }
+        }
+
+        private static IForumAdapter GetImplicitAdapter(IQuest quest)
+        {
+            throw new NotImplementedException("There is no forum adapter implemented for " + quest.Site);
+        }
+    }
+}
