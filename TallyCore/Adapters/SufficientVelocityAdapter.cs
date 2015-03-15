eb3b96233ce68dc5fa63827cf4abdc4ec7dc6365
Kinematics <dsmith@datasync.com>
2015-03-15 05:00:21 -0500

    Rename class for clarity.

--- TallyCore/Adapters/SufficientVelocityAdapter.cs
+++ TallyCore/Adapters/SufficientVelocityAdapter.cs
@@ -12,7 +12,7 @@ namespace NetTally.Adapters
     /// <summary>
     /// Class for extracting data from Sufficient Velocity forums.
     /// </summary>
-    public class SVForumAdapter : XenForoAdapter
+    public class SufficientVelocityAdapter : XenForoAdapter
     {
         override protected string ForumUrl { get; } = "http://forums.sufficientvelocity.com/";
         override protected string ThreadsUrl { get; } = "http://forums.sufficientvelocity.com/threads/";
