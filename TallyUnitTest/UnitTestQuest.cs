7b3a7fcd0ea9dd20aae7329e559f02885d3e68c2
Kinematics <dsmith@datasync.com>
2015-03-16 21:50:14 -0500

    Update unit test for Quest class.

--- TallyUnitTest/UnitTestQuest.cs
+++ TallyUnitTest/UnitTestQuest.cs
@@ -2,13 +2,17 @@
 using System.ComponentModel;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NetTally;
+using NetTally.Adapters;
 
 namespace TallyUnitTest
 {
     [TestClass]
     public class UnitTestQuest
     {
-        string propertyNameRaised = string.Empty;
+// Disable obsolete warnings for fields that we're still testing
+#pragma warning disable 0618
+
+        static string propertyNameRaised = string.Empty;
 
         #region Constructor
         [TestMethod]
@@ -16,10 +20,104 @@ namespace TallyUnitTest
         {
             var a = new Quest();
 
-            Assert.AreEqual("New Entry", a.Name);
+            // Obsolete
+            Assert.AreEqual(Quest.NewEntryName, a.Name);
+            Assert.AreEqual("", a.Site);
+
+            // Normal
+            Assert.AreEqual(Quest.NewThreadEntry, a.ThreadName);
+            Assert.AreEqual("fake-thread", a.DisplayName);
+            Assert.AreEqual("http://forums.sufficientvelocity.com/", a.SiteName);
             Assert.AreEqual(1, a.StartPost);
             Assert.AreEqual(0, a.EndPost);
+            Assert.AreEqual(true, a.ReadToEndOfThread);
+            Assert.AreEqual(false, a.UseVotePartitions);
+            Assert.AreEqual(true, a.PartitionByLine);
+            Assert.AreEqual(false, a.CheckForLastThreadmark);
         }
+
+        [TestMethod]
+        public void TestToString()
+        {
+            var a = new Quest();
+
+            Assert.AreEqual(Quest.NewThreadEntry, a.ToString());
+            a.DisplayName = "Test Display";
+            Assert.AreEqual("Test Display", a.ToString());
+        }
+        #endregion
+
+        #region Names
+        [TestMethod]
+        public void TestDisplayName()
+        {
+            var a = new Quest();
+
+            Assert.AreEqual("fake-thread", a.DisplayName);
+            a.DisplayName = "testing-thread";
+            Assert.AreEqual("testing-thread", a.DisplayName);
+            a.DisplayName = "";
+            Assert.AreEqual("fake-thread", a.DisplayName);
+            a.ThreadName = "http://forums.sufficientvelocity.com/";
+            Assert.AreEqual("forums.sufficientvelocity.com", a.DisplayName);
+            a.DisplayName = "/";
+            Assert.AreEqual("/", a.DisplayName);
+        }
+
+        [TestMethod]
+        public void TestThreadName()
+        {
+            var a = new Quest();
+
+            a.ThreadName = "http://forums.sufficientvelocity.com/";
+            Assert.AreEqual("http://forums.sufficientvelocity.com/", a.ThreadName);
+            a.ThreadName = "http://forums.sufficientvelocity.com/threads/renascence-a-homura-quest.10402/";
+            Assert.AreEqual("http://forums.sufficientvelocity.com/threads/renascence-a-homura-quest.10402/", a.ThreadName);
+            a.ThreadName = "http://forums.sufficientvelocity.com/threads/renascence-a-homura-quest.10402/page-221";
+            Assert.AreEqual("http://forums.sufficientvelocity.com/threads/renascence-a-homura-quest.10402/", a.ThreadName);
+            a.ThreadName = "http://www.fandompost.com/oldforums/showthread.php?39239-Yurikuma-Arashi-Discussion-Thread&p=288335#post288335";
+            Assert.AreEqual("http://www.fandompost.com/oldforums/showthread.php?39239-Yurikuma-Arashi-Discussion-Thread", a.ThreadName);
+        }
+
+        [TestMethod]
+        public void TestSiteName()
+        {
+            var a = new Quest();
+
+            Assert.AreEqual("http://forums.sufficientvelocity.com/", a.SiteName);
+            a.ThreadName = "http://www.fandompost.com/oldforums/showthread.php?39239-Yurikuma-Arashi-Discussion-Thread&p=288335#poast288335";
+            Assert.AreEqual("http://www.fandompost.com/", a.SiteName);
+            a.ThreadName = "renascence-a-homura-quest.10402";
+            Assert.AreEqual("http://forums.sufficientvelocity.com/", a.SiteName);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TestNullThreadName()
+        {
+            var a = new Quest();
+
+            a.ThreadName = null;
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestEmptyThreadName()
+        {
+            var a = new Quest();
+
+            a.ThreadName = "";
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TestNullDisplayName()
+        {
+            var a = new Quest();
+
+            a.DisplayName = null;
+        }
+
         #endregion
 
         #region Name
@@ -30,15 +128,6 @@ namespace TallyUnitTest
             var a = new Quest();
 
             a.Name = null;
-        }
-
-        [TestMethod]
-        [ExpectedException(typeof(ArgumentOutOfRangeException))]
-        public void TestSetNameToEmpty()
-        {
-            var a = new Quest();
-
-            a.Name = string.Empty;
         }
 
         [TestMethod]
@@ -59,25 +148,25 @@ namespace TallyUnitTest
             var a = new Quest();
 
             string testName = "awake-already-homura-nge-pmmm-fusion-quest.11111";
-            string expectedName = "awake-already-homura-nge-pmmm-fusion-quest.11111";
+            //string expectedName = "awake-already-homura-nge-pmmm-fusion-quest.11111";
 
             a.Name = testName;
-            Assert.AreEqual(expectedName, a.Name);
+            Assert.AreEqual(testName, a.Name);
 
             testName = "awake-already-homura-nge-pmmm-fusion-quest.11111/page-34#post-2943518";
 
             a.Name = testName;
-            Assert.AreEqual(expectedName, a.Name);
+            Assert.AreEqual(testName, a.Name);
 
             testName = "http://forums.sufficientvelocity.com/threads/awake-already-homura-nge-pmmm-fusion-quest.11111";
 
             a.Name = testName;
-            Assert.AreEqual(expectedName, a.Name);
+            Assert.AreEqual(testName, a.Name);
 
             testName = "http://forums.sufficientvelocity.com/threads/awake-already-homura-nge-pmmm-fusion-quest.11111/page-34#post-2943518";
 
             a.Name = testName;
-            Assert.AreEqual(expectedName, a.Name);
+            Assert.AreEqual(testName, a.Name);
 
             // This will fail the regex filter, so should just give back the original.
             testName = "/awake-already-homura-nge-pmmm-fusion-quest.11111/page-34#post-2943518";
@@ -121,6 +210,7 @@ namespace TallyUnitTest
         #endregion
 
         #region StartPost
+
         [TestMethod]
         public void TestSetStart()
         {
@@ -193,7 +283,7 @@ namespace TallyUnitTest
         }
         #endregion
 
-        #region End of thread
+        #region Flags
         [TestMethod]
         public void TestReadToEndOfThread()
         {
@@ -207,10 +297,59 @@ namespace TallyUnitTest
 
             a.EndPost = 0;
             Assert.AreEqual(true, a.ReadToEndOfThread);
+
+            a.EndPost = Int32.MaxValue;
+            Assert.AreEqual(false, a.ReadToEndOfThread);
         }
 
+        [TestMethod]
+        public void TestUseVotePartitions()
+        {
+            var a = new Quest();
+
+            a.UseVotePartitions = true;
+            Assert.AreEqual(true, a.UseVotePartitions);
+        }
+
+        [TestMethod]
+        public void TestPartitionByLine()
+        {
+            var a = new Quest();
+
+            a.PartitionByLine = false;
+            Assert.AreEqual(false, a.PartitionByLine);
+        }
+
+        [TestMethod]
+        public void TestCheckForLastThreadmark()
+        {
+            var a = new Quest();
+
+            a.CheckForLastThreadmark = true;
+            Assert.AreEqual(true, a.CheckForLastThreadmark);
+        }
         #endregion
 
+        #region ForumAdapters
+        [TestMethod]
+        public void TestForumAdapters()
+        {
+            Quest a = new Quest();
+
+            var adapter = a.GetForumAdapter();
+            Assert.IsInstanceOfType(adapter, typeof(SufficientVelocityAdapter));
+
+            a.ThreadName = "http://forums.spacebattles.com/";
+            var adapterTask = a.GetForumAdapter(System.Threading.CancellationToken.None);
+            adapter = adapterTask.Result;
+            Assert.IsInstanceOfType(adapter, typeof(SpaceBattlesAdapter));
+
+            a.Site = "http://forums.spacebattles.com/";
+            adapterTask = a.GetForumAdapter(System.Threading.CancellationToken.None);
+            adapter = adapterTask.Result;
+            Assert.IsInstanceOfType(adapter, typeof(SpaceBattlesAdapter));
+        }
+        #endregion
 
         #region Events
         [TestMethod]
@@ -233,6 +372,21 @@ namespace TallyUnitTest
 
             a.EndPost = 20;
             Assert.AreEqual("EndPost", propertyNameRaised);
+
+            a.DisplayName = "Display";
+            Assert.AreEqual("DisplayName", propertyNameRaised);
+
+            a.ThreadName = "http://www.example.com";
+            Assert.AreEqual("ThreadName", propertyNameRaised);
+
+            a.UseVotePartitions = true;
+            Assert.AreEqual("UseVotePartitions", propertyNameRaised);
+
+            a.PartitionByLine = false;
+            Assert.AreEqual("PartitionByLine", propertyNameRaised);
+
+            a.CheckForLastThreadmark = true;
+            Assert.AreEqual("CheckForLastThreadmark", propertyNameRaised);
         }
 
         private void A_PropertyChanged(object sender, PropertyChangedEventArgs e)
@@ -240,5 +394,7 @@ namespace TallyUnitTest
             propertyNameRaised = e.PropertyName;
         }
         #endregion
+
+#pragma warning restore 0618
     }
 }
