ce996b72cb22c71b0307885713a79ee95a7a5951
David Smith <dsmith@datasync.com>
2015-05-27 14:05:31 -0500

    Implement basic attempt at coding the merge window.

--- NetTally/MergeVotesWindow.xaml.cs
+++ NetTally/MergeVotesWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,6 +21,14 @@ namespace NetTally
     /// </summary>
     public partial class MergeVotesWindow : Window
     {
+        IVoteCounter voteCounter;
+
+        public ICollectionView VoteCollectionView1 { get; }
+        public ICollectionView VoteCollectionView2 { get; }
+        public ObservableCollection<string> ObservableVotes { get; }
+
+        //public ObservableCollection<Dictionary<string, HashSet<string>>> ObservableVotes { get; }
+
         public MergeVotesWindow()
         {
             InitializeComponent();
@@ -27,6 +37,87 @@ namespace NetTally
         public MergeVotesWindow(Tally tally)
         {
             InitializeComponent();
+
+            voteCounter = tally.VoteCounter;
+
+            ObservableVotes = new ObservableCollection<string>(voteCounter.VotesWithSupporters.Keys);
+            //ObservableVotes = new ObservableCollection<Dictionary<string, HashSet<string>>>(voteCounter.VotesWithSupporters);
+
+            VoteCollectionView1 = new CollectionView(ObservableVotes);
+            VoteCollectionView2 = new CollectionView(ObservableVotes);
+
+
+            this.DataContext = this;
+        }
+
+        public bool VotesCanMerge
+        {
+            get
+            {
+                if (votesFromListBox.SelectedIndex < 0)
+                    return false;
+                if (votesToListBox.SelectedIndex < 0)
+                    return false;
+
+                return (votesFromListBox.SelectedItem.ToString() != votesToListBox.SelectedItem.ToString());
+            }
+        }
+
+        private void merge_Click(object sender, RoutedEventArgs e)
+        {
+            if (votesFromListBox.SelectedIndex < 0)
+                return;
+            if (votesToListBox.SelectedIndex < 0)
+                return;
+
+            string fromVote = votesFromListBox.SelectedItem.ToString();
+            string toVote = votesToListBox.SelectedItem.ToString();
+
+            try
+            {
+                if (voteCounter.Merge(fromVote, toVote))
+                {
+                    ObservableVotes.Remove(fromVote);
+                    //ShowVoters(votesToListBox);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void listBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            merge.IsEnabled = VotesCanMerge;
+            //ShowVoters(sender as ListBox);
+
+        }
+
+        private void ShowVoters(ListBox listBox)
+        {
+            if (listBox == null)
+                return;
+
+            ListBox voterListBox = votersFromListBox;
+            if (listBox == votesToListBox)
+                voterListBox = votersToListBox;
+
+
+            //voterListBox.Items.Clear();
+
+            if (listBox.SelectedIndex < 0)
+            {
+                return;
+            }
+
+            string vote = listBox.SelectedItem.ToString();
+            HashSet<string> voters;
+            if (voteCounter.VotesWithSupporters.TryGetValue(vote, out voters))
+            {
+                voterListBox.ItemsSource = voters;
+            }
+
         }
     }
 }
