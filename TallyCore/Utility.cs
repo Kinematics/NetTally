5f524eb44a5f0caca81c58c93c8ff37d7b43511f
David Smith <dsmith@datasync.com>
2015-06-01 17:55:02 -0500

    Add a utility class for static regexes and functions and such.

---
+++ TallyCore/Utility.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace NetTally
+{
+    public static class Utility
+    {
+        // Regex for control and formatting characters that we don't want to allow processing of
+        public static Regex UnsafeCharsRegex { get; } = new Regex(@"\p{Cc}|\p{Cf}");
+
+        public static string SafeString(string input)
+        {
+            return UnsafeCharsRegex.Replace(input, "");
+        }
+    }
+}
