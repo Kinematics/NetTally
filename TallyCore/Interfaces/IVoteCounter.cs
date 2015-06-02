6bdfd019516a784d0e095a0c2dcf99478234fe0c
David Smith <dsmith@datasync.com>
2015-06-02 02:01:47 -0500

    Update interface with sets for ranked votes, separate from regular votes.

--- TallyCore/Interfaces/IVoteCounter.cs
+++ TallyCore/Interfaces/IVoteCounter.cs
@@ -6,9 +6,16 @@ namespace NetTally
     public interface IVoteCounter
     {
         string Title { get; set; }
+
+        HashSet<string> PlanNames { get; }
+
         Dictionary<string, HashSet<string>> VotesWithSupporters { get; }
         Dictionary<string, string> VoterMessageId { get; }
-        HashSet<string> PlanNames { get; }
+
+        Dictionary<string, HashSet<string>> RankedVotesWithSupporters { get; }
+        Dictionary<string, string> RankedVoterMessageId { get; }
+
+        bool HasRankedVotes { get; }
 
         void TallyVotes(IQuest quest, List<HtmlDocument> pages);
         bool Merge(string fromVote, string toVote);
