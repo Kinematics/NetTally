7bee7681896f0504d7ae8ab4f0b7ec433ebc0cf7
David Smith <dsmith@datasync.com>
2015-06-02 13:03:25 -0500

    Create a static class for basic manipulation and data extraction of vote lines.

--- TallyCore/Utility.cs
+++ TallyCore/Utility.cs
@@ -22,4 +22,97 @@ namespace NetTally
             return UnsafeCharsRegex.Replace(input, "");
         }
     }
+
+    public static class VoteLine
+    {
+        // Regex to get the different parts of the vote.
+        static readonly Regex voteLineRegex = new Regex(@"^-*\[(?<marker>[xX+✓✔1-9])\]\s*(\[\s*(?<task>(\w|\d)(\s*(\w|\d)+)*\??)\s*\])?(?<content>.*)");
+        // Regex to match any markup that we'll want to remove during comparisons.
+        static readonly Regex markupRegex = new Regex(@"\[/?[ibu]\]|\[color[^]]*\]|\[/color\]");
+        // Regex to allow us to collapse a vote to a commonly comparable version.
+        static readonly Regex collapseRegex = new Regex(@"\s|\.");
+        // Regex to allow us to strip leading dashes from a per-line vote.
+        static readonly Regex leadHyphenRegex = new Regex(@"^-+");
+
+        /// <summary>
+        /// Given a vote line, remove any BBCode formatting chunks, and trim the ends.
+        /// </summary>
+        /// <param name="voteLine">The vote line to examine.</param>
+        /// <returns>Returns the vote line without any BBCode formatting.</returns>
+        public static string CleanVote(string voteLine)
+        {
+            return markupRegex.Replace(voteLine, "").Trim();
+        }
+
+        /// <summary>
+        /// Collapse a vote to a minimized form, for comparison.
+        /// All BBCode markup is removed, along with all spaces and periods,
+        /// and leading dashes when partitioning by line.  The text is then
+        /// lowercased.
+        /// </summary>
+        /// <param name="voteLine">Original vote line to minimize.</param>
+        /// <param name="quest">The quest being tallied.</param>
+        /// <returns>Returns a minimized version of the vote string.</returns>
+        public static string MinimizeVote(string voteLine, IQuest quest)
+        {
+            string cleaned = CleanVote(voteLine);
+            cleaned = collapseRegex.Replace(cleaned, "");
+            cleaned = cleaned.ToLower();
+            if (quest.UseVotePartitions && quest.PartitionByLine)
+                cleaned = leadHyphenRegex.Replace(cleaned, "");
+
+            return cleaned;
+        }
+
+        /// <summary>
+        /// Get the marker of the vote line.
+        /// </summary>
+        /// <param name="voteLine">The vote line being examined.</param>
+        /// <returns>Returns the marker of the vote line.</returns>
+        public static string GetVoteMarker(string voteLine)
+        {
+            string cleaned = CleanVote(voteLine);
+            Match m = voteLineRegex.Match(cleaned);
+            if (m.Success)
+            {
+                return m.Groups["marker"].Value;
+            }
+
+            return string.Empty;
+        }
+
+        /// <summary>
+        /// Get the (optional) task name from the provided vote line.
+        /// </summary>
+        /// <param name="voteLine">The vote line being examined.</param>
+        /// <returns>Returns the name of the task, if found, or an empty string.</returns>
+        public static string GetVoteTask(string voteLine)
+        {
+            string cleaned = CleanVote(voteLine);
+            Match m = voteLineRegex.Match(cleaned);
+            if (m.Success)
+            {
+                return m.Groups["task"].Value;
+            }
+
+            return string.Empty;
+        }
+
+        /// <summary>
+        /// Get the content of the vote line.
+        /// </summary>
+        /// <param name="voteLine">The vote line being examined.</param>
+        /// <returns>Returns the content of the vote line.</returns>
+        public static string GetVoteContent(string voteLine)
+        {
+            string cleaned = CleanVote(voteLine);
+            Match m = voteLineRegex.Match(cleaned);
+            if (m.Success)
+            {
+                return m.Groups["content"].Value;
+            }
+
+            return string.Empty;
+        }
+    }
 }
