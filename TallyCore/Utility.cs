d6d3bea0a04ef82b88843b35db66c7adcedb5cfb
David Smith <dsmith@datasync.com>
2015-06-01 22:11:10 -0500

    Exclude all control character groups, instead of just formatting and control.

--- TallyCore/Utility.cs
+++ TallyCore/Utility.cs
@@ -12,7 +12,7 @@ namespace NetTally
         // Regex for control and formatting characters that we don't want to allow processing of.
         // EG: \u200B, non-breaking space
         // Do not remove CR/LF characters
-        public static Regex UnsafeCharsRegex { get; } = new Regex(@"[\p{Cf}\p{Cc}-[\r\n]]");
+        public static Regex UnsafeCharsRegex { get; } = new Regex(@"[\p{C}-[\r\n]]");
 
         public static string SafeString(string input)
         {
