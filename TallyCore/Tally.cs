92064261edf9c398455de036c4dd720f992157f0
David Smith <dsmith@datasync.com>
2015-06-02 22:33:32 -0500

    Remove unused using.

--- TallyCore/Tally.cs
+++ TallyCore/Tally.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Text;
-using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using HtmlAgilityPack;
