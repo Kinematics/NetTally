b57b1e1a9c1bdd5406fb14b25b2ce4d8f8b1fcfa
David Smith <dsmith@datasync.com>
2015-06-03 19:05:30 -0500

    Add a converter for converting between a dropdown's selected index and a DisplayMode enum.

--- NetTally/BindingConverters.cs
+++ NetTally/BindingConverters.cs
@@ -5,7 +5,6 @@ using System.Windows.Data;
 
 namespace NetTally
 {
-
     /// <summary>
     /// Data binding conversion class to convert the bool to either the same value,
     /// or the inverse of the value, depending on the parameter value.
@@ -160,4 +159,43 @@ namespace NetTally
         }
     }
 
+
+    /// <summary>
+    /// Data binding conversion class to convert the bool to either the same value,
+    /// or the inverse of the value, depending on the parameter value.
+    /// The 'parameter' parameter specifies whether to invert the boolean value.
+    /// Value of "Invert" will cause it to return the negation of the bool.
+    /// </summary>
+    [ValueConversion(typeof(DisplayMode), typeof(int))]
+    public class DisplayModeConverter : IValueConverter
+    {
+        /// <summary>
+        /// Convert from source (property bool) to target (control bool).
+        /// </summary>
+        /// <returns>Returns whether the specified target control value should be on or off.</returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is DisplayMode)
+            {
+                return (int)value;
+            }
+
+            return -1;
+        }
+
+        /// <summary>
+        /// Convert from target (control bool) to source (property bool).
+        /// </summary>
+        /// <returns>Returns what the source property value should be set to
+        /// based on the target value.</returns>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int)
+            {
+                return (DisplayMode)value;
+            }
+
+            return DisplayMode.Normal;
+        }
+    }
 }
