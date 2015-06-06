d1adbda997f371d266205d990423ec5372b24963
David Smith <dsmith@datasync.com>
2015-06-05 22:32:54 -0500

    Add a debug mode class that checks for a file containing the word "debug" in its filename in the program's directory. Allows the user to activate debugmode without adding an option to the UI.

---
+++ TallyCore/DebugMode.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace NetTally
+{
+    public sealed class DebugMode
+    {
+        // Prevent outside instantiation
+        private DebugMode()
+        {
+            Active = CheckForDebugFile();
+        }
+
+        static DebugMode()
+        {
+        }
+
+        private static readonly DebugMode _singleton = new DebugMode();
+
+        public static DebugMode Instance { get { return _singleton; } }
+
+        public bool Active { get; }
+
+        private bool CheckForDebugFile()
+        {
+            Regex r = new Regex(@"\bdebug\b");
+            var files = Directory.EnumerateFiles(".");
+
+            return files.Any(f => r.Match(f).Success);
+        }
+
+    }
+}
