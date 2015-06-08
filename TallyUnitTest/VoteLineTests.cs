8709ad45b0e1fc8cb6c36dd60d9152b4c66ab087
David Smith <dsmith@datasync.com>
2015-06-08 17:10:59 -0500

    Move tests over to VoteLine.

--- TallyUnitTest/VoteLineTests.cs
+++ TallyUnitTest/VoteLineTests.cs
@@ -44,5 +44,33 @@ namespace TallyUnitTest
             Assert.AreEqual(expected, results);
         }
 
+        [TestMethod()]
+        public void StripFormattingTest()
+        {
+            string line1 = "[x] Vote for stuff";
+            string line2 = "-[x] Vote for stuff";
+
+            string line3 = "[b][x] Vote for stuff[/b]";
+            string line4 = "[color=blue][x] Vote for stuff[/color]";
+            string line5 = "[b][x] Vote for stuff";
+            string line6 = "[color=blue][b][x] Vote for stuff[/b]";
+            string line7 = "[b]-[x] Vote for stuff";
+            string line8 = "[color=blue]-[x] Vote for stuff[/color]";
+
+            string e = "";
+
+            Assert.AreEqual(line1, VoteLine.CleanVote(line1));
+            Assert.AreEqual(line1, VoteLine.CleanVote(line3));
+            Assert.AreEqual(line1, VoteLine.CleanVote(line4));
+            Assert.AreEqual(line1, VoteLine.CleanVote(line5));
+            Assert.AreEqual(line1, VoteLine.CleanVote(line6));
+
+            Assert.AreEqual(line2, VoteLine.CleanVote(line2));
+            Assert.AreEqual(line2, VoteLine.CleanVote(line7));
+            Assert.AreEqual(line2, VoteLine.CleanVote(line8));
+
+            Assert.AreEqual(e, VoteLine.CleanVote(e));
+        }
+
     }
 }
