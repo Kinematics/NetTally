11c0d20c0baa7d13ed0b9acf83d1e95f7dc7d348
David Smith <dsmith@datasync.com>
2015-06-15 20:37:35 -0500

    Rearrange interface entries for clarity.

--- TallyCore/Interfaces/IVoteCounter.cs
+++ TallyCore/Interfaces/IVoteCounter.cs
@@ -5,34 +5,29 @@ namespace NetTally
 {
     public interface IVoteCounter
     {
-        string Title { get; set; }
-
-        HashSet<string> PlanNames { get; }
-
-        Dictionary<string, HashSet<string>> VotesWithSupporters { get; }
-        Dictionary<string, string> VoterMessageId { get; }
-
-        Dictionary<string, HashSet<string>> RankedVotesWithSupporters { get; }
-        Dictionary<string, string> RankedVoterMessageId { get; }
-
-        bool HasRankedVotes { get; }
-
         void TallyVotes(IQuest quest, List<HtmlDocument> pages);
+        void Reset();
+        void AddVoteSupport(string vote, string voter, VoteType voteType, IQuest quest);
+        void RemoveSupport(string voter, VoteType voteType);
+        void AddVoterPostID(string voter, string postID, VoteType voteType);
         bool Merge(string fromVote, string toVote, VoteType voteType);
         bool Join(List<string> voters, string voterToJoin, VoteType voteType);
-        bool Delete(string vote, VoteType voteType);
         bool Rename(string oldVote, string newVote, VoteType voteType);
+        bool Delete(string vote, VoteType voteType);
 
         Dictionary<string, HashSet<string>> GetVotesCollection(VoteType voteType);
         Dictionary<string, string> GetVotersCollection(VoteType voteType);
 
-        void Reset();
-
-        void RemoveSupport(string voter, VoteType voteType);
-
-        void AddVoterPostID(string voter, string postID, VoteType voteType);
-        void AddVoteSupport(string vote, string voter, VoteType voteType, IQuest quest);
-
         List<string> GetVotesFromReference(string voteLine);
+
+        string Title { get; set; }
+        bool HasRankedVotes { get; }
+
+        Dictionary<string, HashSet<string>> VotesWithSupporters { get; }
+        Dictionary<string, HashSet<string>> RankedVotesWithSupporters { get; }
+        Dictionary<string, string> VoterMessageId { get; }
+        Dictionary<string, string> RankedVoterMessageId { get; }
+
+        HashSet<string> PlanNames { get; }
     }
 }
