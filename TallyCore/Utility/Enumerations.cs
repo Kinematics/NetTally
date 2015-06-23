77598601fe1d2d0134c124e14901507a58e0ff7e
David Smith <dsmith@datasync.com>
2015-06-23 18:50:30 -0500

    Get rid of code that was accidentally copied from another class.

--- TallyCore/Utility/Enumerations.cs
+++ TallyCore/Utility/Enumerations.cs
@@ -55,23 +55,6 @@ namespace NetTally
 
     public static class Enumerations
     {
-        // Regex for control and formatting characters that we don't want to allow processing of.
-        // EG: \u200B, non-breaking space
-        // Do not remove CR/LF characters
-        public static Regex UnsafeCharsRegex { get; } = new Regex(@"[\p{C}-[\r\n]]");
-
-        /// <summary>
-        /// Filter unsafe characters from the provided string.
-        /// </summary>
-        /// <param name="input">The string to filter.</param>
-        /// <returns>The input string with all unicode control characters (except cr/lf) removed.</returns>
-        public static string SafeString(string input)
-        {
-            return UnsafeCharsRegex.Replace(input, "");
-        }
-
-        public static string PlanNameMarker { get; } = "\u25C8";
-
         /// <summary>
         /// Gets a user-friendly string description of an enum value.
         /// </summary>
