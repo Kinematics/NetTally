9c559d43a96919ad408119c2618fc61a2c2a03a2
David Smith <dsmith@datasync.com>
2015-08-12 15:14:50 -0500

    Implement a working Vote class.

--- TallyCore/Votes/Vote.cs
+++ TallyCore/Votes/Vote.cs
@@ -4,21 +4,74 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace NetTally.Votes
+namespace NetTally
 {
-    public class Vote
+    public class Vote : IComparable, IComparer<Vote>
     {
-        public List<VoteLine> VoteLines { get; } = new List<VoteLine>();
+        public VoteType VoteType { get; }
+        public List<PostLine> VoteLines { get; }
 
-        public List<List<VoteLine>> VoteBlocks
+        public Vote(IEnumerable<PostLine> voteLines, VoteType voteType)
         {
-            get
-            {
-                var a = from v in VoteLines
-                        select new List<VoteLine>() { v };
+            VoteLines = voteLines.ToList();
+            VoteType = voteType;
+        }
 
-                return a.ToList();
+        public Vote(PostLine voteLine, VoteType voteType)
+        {
+            VoteLines = new List<PostLine>() { voteLine };
+            VoteType = voteType;
+        }
+
+        /// <summary>
+        /// Return the minimized string of all the vote lines in the vote.
+        /// </summary>
+        /// <returns></returns>
+        public string Minimized()
+        {
+            if (VoteLines.Count == 1)
+            {
+                return VoteString.MinimizeVote(VoteLines.First().Clean, PartitionMode.ByLine);
             }
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var line in VoteLines)
+            {
+                sb.Append(VoteString.MinimizeVote(line.Clean, PartitionMode.None));
+            }
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// IComparer function.
+        /// </summary>
+        /// <param name="x">The first object being compared.</param>
+        /// <param name="y">The second object being compared.</param>
+        /// <returns>Returns a negative value if x is 'before' y, 0 if they're equal, and
+        /// a positive value if x is 'after' y.</returns>
+        public int Compare(Vote x, Vote y)
+        {
+            if (x == null && y == null)
+                return 0;
+            if (x == null)
+                return -1;
+            if (y == null)
+                return 1;
+
+            return string.Compare(x.Minimized(), y.Minimized());
+        }
+
+        /// <summary>
+        /// IComparable function.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns>Returns a negative value if this is 'before' y, 0 if they're equal, and
+        /// a positive value if this is 'after' y.</returns>
+        public int CompareTo(object obj)
+        {
+            return Compare(this, obj as Vote);
         }
     }
 }
