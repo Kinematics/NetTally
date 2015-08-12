8714d06ffb45f130f6a2d9fad6c7f330657edcba
David Smith <dsmith@datasync.com>
2015-08-12 14:11:08 -0500

    Create new file for PostLine class.

---
+++ TallyCore/Votes/PostLine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetTally
+{
+    public class PostLine
+    {
+        public string Original { get; }
+        public string Clean { get; }
+
+        public PostLine(string line)
+        {
+            Original = line;
+            Clean = VoteString.CleanVote(line);
+        }
+    }
+}
