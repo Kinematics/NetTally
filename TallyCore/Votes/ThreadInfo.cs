af66ca73fb3607a006a0391cb0e800329036d3eb
David Smith <dsmith@datasync.com>
2015-10-29 12:53:52 -0500

    Add a class for thread info, kept separate from the posts.

---
+++ TallyCore/Votes/ThreadInfo.cs
@@ -0,0 +1,21 @@
+﻿using HtmlAgilityPack;
+
+namespace NetTally.Votes
+{
+    /// <summary>
+    /// Class to contain the author and title of a forum thread.
+    /// </summary>
+    public class ThreadInfo
+    {
+        public string Title { get; }
+        public string Author { get; }
+
+        public ThreadInfo(HtmlDocument page, IQuest quest)
+        {
+            IForumAdapter forumAdapter = quest.GetForumAdapter();
+
+            Title = forumAdapter.GetPageTitle(page);
+            Author = forumAdapter.GetAuthorOfThread(page);
+        }
+    }
+}
