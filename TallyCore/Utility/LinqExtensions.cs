cd00a8b0503bb84849d6b568b9e3c095325de6b5
David Smith <dsmith@datasync.com>
2015-11-25 19:50:52 -0600

    Add grouping linq extension functions.

---
+++ TallyCore/Utility/LinqExtensions.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetTally.Utility
+{
+    public class GroupOfAdjacent<TSource, TKey> : IEnumerable<TSource>, IGrouping<TKey, TSource>
+    {
+        public TKey Key { get; set; }
+        private List<TSource> GroupList { get; set; }
+
+        IEnumerator IEnumerable.GetEnumerator() => ((IEnumerable<TSource>)this).GetEnumerator();
+
+        IEnumerator<TSource> IEnumerable<TSource>.GetEnumerator()
+        {
+            foreach (var s in GroupList)
+                yield return s;
+        }
+
+        public GroupOfAdjacent(List<TSource> source, TKey key)
+        {
+            GroupList = source;
+            Key = key;
+        }
+    }
+
+    public static class LinqExtensions
+    {
+        public static IEnumerable<IGrouping<TKey, TSource>> GroupAdjacent<TSource, TKey>(this IEnumerable<TSource> source,
+            Func<TSource, TKey> keySelector)
+        {
+            TKey last = default(TKey);
+            bool haveLast = false;
+            List<TSource> list = new List<TSource>();
+
+            foreach (TSource s in source)
+            {
+                TKey k = keySelector(s);
+                if (haveLast)
+                {
+                    if (!k.Equals(last))
+                    {
+                        yield return new GroupOfAdjacent<TSource, TKey>(list, last);
+                        list = new List<TSource>();
+                        list.Add(s);
+                        last = k;
+                    }
+                    else
+                    {
+                        list.Add(s);
+                        last = k;
+                    }
+                }
+                else
+                {
+                    list.Add(s);
+                    last = k;
+                    haveLast = true;
+                }
+            }
+
+            if (haveLast)
+                yield return new GroupOfAdjacent<TSource, TKey>(list, last);
+        }
+
+        public static IEnumerable<IGrouping<TKey, TSource>> GroupAdjacentBySub<TSource, TKey>(this IEnumerable<TSource> source,
+            Func<TSource, TKey> keySelector)
+        {
+            TKey last = default(TKey);
+            bool haveLast = false;
+            List<TSource> list = new List<TSource>();
+
+            foreach (TSource s in source)
+            {
+                TKey k = keySelector(s);
+                if (haveLast)
+                {
+                    if (k == null)
+                    {
+                        list.Add(s);
+                    }
+                    else
+                    {
+                        yield return new GroupOfAdjacent<TSource, TKey>(list, last);
+                        list = new List<TSource>();
+                        list.Add(s);
+                        last = k;
+                    }
+                }
+                else
+                {
+                    list.Add(s);
+                    if (k != null)
+                        last = k;
+                    else
+                        last = default(TKey);
+                    haveLast = true;
+                }
+            }
+
+            if (haveLast)
+                yield return new GroupOfAdjacent<TSource, TKey>(list, last);
+        }
+    }
+}
