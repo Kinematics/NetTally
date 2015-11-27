12864a7e6cf730b6d278bcb034b2707c7ab4d605
David Smith <dsmith@datasync.com>
2015-11-27 02:13:23 -0600

    Create a custom string comparer class to allow for comparisons that ignore symbols.

--- TallyCore/Utility/Utility.cs
+++ TallyCore/Utility/Utility.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.Globalization;
 
 namespace NetTally.Utility
@@ -31,6 +32,66 @@ namespace NetTally.Utility
             int result = string.Compare(compX, compY, CultureInfo.CurrentUICulture, CompareOptions.IgnoreCase);
 
             return result;
+        }
+    }
+
+    public class CustomStringComparer : IComparer, IEqualityComparer, IComparer<string>, IEqualityComparer<string>
+    {
+        private CompareInfo myComp;
+        private CompareOptions myOptions = CompareOptions.None;
+
+        // Constructs a comparer using the specified CompareOptions.
+        public CustomStringComparer(CompareInfo cmpi, CompareOptions options)
+        {
+            myComp = cmpi;
+            this.myOptions = options;
+        }
+
+        // Compares strings with the CompareOptions specified in the constructor.
+        int IComparer<string>.Compare(string x, string y)
+        {
+            if (x == y) return 0;
+            if (x == null) return -1;
+            if (y == null) return 1;
+
+            return myComp.Compare(x, y, myOptions);
+        }
+
+        int IComparer.Compare(object x, object y)
+        {
+            if (x == y) return 0;
+            if (x == null) return -1;
+            if (y == null) return 1;
+
+            String sx = x as String;
+            String sy = y as String;
+            if (sx != null && sy != null)
+                return myComp.Compare(sx, sy, myOptions);
+            throw new ArgumentException("x and y should be strings.");
+        }
+
+        bool IEqualityComparer.Equals(object x, object y)
+        {
+            return ((IComparer)this).Compare(x, y) == 0;
+        }
+
+        bool IEqualityComparer<string>.Equals(string x, string y)
+        {
+            return ((IComparer<string>)this).Compare(x, y) == 0;
+        }
+
+        int IEqualityComparer.GetHashCode(object obj)
+        {
+            string str = obj as string;
+            if (str != null)
+                return str.GetHashCode();
+            else
+                return 0;
+        }
+
+        int IEqualityComparer<string>.GetHashCode(string obj)
+        {
+            return obj.GetHashCode();
         }
     }
 
