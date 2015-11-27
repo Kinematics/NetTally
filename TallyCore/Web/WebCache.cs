6904e0414d7f30e3eb3f5d0333814f2ab90ea3ab
David Smith <dsmith@datasync.com>
2015-11-27 14:36:51 -0600

    Delete CachedPage file, and move that small utility class into WebCache.

--- TallyCore/Web/WebCache.cs
+++ TallyCore/Web/WebCache.cs
@@ -5,6 +5,23 @@ using HtmlAgilityPack;
 
 namespace NetTally
 {
+    /// <summary>
+    /// Class to hold a web page, and the time at which it was loaded.
+    /// </summary>
+    public class CachedPage
+    {
+        public DateTime Timestamp { get; } = DateTime.Now;
+        public HtmlDocument Doc { get; }
+
+        public CachedPage(HtmlDocument doc)
+        {
+            Doc = doc;
+        }
+    }
+
+    /// <summary>
+    /// Class to handle caching web content.
+    /// </summary>
     public class WebCache
     {
         Dictionary<string, CachedPage> PageCache { get; }
