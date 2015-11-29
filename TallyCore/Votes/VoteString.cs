44c84b602e749048959a373a980f9bca9d891489
David Smith <dsmith@datasync.com>
2015-11-29 14:25:34 -0600

    Remove the + symbol as a valid vote marker.  It's never used, and doesn't contribute anything useful to the valid options.

--- TallyCore/Votes/VoteString.cs
+++ TallyCore/Votes/VoteString.cs
@@ -19,14 +19,14 @@ namespace NetTally
 
         static string markup = @"(\[/?[ibu]\]|\[color[^]]*\]|\[/color\])*";
         static string markups = $@"({markup}\s*)*";
-        static readonly Regex getPrefixRegex = new Regex($@"^(?<m1>({markup}\s*)*)(?<prefix>-*)\s*(?={markup}\[\s*{markup}\s*[xX+✓✔1-9])(?<remainder>.*)");
-        static readonly Regex getMarkerRegex = new Regex($@"^\s*(?<m1>({markup}\s*)*)(\[\s*(?<m2>({markup}\s*)*)(?<marker>[xX+✓✔1-9])\s*(?<m3>({markup}\s*)*)\])(?<remainder>.*)");
+        static readonly Regex getPrefixRegex = new Regex($@"^(?<m1>({markup}\s*)*)(?<prefix>-*)\s*(?={markup}\[\s*{markup}\s*[xX✓✔1-9])(?<remainder>.*)");
+        static readonly Regex getMarkerRegex = new Regex($@"^\s*(?<m1>({markup}\s*)*)(\[\s*(?<m2>({markup}\s*)*)(?<marker>[xX✓✔1-9])\s*(?<m3>({markup}\s*)*)\])(?<remainder>.*)");
         static readonly Regex getTaskRegex = new Regex($@"^\s*(?<m1>({markup}\s*)*)\s*(\[\s*(?<m2>({markup}\s*)*)(?!url=)(?<task>[^\[\]]+)\s*(?<m3>({markup}\s*)*)\])?\s*(?<remainder>.*)");
 
-        static readonly Regex getPartsRegex = new Regex($@"^(?<m1>{markups})(?<prefix>-*)(?<m2>{markups})\[\s*(?<m3>{markups})\s*(?<marker>[xX+✓✔1-9])\s*(?<m4>{markups})\s*\]\s*(?<m5>{markups})\s*(\[\s*(?<m6>{markups})\s*(?!url=)(?<task>[^\[\]]+)\s*(?<m7>{markups})\s*\])?\s*(?<content>.*)");
+        static readonly Regex getPartsRegex = new Regex($@"^(?<m1>{markups})(?<prefix>-*)(?<m2>{markups})\[\s*(?<m3>{markups})\s*(?<marker>[xX✓✔1-9])\s*(?<m4>{markups})\s*\]\s*(?<m5>{markups})\s*(\[\s*(?<m6>{markups})\s*(?!url=)(?<task>[^\[\]]+)\s*(?<m7>{markups})\s*\])?\s*(?<content>.*)");
 
         // Regex to get the different parts of the vote. Content includes only the first line of the vote.
-        static readonly Regex voteLineRegex = new Regex(@"^(?<prefix>-*)\s*\[\s*(?<marker>[xX+✓✔1-9])\s*\]\s*(\[\s*(?!url=)(?<task>[^]]*?)\s*\])?\s*(?<content>.*)");
+        static readonly Regex voteLineRegex = new Regex(@"^(?<prefix>-*)\s*\[\s*(?<marker>[xX✓✔1-9])\s*\]\s*(\[\s*(?!url=)(?<task>[^]]*?)\s*\])?\s*(?<content>.*)");
         // Regex to match any markup that we'll want to remove during comparisons.
         static readonly Regex markupRegex = new Regex(@"\[/?[ibu]\]|\[color[^]]*\]|\[/color\]");
         // Regex to allow us to collapse a vote to a commonly comparable version.
@@ -38,7 +38,7 @@ namespace NetTally
         // Regex to allow us to strip leading dashes from a per-line vote.
         static readonly Regex leadHyphenRegex = new Regex(@"^-+");
         // Regex for separating out the task from the other portions of a vote line.
-        static readonly Regex taskRegex = new Regex(@"^(?<pre>.*?\[[xX+✓✔1-9]\])\s*(\[\s*(?!url=|color=|b\]|i\]|u\])(?<task>[^]]*?)\s*\])?\s*(?<remainder>.+)", RegexOptions.Singleline);
+        static readonly Regex taskRegex = new Regex(@"^(?<pre>.*?\[[xX✓✔1-9]\])\s*(\[\s*(?!url=|color=|b\]|i\]|u\])(?<task>[^]]*?)\s*\])?\s*(?<remainder>.+)", RegexOptions.Singleline);
         // Potential reference to another user's plan.
         static readonly Regex referenceNameRegex = new Regex(@"^(?<label>(base\s*)?plan(:|\s)+)?(?<reference>.+)", RegexOptions.IgnoreCase);
         // Potential reference to another user's plan.
@@ -47,7 +47,7 @@ namespace NetTally
         static readonly Regex condensedVoteRegex = new Regex(@"^\[(?<task>[^]]*)\]\s*(?<content>.+)");
 
         // Regex for the pre-content area of a vote line, that will only match if there are no BBCode tags in that area of the vote line.
-        static readonly Regex precontentRegex = new Regex(@"^(?:[\s-]*)\[[xX+✓✔1-9]\](?!\s*\[/(?:[bui]|color)\])(?!(?:\s*\[(?:[bui]|color=[^]]+)\])+\s*\[(?![bui]|color=[^]]+|url=[^]]+)[^]]+\])");
+        static readonly Regex precontentRegex = new Regex(@"^(?:[\s-]*)\[[xX✓✔1-9]\](?!\s*\[/(?:[bui]|color)\])(?!(?:\s*\[(?:[bui]|color=[^]]+)\])+\s*\[(?![bui]|color=[^]]+|url=[^]]+)[^]]+\])");
 
         // Regex for any opening BBCode tag.
         static readonly Regex openBBCodeRegex = new Regex(@"\[(b|i|u|color=[^]]+)\]");
