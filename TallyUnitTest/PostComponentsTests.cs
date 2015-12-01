eba8308e600355e60c0b2217320e65effc94e163
David Smith <dsmith@datasync.com>
2015-11-30 19:08:36 -0600

    Clean up tests for post component class.  Add a variety of input parsing.

--- TallyUnitTest/PostComponentsTests.cs
+++ TallyUnitTest/PostComponentsTests.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Linq;
 using NetTally;
 
@@ -7,13 +8,70 @@ namespace TallyUnitTest
     [TestClass()]
     public class PostComponentsTests
     {
-        // Test that various constructions result in proper object formation.
+        static string author = "Kinematics";
+        static string id = "12345";
+
+        #region Exceptions during construction
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void BadConstruction1()
+        {
+            string msg = @"What do you think they'll be doing now?";
+
+            PostComponents p = new PostComponents(null, id, msg);
+        }
 
         [TestMethod()]
-        public void ConstructorTest1()
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void BadConstruction2()
         {
-            string author = "Kinematics";
-            string id = "12345";
+            string msg = @"What do you think they'll be doing now?";
+
+            PostComponents p = new PostComponents(author, null, msg);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void BadConstruction3()
+        {
+            //string msg = @"What do you think they'll be doing now?";
+
+            PostComponents p = new PostComponents(author, id, null);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void BadConstruction4()
+        {
+            string msg = @"What do you think they'll be doing now?";
+
+            PostComponents p = new PostComponents("", id, msg);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void BadConstruction5()
+        {
+            string msg = @"What do you think they'll be doing now?";
+
+            PostComponents p = new PostComponents(author, "", msg);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void BadConstruction6()
+        {
+            //string msg = @"What do you think they'll be doing now?";
+
+            PostComponents p = new PostComponents(author, id, "");
+        }
+        #endregion
+
+        #region Basic non-votes, and checking author and ID
+
+        [TestMethod()]
+        public void NoVoteContent()
+        {
             string msg = @"What do you think they'll be doing now?";
 
             PostComponents p = new PostComponents(author, id, msg);
@@ -26,10 +84,8 @@ namespace TallyUnitTest
         }
 
         [TestMethod()]
-        public void ConstructorTest2()
+        public void EmbeddedMarker()
         {
-            string author = "Kinematics";
-            string id = "12345";
             string msg = @"What do you think they'll be doing now? Use [x] to indicate which you're selecting.";
 
             PostComponents p = new PostComponents(author, id, msg);
@@ -42,29 +98,26 @@ namespace TallyUnitTest
         }
 
         [TestMethod()]
-        public void ConstructorTest3()
+        public void NonNumericID()
         {
-            string author = "Kinematics";
-            string id = "12345";
-            string msg = @"What do you think they'll be doing now?
-[x] Ferris wheel";
+            string id = "A3000F";
+            string msg = @"What do you think they'll be doing now?";
 
             PostComponents p = new PostComponents(author, id, msg);
 
             Assert.AreEqual(author, p.Author);
             Assert.AreEqual(id, p.ID);
-            Assert.AreEqual(12345, p.IDValue);
-            Assert.IsTrue(p.IsVote);
-            Assert.IsNotNull(p.VoteStrings);
-            Assert.AreEqual(1, p.VoteStrings.Count);
-            Assert.AreEqual(@"[x] Ferris wheel", p.VoteStrings.First());
+            Assert.AreEqual(0, p.IDValue);
+            Assert.IsFalse(p.IsVote);
+            Assert.IsNull(p.VoteStrings);
         }
 
+        #endregion
+
+        #region Various vote constructions
         [TestMethod()]
-        public void ConstructorTest4()
+        public void ConstructTallyPost()
         {
-            string author = "Kinematics";
-            string id = "12345";
             string msg = @"What do you think they'll be doing now? Use [x] to indicate which you're selecting.
 Last vote was:
 [color=invisible]##### NetTally 1.1.8[/color]
@@ -72,14 +125,164 @@ Last vote was:
 
             PostComponents p = new PostComponents(author, id, msg);
 
-            Assert.AreEqual(author, p.Author);
-            Assert.AreEqual(id, p.ID);
-            Assert.AreEqual(12345, p.IDValue);
             Assert.IsFalse(p.IsVote);
             Assert.IsNull(p.VoteStrings);
         }
 
-        // Test that various comparisons properly relate two separate objects.
+        [TestMethod()]
+        public void ConstructorTest1()
+        {
+            string msg =
+@"What do you think they'll be doing now?
+[x] Ferris wheel";
+
+            PostComponents p = new PostComponents(author, id, msg);
+
+            Assert.IsTrue(p.IsVote);
+            Assert.IsNotNull(p.VoteStrings);
+            Assert.AreEqual(1, p.VoteStrings.Count);
+            Assert.AreEqual(@"[x] Ferris wheel", p.VoteStrings.First());
+        }
+
+        [TestMethod()]
+        public void ConstructorTest2()
+        {
+            string msg =
+@"What do you think they'll be doing now?
+[x] Ferris wheel
+Additional:
+[x] Ice cream";
+
+            PostComponents p = new PostComponents(author, id, msg);
+
+            Assert.IsTrue(p.IsVote);
+            Assert.IsNotNull(p.VoteStrings);
+            Assert.AreEqual(2, p.VoteStrings.Count);
+            Assert.AreEqual(@"[x] Ferris wheel", p.VoteStrings[0]);
+            Assert.AreEqual(@"[x] Ice cream", p.VoteStrings[1]);
+        }
+
+        [TestMethod()]
+        public void ConstructorTest3()
+        {
+            string msg =
+@"What do you think they'll be doing now?
+[x] Ferris wheel
+-[x] At the top";
+
+            PostComponents p = new PostComponents(author, id, msg);
+
+            Assert.IsTrue(p.IsVote);
+            Assert.IsNotNull(p.VoteStrings);
+            Assert.AreEqual(2, p.VoteStrings.Count);
+            Assert.AreEqual(@"[x] Ferris wheel", p.VoteStrings[0]);
+            Assert.AreEqual(@"-[x] At the top", p.VoteStrings[1]);
+        }
+
+        [TestMethod()]
+        public void ConstructorTest4()
+        {
+            string msg =
+@"What do you think they'll be doing now?
+[x] Ferris wheel
+- [X] At the top";
+
+            PostComponents p = new PostComponents(author, id, msg);
+
+            Assert.IsTrue(p.IsVote);
+            Assert.IsNotNull(p.VoteStrings);
+            Assert.AreEqual(2, p.VoteStrings.Count);
+            Assert.AreEqual(@"[x] Ferris wheel", p.VoteStrings[0]);
+            Assert.AreEqual(@"- [X] At the top", p.VoteStrings[1]);
+        }
+
+        [TestMethod()]
+        public void ConstructorTest5()
+        {
+            string msg =
+@"What do you think they'll be doing now?
+[x] Ferris wheel
+- - [X] - At the top";
+
+            PostComponents p = new PostComponents(author, id, msg);
+
+            Assert.IsTrue(p.IsVote);
+            Assert.IsNotNull(p.VoteStrings);
+            Assert.AreEqual(2, p.VoteStrings.Count);
+            Assert.AreEqual(@"[x] Ferris wheel", p.VoteStrings[0]);
+            Assert.AreEqual(@"- - [X] - At the top", p.VoteStrings[1]);
+        }
+
+        [TestMethod()]
+        public void ConstructorTest6()
+        {
+            string msg =
+@"What do you think they'll be doing now?
+[b][x] Ferris wheel
+-[x] At the top[/b]";
+
+            PostComponents p = new PostComponents(author, id, msg);
+
+            Assert.IsTrue(p.IsVote);
+            Assert.IsNotNull(p.VoteStrings);
+            Assert.AreEqual(2, p.VoteStrings.Count);
+            Assert.AreEqual(@"[x] Ferris wheel", p.VoteStrings[0]);
+            Assert.AreEqual(@"-[x] At the top", p.VoteStrings[1]);
+        }
+
+        [TestMethod()]
+        public void ConstructorTest7()
+        {
+            string msg =
+@"What do you think they'll be doing now?
+[x] Ferris wheel
+[X] [i]Teacups[/i]";
+
+            PostComponents p = new PostComponents(author, id, msg);
+
+            Assert.IsTrue(p.IsVote);
+            Assert.IsNotNull(p.VoteStrings);
+            Assert.AreEqual(2, p.VoteStrings.Count);
+            Assert.AreEqual(@"[x] Ferris wheel", p.VoteStrings[0]);
+            Assert.AreEqual(@"[X] [i]Teacups[/i]", p.VoteStrings[1]);
+        }
+
+        [TestMethod()]
+        public void ConstructorTest8()
+        {
+            string msg =
+@"What do you think they'll be doing now?
+[x] Ferris wheel
+[x] [color=orange]Teacups[/color]";
+
+            PostComponents p = new PostComponents(author, id, msg);
+
+            Assert.IsTrue(p.IsVote);
+            Assert.IsNotNull(p.VoteStrings);
+            Assert.AreEqual(2, p.VoteStrings.Count);
+            Assert.AreEqual(@"[x] Ferris wheel", p.VoteStrings[0]);
+            Assert.AreEqual(@"[x] [color=orange]Teacups[/color]", p.VoteStrings[1]);
+        }
+
+        [TestMethod()]
+        public void ConstructorTest9()
+        {
+            string msg =
+@"What do you think they'll be doing now?
+[x] Ferris wheel
+[x] [color=#ff00AA]Teacups[/color]";
+
+            PostComponents p = new PostComponents(author, id, msg);
+
+            Assert.IsTrue(p.IsVote);
+            Assert.IsNotNull(p.VoteStrings);
+            Assert.AreEqual(2, p.VoteStrings.Count);
+            Assert.AreEqual(@"[x] Ferris wheel", p.VoteStrings[0]);
+            Assert.AreEqual(@"[x] [color=#ff00AA]Teacups[/color]", p.VoteStrings[1]);
+        }
+        #endregion
+
+        #region Test that various comparisons properly relate two separate objects.
 
         [TestMethod()]
         public void CompareTest1()
@@ -151,5 +354,6 @@ Last vote was:
 
             Assert.IsTrue(p1.CompareTo(p2) > 0);
         }
+        #endregion
     }
 }
