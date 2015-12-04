bb450dd0301a22d0bf6596d9e8cca27a5d8b8341
David Smith <dsmith@datasync.com>
2015-12-04 14:18:12 -0600

    Track the calling method for requests to log messages. Output the timestamp and calling method in the log.

--- NetTally/ErrorLog.cs
+++ NetTally/ErrorLog.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Text;
+using System.Runtime.CompilerServices;
 
 namespace NetTally
 {
@@ -13,16 +14,18 @@ namespace NetTally
         /// Public shortcut function to log an exception.
         /// </summary>
         /// <param name="e">Exception to be logged.</param>
+        /// <param name="callingMethod">The method that made the request to log the error.</param>
         /// <returns>Returns the name of the file the error was logged to.</returns>
-        public static string Log(Exception e) => Log(exception: e);
+        public static string Log(Exception e, [CallerMemberName] string callingMethod = "") => Log(exception: e, callingMethod: callingMethod);
 
         /// <summary>
         /// Public function to log either a text message or an exception, or both.
         /// </summary>
         /// <param name="message">The message to log.</param>
         /// <param name="exception">The exception to log.</param>
+        /// <param name="callingMethod">The method that made the request to log the error.</param>
         /// <returns>Returns the name of the file the log was saved in.</returns>
-        public static string Log(string message = null, Exception exception = null)
+        public static string Log(string message = null, Exception exception = null, [CallerMemberName] string callingMethod = "")
         {
             try
             {
@@ -30,7 +33,7 @@ namespace NetTally
                 if (filename == null || filename == string.Empty)
                     return null;
 
-                string output = ComposeOutput(message, exception);
+                string output = ComposeOutput(callingMethod, message, exception);
                 if (output == null)
                     return null;
 
@@ -43,18 +46,28 @@ namespace NetTally
                 return null;
             }
         }
-        
+
         /// <summary>
         /// Generates the string that will be saved to the output log.
         /// </summary>
+        /// <param name="callingMethod">The method that made the request to log the error.</param>
         /// <param name="message">Text message to output.</param>
         /// <param name="ex">The exception whose message and stack trace will be output.</param>
         /// <returns>Returns the compiled output string.</returns>
-        private static string ComposeOutput(string message, Exception ex)
+        private static string ComposeOutput(string callingMethod, string message, Exception ex)
         {
             try
             {
                 StringBuilder sb = new StringBuilder();
+
+                sb.Append("Timestamp: ");
+                sb.AppendLine(DateTime.Now.ToLongTimeString());
+                if (!string.IsNullOrEmpty(callingMethod))
+                {
+                    sb.Append("Called from: ");
+                    sb.AppendLine(callingMethod);
+                }
+                sb.AppendLine();
 
                 if (message != null)
                     sb.Append(message);
