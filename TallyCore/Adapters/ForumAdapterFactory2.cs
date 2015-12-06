3deb883bd3f66604f32e8b196379826bd56e4563
David Smith <dsmith@datasync.com>
2015-12-06 17:11:39 -0600

    Add general reflection for finding an adapter that can handle a given page. That way the factory doesn't have to be updated per additional class added.

--- TallyCore/Adapters/ForumAdapterFactory2.cs
+++ TallyCore/Adapters/ForumAdapterFactory2.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -47,8 +48,52 @@ namespace NetTally.Adapters
 
                 var page = await webPageProvider.GetPage(uri.AbsoluteUri, uri.Host, Caching.UseCache, token);
 
-                if (XenForoAdapter2.CanHandlePage(page))
-                    return new XenForoAdapter2(uri);
+                if (page == null)
+                    return null;
+
+                //if (XenForoAdapter2.CanHandlePage(page))
+                //    return new XenForoAdapter2(uri);
+
+                //if (vBulletinAdapter3_2.CanHandlePage(page))
+                //    return new vBulletinAdapter3_2(uri);
+
+
+                const string detectMethodName = "CanHandlePage";
+
+                Type ti = typeof(IForumAdapter2);
+
+                //from asm in AppDomain.CurrentDomain.GetAssemblies()
+
+                var adapterList = from t in Assembly.GetExecutingAssembly().GetTypes()
+                                  where (!t.IsInterface && ti.IsAssignableFrom(t))
+                                  select t;
+
+                foreach (var adapterClass in adapterList)
+                {
+                    try
+                    {
+                        var detect = adapterClass.FindMembers(MemberTypes.Method, BindingFlags.Static | BindingFlags.Public, Type.FilterName, detectMethodName);
+
+                        if (detect.Any())
+                        {
+                            bool result = (bool)adapterClass.InvokeMember(detectMethodName,
+                                BindingFlags.Public | BindingFlags.Static | BindingFlags.InvokeMethod,
+                                null, null, new object[] { page });
+
+                            if (result)
+                            {
+                                var adapter = (IForumAdapter2)Activator.CreateInstance(adapterClass, new object[] { uri });
+
+                                return adapter;
+                            }
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        { }
+                    }
+                }
+
             }
             catch (OperationCanceledException)
             { }
