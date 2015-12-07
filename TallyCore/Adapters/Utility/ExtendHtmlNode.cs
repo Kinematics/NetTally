7395264db1f33279f1dff04fb8f2bd344ada0f7b
David Smith <dsmith@datasync.com>
2015-12-06 21:39:46 -0600

    Add a utility extension class for HTML manipulation and searching.

---
+++ TallyCore/Adapters/Utility/ExtendHtmlNode.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace NetTally.Adapters
+{
+    public static class ExtendHtmlNode
+    {
+        public static HtmlNode GetChildNodeWithClass(this HtmlNode node, string htmlClass, string childName = null)
+        {
+            if (string.IsNullOrEmpty(htmlClass))
+                throw new ArgumentNullException(nameof(htmlClass));
+
+            IEnumerable<HtmlNode> children;
+            if (string.IsNullOrEmpty(childName))
+                children = node.ChildNodes;
+            else
+                children = node.Elements(childName);
+
+            return children.FirstOrDefault(n => n.GetAttributeValue("class", "").Split(' ').Contains(htmlClass));
+        }
+    }
+}
