5448655e570a0b4b17c80837db007f3dde6f0415
David Smith <dsmith@datasync.com>
2015-12-07 18:29:15 -0600

    Comments. Switch to a using{} for the web page provider.

--- TallyCore/Adapters/ForumAdapterFactory.cs
+++ TallyCore/Adapters/ForumAdapterFactory.cs
@@ -9,12 +9,27 @@ namespace NetTally.Adapters
     public static class ForumAdapterFactory
     {
         #region Public functions
+        /// <summary>
+        /// Overload to get an adapter for a quest without specifying a cancellation token.
+        /// </summary>
+        /// <param name="quest">The quest to get the adapter for.</param>
+        /// <returns>Returns a forum adapter for the quest.</returns>
         public async static Task<IForumAdapter2> GetAdapter(IQuest quest) => await GetAdapter(quest, CancellationToken.None);
 
+        /// <summary>
+        /// Function to generate an appropriate forum adapter for the specified quest.
+        /// Generates an exception if the quest's thread URL is not well-formed.
+        /// </summary>
+        /// <param name="quest">The quest to get the adapter for.</param>
+        /// <param name="token">Cancellation token.</param>
+        /// <returns>Returns an appropriate forum adapter for the quest, if found. Otherwise, null.</returns>
         public async static Task<IForumAdapter2> GetAdapter(IQuest quest, CancellationToken token)
         {
+            if (quest == null)
+                throw new ArgumentNullException(nameof(quest));
+
             if (!Uri.IsWellFormedUriString(quest.ThreadName, UriKind.Absolute))
-                throw new InvalidOperationException($"Thread is not valid:\n{quest.ThreadName}\n\nPlease enter a valid URL.");
+                throw new InvalidOperationException($"Thread URL is not valid:\n{quest.ThreadName}\n\nPlease enter a valid URL.");
 
             Uri uri = new Uri(quest.ThreadName);
 
@@ -28,6 +43,12 @@ namespace NetTally.Adapters
         #endregion
 
         #region Private methods
+        /// <summary>
+        /// Generate a forum adapter object for certain known forums,
+        /// without needing to check the contents of the forum page.
+        /// </summary>
+        /// <param name="uri">The URI for the forum thread.</param>
+        /// <returns>Returns a forum adapter for certain known forums. Otherwise, null.</returns>
         private static IForumAdapter2 GetKnownForumAdapter(Uri uri)
         {
             switch (uri.Host)
@@ -42,73 +63,78 @@ namespace NetTally.Adapters
             }
         }
 
+        /// <summary>
+        /// Generate a forum adapter for any unknown forum.
+        /// Need to load a forum page (asynchronously) to determine which forum adapter is needed.
+        /// </summary>
+        /// <param name="uri">The URI of the forum thread to check.</param>
+        /// <param name="token">Cancellation token.</param>
+        /// <returns>Returns a forum adapter for the forum thread if one can be determined.
+        /// Otherwise, null.</returns>
         private async static Task<IForumAdapter2> GetUnknownForumAdapter(Uri uri, CancellationToken token)
         {
-            IPageProvider webPageProvider = null;
-
-            try
+            using (IPageProvider webPageProvider = new WebPageProvider())
             {
-                webPageProvider = new WebPageProvider();
-
-                var page = await webPageProvider.GetPage(uri.AbsoluteUri, uri.Host, Caching.UseCache, token);
-
-                if (page == null)
-                    return null;
-
-                if (token.IsCancellationRequested)
-                    return null;
-
-                //if (XenForoAdapter2.CanHandlePage(page))
-                //    return new XenForoAdapter2(uri);
-
-                //if (vBulletinAdapter3_2.CanHandlePage(page))
-                //    return new vBulletinAdapter3_2(uri);
-
-
-                const string detectMethodName = "CanHandlePage";
-
-                Type ti = typeof(IForumAdapter2);
-
-                //from asm in AppDomain.CurrentDomain.GetAssemblies()
-
-                var adapterList = from t in Assembly.GetExecutingAssembly().GetTypes()
-                                  where (!t.IsInterface && ti.IsAssignableFrom(t))
-                                  select t;
-
-                foreach (var adapterClass in adapterList)
+                try
                 {
-                    try
+                    var page = await webPageProvider.GetPage(uri.AbsoluteUri, uri.Host, Caching.UseCache, token);
+
+                    if (page == null)
+                        return null;
+
+                    if (token.IsCancellationRequested)
+                        return null;
+
+                    // Manual checks against known classes. Skipping to do automatic checks.
+                    //if (XenForoAdapter2.CanHandlePage(page))
+                    //    return new XenForoAdapter2(uri);
+
+                    //if (vBulletinAdapter3_2.CanHandlePage(page))
+                    //    return new vBulletinAdapter3_2(uri);
+
+
+                    const string detectMethodName = "CanHandlePage";
+
+                    Type ti = typeof(IForumAdapter2);
+
+                    // Get the list of all adapter classes built off of the IForumAdapter interface.
+                    var adapterList = from t in Assembly.GetExecutingAssembly().GetTypes()
+                                      where (!t.IsInterface && ti.IsAssignableFrom(t))
+                                      select t;
+
+                    foreach (var adapterClass in adapterList)
                     {
-                        var detect = adapterClass.FindMembers(MemberTypes.Method, BindingFlags.Static | BindingFlags.Public, Type.FilterName, detectMethodName);
-
-                        if (detect.Any())
+                        try
                         {
-                            bool result = (bool)adapterClass.InvokeMember(detectMethodName,
-                                BindingFlags.Public | BindingFlags.Static | BindingFlags.InvokeMethod,
-                                null, null, new object[] { page });
+                            var detectMethodArray = adapterClass.FindMembers(MemberTypes.Method, BindingFlags.Static | BindingFlags.Public,
+                                Type.FilterName, detectMethodName);
 
-                            if (result)
+                            if (detectMethodArray.Any())
                             {
-                                var adapter = (IForumAdapter2)Activator.CreateInstance(adapterClass, new object[] { uri });
+                                bool result = (bool)adapterClass.InvokeMember(detectMethodName,
+                                    BindingFlags.Public | BindingFlags.Static | BindingFlags.InvokeMethod,
+                                    null, null, new object[] { page });
 
-                                return adapter;
+                                if (result)
+                                {
+                                    var adapter = (IForumAdapter2)Activator.CreateInstance(adapterClass, new object[] { uri });
+
+                                    return adapter;
+                                }
                             }
                         }
+                        catch (Exception e)
+                        {
+                            { }
+                        }
                     }
-                    catch (Exception e)
-                    {
-                        { }
-                    }
+
                 }
-
-            }
-            catch (OperationCanceledException)
-            { }
-            finally
-            {
-                webPageProvider?.Dispose();
+                catch (OperationCanceledException)
+                { }
             }
 
+            // If nothing was found, return null.
             return null;
         }
         #endregion
