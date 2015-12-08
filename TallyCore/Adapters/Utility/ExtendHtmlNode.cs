98c51a0fd3fae321d40a0795d99922bd4b1bb640
David Smith <dsmith@datasync.com>
2015-12-07 18:40:41 -0600

    Add comments.

--- TallyCore/Adapters/Utility/ExtendHtmlNode.cs
+++ TallyCore/Adapters/Utility/ExtendHtmlNode.cs
@@ -9,6 +9,15 @@ namespace NetTally.Adapters
 {
     public static class ExtendHtmlNode
     {
+        /// <summary>
+        /// Get the single HTML node result for searching for a direct child element
+        /// that has the specified class attribute.
+        /// </summary>
+        /// <param name="node">The object the extension method is being used on.</param>
+        /// <param name="element">The name of the element we're looking for.
+        /// If it's null or empty, any element will work.</param>
+        /// <param name="@class">The name of the class to search for.  Must be provided.</param>
+        /// <returns>Returns the element with the specified class, if found.  Otherwise, null.</returns>
         public static HtmlNode GetChildWithClass(this HtmlNode node, string element, string @class)
         {
             if (string.IsNullOrEmpty(@class))
@@ -23,6 +32,15 @@ namespace NetTally.Adapters
             return children.FirstOrDefault(n => n.GetAttributeValue("class", "").Split(' ').Contains(@class));
         }
 
+        /// <summary>
+        /// Get the single HTML node result for searching for any descendant element
+        /// that has the specified class attribute.
+        /// </summary>
+        /// <param name="node">The object the extension method is being used on.</param>
+        /// <param name="element">The name of the element we're looking for.
+        /// If it's null or empty, any element will work.</param>
+        /// <param name="@class">The name of the class to search for.  Must be provided.</param>
+        /// <returns>Returns the element with the specified class, if found.  Otherwise, null.</returns>
         public static HtmlNode GetDescendantNodeWithClass(this HtmlNode node, string element, string @class)
         {
             if (string.IsNullOrEmpty(@class))
