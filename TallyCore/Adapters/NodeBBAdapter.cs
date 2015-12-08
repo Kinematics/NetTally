80444cb8f72345037c188404d25cd1d145c320e9
David Smith <dsmith@datasync.com>
2015-12-08 16:39:07 -0600

    Add NodeBB and pbpBB forum adapters. Neither can actually run because of limitations in the forum software.

---
+++ TallyCore/Adapters/NodeBBAdapter.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace NetTally.Adapters
+{
+    public class NodeBBAdapter : IForumAdapter
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="site">URI for the thread to manage.</param>
+        public NodeBBAdapter(Uri site)
+        {
+            Site = site;
+        }
+
+        #region Site properties
+        Uri site;
+        // Example: https://community.nodebb.org/topic/180/who-is-using-nodebb?page=2
+        static readonly Regex siteRegex = new Regex(@"^(?!.*\s)(?<base>https?://[^/]+/([^/]+/)*)(?<thread>[^?]+(?=?|$))");
+
+        /// <summary>
+        /// Property for the site this adapter is handling.
+        /// Can be changed if the quest thread details are changed.
+        /// </summary>
+        public Uri Site
+        {
+            get
+            {
+                return site;
+            }
+            set
+            {
+                // Must set a valid value
+                if (value == null)
+                    throw new ArgumentNullException(nameof(Site));
+
+                // If object doesn't have a value, just set and be done.
+                if (site == null)
+                {
+                    site = value;
+                    UpdateSiteData();
+                    return;
+                }
+
+                // If the URI didn't change, we don't need to do anything.
+                if (site.AbsoluteUri == value.AbsoluteUri)
+                {
+                    return;
+                }
+
+                // If the host *did* change, we can't consider this a valid adapter anymore.
+                if (site.Host != value.Host)
+                {
+                    throw new InvalidOperationException("Host has changed.");
+                }
+
+                // Otherwise, just update the site URI and data.
+                site = value;
+                UpdateSiteData();
+            }
+        }
+
+        /// <summary>
+        /// When the Site value changes, update the base site and thread name values appropriately.
+        /// </summary>
+        private void UpdateSiteData()
+        {
+            if (site == null)
+                throw new InvalidOperationException("Site value is null.");
+
+            Match m = siteRegex.Match(site.AbsoluteUri);
+            if (m.Success)
+            {
+                BaseSite = m.Groups["base"].Value;
+                ThreadName = m.Groups["thread"].Value;
+            }
+            else
+            {
+                throw new ArgumentException($"Invalid vBulletin3 site URL format:\n{site.AbsoluteUri}");
+            }
+        }
+
+        string BaseSite { get; set; }
+        string ThreadName { get; set; }
+
+        string ThreadBaseUrl => $"{BaseSite}{ThreadName}";
+        string PostsBaseUrl => $"{BaseSite}?p=";
+        #endregion
+
+        #region Public interface
+        /// <summary>
+        /// Get the default number of posts per page for this forum type.
+        /// </summary>
+        public int DefaultPostsPerPage => 20;
+
+        /// <summary>
+        /// Generate a URL to access the specified page of the adapter's thread.
+        /// </summary>
+        /// <param name="page">The page of the thread that is being loaded.</param>
+        /// <returns>Returns a URL formatted to load the requested page of the thread.</returns>
+        public string GetUrlForPage(int page, int postsPerPage) => $"{ThreadBaseUrl}&page={page}";
+
+        /// <summary>
+        /// Generate a URL to access the specified post of the adapter's thread.
+        /// </summary>
+        /// <param name="postId">The permalink ID of the post being requested.</param>
+        /// <returns>Returns a URL formatted to load the requested post.</returns>
+        public string GetPermalinkForId(string postId) => $"{PostsBaseUrl}{postId}";
+
+        /// <summary>
+        /// Get the starting post number, to begin tallying from.
+        /// Since vBulletin doesn't have threadmarks, this will always be whatever
+        /// is specified as the start post for the quest.
+        /// </summary>
+        /// <param name="quest">The quest being tallied.</param>
+        /// <param name="pageProvider">A page provider to allow loading the threadmark page.</param>
+        /// <param name="token">A cancellation token.</param>
+        /// <returns>Returns data indicating where to begin tallying the thread.</returns>
+        public Task<ThreadStartValue> GetStartingPostNumber(IQuest quest, IPageProvider pageProvider, CancellationToken token) =>
+            Task.FromResult(new ThreadStartValue(true, quest.StartPost));
+
+        /// <summary>
+        /// Get thread info from the provided page.
+        /// </summary>
+        /// <param name="page">A web page from a forum that this adapter can handle.</param>
+        /// <returns>Returns thread information that can be gleaned from that page.</returns>
+        public ThreadInfo GetThreadInfo(HtmlDocument page)
+        {
+            if (page == null)
+                throw new ArgumentNullException(nameof(page));
+
+            string title;
+            string author = string.Empty;
+            int pages = 1;
+
+            HtmlNode doc = page.DocumentNode.Element("html");
+
+            // Find the page title
+            title = PostText.CleanupWebString(doc.Element("head")?.Element("title")?.InnerText);
+
+            // Find the number of pages
+            var body = doc.Element("body");
+            var main = body?.Element("main").Elements("div").FirstOrDefault(n => n.Id == "content");
+
+            var paginationContainer = main.GetDescendantWithClass("div", "pagination-container");
+
+            if (paginationContainer != null)
+            {
+                var lastPage = paginationContainer.Element("ul").Elements("li").LastOrDefault(n => n.GetAttributeValue("class", "").Split(' ').Contains("page"));
+                var lastPageNumber = lastPage?.Element("a")?.GetAttributeValue("data-page", "1");
+
+                if (lastPageNumber != null)
+                    pages = int.Parse(lastPageNumber);
+            }
+
+            ThreadInfo info = new ThreadInfo(title, author, pages);
+
+            return info;
+        }
+
+        /// <summary>
+        /// Get a list of posts from the provided page.
+        /// </summary>
+        /// <param name="page">A web page from a forum that this adapter can handle.</param>
+        /// <returns>Returns a list of constructed posts from this page.</returns>
+        public IEnumerable<PostComponents> GetPosts(HtmlDocument page)
+        {
+            var body = page.DocumentNode.Element("html").Element("body");
+            var main = body?.Element("main").Elements("div").FirstOrDefault(n => n.Id == "content");
+            var topic = main?.GetDescendantWithClass("div", "topic");
+            var postlist = topic?.GetChildWithClass("ul", "posts");
+
+            if (postlist == null)
+                return new List<PostComponents>();
+
+            var posts = from p in postlist.Elements("li")
+                        select GetPost(p);
+
+            return posts;
+        }
+        #endregion
+
+        #region Utility
+        /// <summary>
+        /// Get a completed post from the provided HTML div node.
+        /// </summary>
+        /// <param name="li">Div node that contains the post.</param>
+        /// <returns>Returns a post object with required information.</returns>
+        private PostComponents GetPost(HtmlNode li)
+        {
+            if (li == null)
+                throw new ArgumentNullException(nameof(li));
+
+            string author = "";
+            string id;
+            string text;
+            int number = 0;
+
+            id = li.GetAttributeValue("data-pid", "");
+            author = li.GetAttributeValue("data-username", "");
+            number = int.Parse(li.GetAttributeValue("data-index", "0"));
+
+            var content = li.GetChildWithClass("div", "content");
+
+            // Get the full post text.
+            text = PostText.ExtractPostText(content, n => false);
+
+
+            PostComponents post;
+            try
+            {
+                post = new PostComponents(author, id, text, number);
+            }
+            catch
+            {
+                post = null;
+            }
+
+            return post;
+        }
+        #endregion
+
+        #region Detection
+        /// <summary>
+        /// Static detection of whether the provided web page is a XenForo forum thread.
+        /// </summary>
+        /// <param name="page">Web page to examine.</param>
+        /// <returns>Returns true if it's detected as a XenForo page.  Otherwise, false.</returns>
+        public static bool CanHandlePage(HtmlDocument page)
+        {
+            if (page == null)
+                return false;
+
+            var html = page.DocumentNode.Element("html");
+
+            // No idea how to tell...
+
+            return false;
+        }
+        #endregion
+
+    }
+}
---
+++ TallyCore/Adapters/phpBBAdapter.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace NetTally.Adapters
+{
+    public class phpBBAdapter : IForumAdapter
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="site">URI for the thread to manage.</param>
+        public phpBBAdapter(Uri site)
+        {
+            Site = site;
+        }
+
+        #region Site properties
+        Uri site;
+        // Example: https://www.phpbb.com/community/viewtopic.php?f=466&t=2347241&start=15#p14278841
+        // https://www.phpbb.com/community/viewtopic.php?t=2347241&p14278841
+        static readonly Regex siteRegex = new Regex(@"^(?<base>https?://[^/]+/([^/]+/)*)viewtopic.php?(f=\d+&)?(t=(?<thread>\d+(?=?|#|$)))");
+
+        /// <summary>
+        /// Property for the site this adapter is handling.
+        /// Can be changed if the quest thread details are changed.
+        /// </summary>
+        public Uri Site
+        {
+            get
+            {
+                return site;
+            }
+            set
+            {
+                // Must set a valid value
+                if (value == null)
+                    throw new ArgumentNullException(nameof(Site));
+
+                // If object doesn't have a value, just set and be done.
+                if (site == null)
+                {
+                    site = value;
+                    UpdateSiteData();
+                    return;
+                }
+
+                // If the URI didn't change, we don't need to do anything.
+                if (site.AbsoluteUri == value.AbsoluteUri)
+                {
+                    return;
+                }
+
+                // If the host *did* change, we can't consider this a valid adapter anymore.
+                if (site.Host != value.Host)
+                {
+                    throw new InvalidOperationException("Host has changed.");
+                }
+
+                // Otherwise, just update the site URI and data.
+                site = value;
+                UpdateSiteData();
+            }
+        }
+
+        /// <summary>
+        /// When the Site value changes, update the base site and thread name values appropriately.
+        /// </summary>
+        private void UpdateSiteData()
+        {
+            if (site == null)
+                throw new InvalidOperationException("Site value is null.");
+
+            Match m = siteRegex.Match(site.AbsoluteUri);
+            if (m.Success)
+            {
+                BaseSite = m.Groups["base"].Value;
+                ThreadName = m.Groups["thread"].Value;
+            }
+            else
+            {
+                throw new ArgumentException($"Invalid vBulletin3 site URL format:\n{site.AbsoluteUri}");
+            }
+        }
+
+        string BaseSite { get; set; }
+        string ThreadName { get; set; }
+
+        string ThreadBaseUrl => $"{BaseSite}viewtopic.php?t={ThreadName}";
+        string PostsBaseUrl => $"{ThreadBaseUrl}&p";
+        #endregion
+
+        #region Public interface
+        /// <summary>
+        /// Get the default number of posts per page for this forum type.
+        /// </summary>
+        public int DefaultPostsPerPage => 15;
+
+        /// <summary>
+        /// Generate a URL to access the specified page of the adapter's thread.
+        /// </summary>
+        /// <param name="page">The page of the thread that is being loaded.</param>
+        /// <returns>Returns a URL formatted to load the requested page of the thread.</returns>
+        public string GetUrlForPage(int page, int postsPerPage)
+        {
+            if (page < 1)
+                return ThreadBaseUrl;
+
+            int postIncrement = postsPerPage * (page - 1);
+
+            return $"{ThreadBaseUrl}&start={postIncrement}";
+        }
+
+        /// <summary>
+        /// Generate a URL to access the specified post of the adapter's thread.
+        /// </summary>
+        /// <param name="postId">The permalink ID of the post being requested.</param>
+        /// <returns>Returns a URL formatted to load the requested post.</returns>
+        public string GetPermalinkForId(string postId) => $"{PostsBaseUrl}{postId}";
+
+        /// <summary>
+        /// Get the starting post number, to begin tallying from.
+        /// Since vBulletin doesn't have threadmarks, this will always be whatever
+        /// is specified as the start post for the quest.
+        /// </summary>
+        /// <param name="quest">The quest being tallied.</param>
+        /// <param name="pageProvider">A page provider to allow loading the threadmark page.</param>
+        /// <param name="token">A cancellation token.</param>
+        /// <returns>Returns data indicating where to begin tallying the thread.</returns>
+        public Task<ThreadStartValue> GetStartingPostNumber(IQuest quest, IPageProvider pageProvider, CancellationToken token) =>
+            Task.FromResult(new ThreadStartValue(true, quest.StartPost));
+
+        /// <summary>
+        /// Get thread info from the provided page.
+        /// </summary>
+        /// <param name="page">A web page from a forum that this adapter can handle.</param>
+        /// <returns>Returns thread information that can be gleaned from that page.</returns>
+        public ThreadInfo GetThreadInfo(HtmlDocument page)
+        {
+            if (page == null)
+                throw new ArgumentNullException(nameof(page));
+
+            string title;
+            string author = string.Empty;
+            int pages = 1;
+
+            HtmlNode doc = page.DocumentNode.Element("html");
+
+            // Find the page title
+            title = PostText.CleanupWebString(doc.Element("head")?.Element("title")?.InnerText);
+
+            // Find the number of pages
+            var body = doc.Element("body");
+            var wrap = body?.Elements("div").FirstOrDefault(n => n.Id == "wrap");
+            var pagebody = wrap?.Elements("div").FirstOrDefault(n => n.Id == "page-body");
+
+            if (pagebody != null)
+            {
+                // Different versions of the forum have different methods of showing page numbers
+
+                var topicactions = pagebody.GetChildWithClass("topic-actions");
+                if (topicactions != null)
+                {
+                    var pagination = topicactions.GetChildWithClass("pagination");
+                    string paginationText = pagination?.InnerText;
+                    if (paginationText != null)
+                    {
+                        Regex pageOf = new Regex(@"Page\s*\d+\s*of\s*(?<pages>\d+)");
+                        Match m = pageOf.Match(paginationText);
+                        if (m.Success)
+                            pages = int.Parse(m.Groups["pages"].Value);
+                    }
+                }
+                else
+                {
+                    var actionbar = pagebody.GetChildWithClass("action-bar");
+                    var pagination = actionbar?.GetChildWithClass("pagination");
+
+                    var ul = pagination?.Element("ul");
+                    var lastPageLink = ul?.Elements("li")?.LastOrDefault(n => !n.GetAttributeValue("class", "").Split(' ').Contains("next"));
+
+                    if (lastPageLink != null)
+                    {
+                        pages = int.Parse(lastPageLink.InnerText);
+                    }
+                }
+            }
+
+            ThreadInfo info = new ThreadInfo(title, author, pages);
+
+            return info;
+        }
+
+        /// <summary>
+        /// Get a list of posts from the provided page.
+        /// </summary>
+        /// <param name="page">A web page from a forum that this adapter can handle.</param>
+        /// <returns>Returns a list of constructed posts from this page.</returns>
+        public IEnumerable<PostComponents> GetPosts(HtmlDocument page)
+        {
+            var body = page.DocumentNode.Element("html").Element("body");
+            var wrap = body?.Elements("div").FirstOrDefault(n => n.Id == "wrap");
+            var pagebody = wrap?.Elements("div").FirstOrDefault(n => n.Id == "page-body");
+
+            if (pagebody == null)
+                return new List<PostComponents>();
+
+            var posts = from p in pagebody.Elements("div")
+                        where p.GetAttributeValue("class", "").Split(' ').Contains("post")
+                        select GetPost(p);
+
+            return posts;
+        }
+        #endregion
+
+        #region Utility
+        /// <summary>
+        /// Get a completed post from the provided HTML div node.
+        /// </summary>
+        /// <param name="div">Div node that contains the post.</param>
+        /// <returns>Returns a post object with required information.</returns>
+        private PostComponents GetPost(HtmlNode div)
+        {
+            if (div == null)
+                throw new ArgumentNullException(nameof(div));
+
+            string author = "";
+            string id;
+            string text;
+            int number = 0;
+
+            // id="p12345"
+            id = div.Id.Substring(1);
+
+
+            var inner = div.GetChildWithClass("div", "inner");
+            var postbody = inner.GetChildWithClass("div", "postbody");
+            var authorNode = postbody.GetChildWithClass("p", "author");
+            var authorStrong = authorNode.Descendants("strong").FirstOrDefault();
+            var authorAnchor = authorStrong.Element("a");
+
+            author = PostText.CleanupWebString(authorAnchor.InnerText);
+
+            // No way to get the post number??
+
+
+            // Get the full post text.  Two different layout variants.
+            var content = postbody.GetChildWithClass("div", "content");
+            if (content == null)
+                content = postbody.Elements("div").FirstOrDefault(n => n.Id.StartsWith("post_content"));
+
+            text = PostText.ExtractPostText(content, n => false);
+
+
+            PostComponents post;
+            try
+            {
+                post = new PostComponents(author, id, text, number);
+            }
+            catch
+            {
+                post = null;
+            }
+
+            return post;
+        }
+        #endregion
+
+        #region Detection
+        /// <summary>
+        /// Static detection of whether the provided web page is a XenForo forum thread.
+        /// </summary>
+        /// <param name="page">Web page to examine.</param>
+        /// <returns>Returns true if it's detected as a XenForo page.  Otherwise, false.</returns>
+        public static bool CanHandlePage(HtmlDocument page)
+        {
+            if (page == null)
+                return false;
+
+            var body = page.DocumentNode.Element("html").Element("body");
+
+            // Not viable to tally without post numbers in posts
+
+            //if (body.Id == "phpbb")
+            //    return true;
+
+            return false;
+        }
+        #endregion
+
+    }
+}
