10851a0e7ff4259a8a7636c018022af5db6d4849
David Smith <dsmith@datasync.com>
2015-12-10 21:14:50 -0600

    Move the CustomStringComparer class into the Text.cs file, with the other text-related class/functions. Update comments and tidy.

--- TallyCore/Utility/Utility.cs
+++ TallyCore/Utility/Utility.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
 using System.Globalization;
 
 namespace NetTally.Utility
@@ -33,53 +32,6 @@ namespace NetTally.Utility
 
             return result;
         }
-    }
-
-    public class CustomStringComparer : IComparer, IEqualityComparer, IComparer<string>, IEqualityComparer<string>
-    {
-        private CompareInfo myComp;
-        private CompareOptions myOptions = CompareOptions.None;
-
-        // Constructs a comparer using the specified CompareOptions.
-        public CustomStringComparer(CompareInfo cmpi, CompareOptions options)
-        {
-            myComp = cmpi;
-            this.myOptions = options;
-        }
-
-        // Compares strings with the CompareOptions specified in the constructor.
-        public int Compare(string x, string y)
-        {
-            if (x == y) return 0;
-            if (x == null) return -1;
-            if (y == null) return 1;
-
-            return myComp.Compare(x, y, myOptions);
-        }
-
-        public bool Equals(string x, string y) => this.Compare(x, y) == 0;
-
-        public int GetHashCode(string obj) => obj.GetHashCode();
-
-
-        // Compares strings with the CompareOptions specified in the constructor.
-        int IComparer.Compare(object x, object y)
-        {
-            if (x == y) return 0;
-            if (x == null) return -1;
-            if (y == null) return 1;
-
-            String sx = x as String;
-            String sy = y as String;
-            if (sx != null && sy != null)
-                return myComp.Compare(sx, sy, myOptions);
-            throw new ArgumentException("x and y should be strings.");
-        }
-
-        bool IEqualityComparer.Equals(object x, object y) => ((IComparer)this).Compare(x, y) == 0;
-
-        int IEqualityComparer.GetHashCode(object obj) => (obj as string)?.GetHashCode() ?? 0;
-
     }
     
 }
