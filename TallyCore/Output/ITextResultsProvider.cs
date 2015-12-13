056e4a4eed2bce9a3afe316e2442e51cfce39d4e
David Smith <dsmith@datasync.com>
2015-12-13 00:48:03 -0600

    Add comments for the interface function.

--- TallyCore/Output/ITextResultsProvider.cs
+++ TallyCore/Output/ITextResultsProvider.cs
@@ -8,6 +8,13 @@ namespace NetTally
 {
     public interface ITextResultsProvider
     {
+        /// <summary>
+        /// Public function to generate the full output for the tally.
+        /// </summary>
+        /// <param name="quest">The quest being tallied.</param>
+        /// <param name="voteCounter">The vote counter holding the tally results.</param>
+        /// <param name="displayMode">The mode requested for how to format the output.</param>
+        /// <returns>Returns the full string to be displayed.</returns>
         string BuildOutput(IQuest quest, IVoteCounter voteCounter, DisplayMode displayMode);
     }
 }
