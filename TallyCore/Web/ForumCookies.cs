e5ebc3b1cb714dafa6eae9ef6a0e37d98081f736
David Smith <dsmith@datasync.com>
2015-12-13 17:38:23 -0600

    Tidy, comments.

--- TallyCore/Web/ForumCookies.cs
+++ TallyCore/Web/ForumCookies.cs
@@ -30,11 +30,17 @@ namespace NetTally
             return cookies;
         }
 
+        /// <summary>
+        /// Get a collection of all the cookies we know about.  These can all be stored
+        /// in the HttpClientManager, and it will use whichever is needed.
+        /// </summary>
+        /// <returns>Returns the collection of all known cookies.</returns>
         public static CookieCollection GetAllCookies()
         {
             CookieCollection cookies = new CookieCollection();
 
             Cookie cookie;
+
             // Cookie for vote tally account on QQ, to allow reading the NSFW forums.
             cookie = new Cookie("xf_user", "2940%2C3f6f04f8921e0b26f3cd6c6399af3a04d3520769", "/", "forum.questionablequesting.com");
             cookie.Expires = DateTime.Now + TimeSpan.FromDays(30);
