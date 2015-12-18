0c439a1fd9c518bea83258fb708d3f8549a3191c
David Smith <dsmith@datasync.com>
2015-12-18 00:31:58 -0600

    Rebuild of VoteNode. Add a function that can extract the string representation based on the display mode (compact modes only). Don't try to pre-define the final string. AddChild now is done by taking the parameters, not a node.  We handle the construction so that we can attach as a parent.

--- TallyCore/Output/Utility/VoteNode.cs
+++ TallyCore/Output/Utility/VoteNode.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace NetTally.Output
 {
@@ -11,19 +12,30 @@ namespace NetTally.Output
     /// </summary>
     public class VoteNode
     {
+        readonly VoteNode Parent;
+
         public string Text { get; set; }
-        public string Line { get; set; }
-        public HashSet<string> Voters { get; } = new HashSet<string>();
+
         public List<VoteNode> Children { get; } = new List<VoteNode>();
 
-        public int VoterCount => Voters.Count(v => !v.StartsWith(Utility.Text.PlanNameMarker, StringComparison.Ordinal));
+        public HashSet<string> Voters { get; } = new HashSet<string>();
+        public HashSet<string> AllVoters { get; } = new HashSet<string>();
+
+        public int VoterCount => AllVoters.Count(v => !v.StartsWith(Utility.Text.PlanNameMarker, StringComparison.Ordinal));
+
+        bool HasParent => Parent != null;
+        bool HasChildren => Children.Count > 0;
 
         public VoteNode(string text, HashSet<string> voters)
+            : this(text, voters, null)
         {
-            Text = text;
+        }
 
+        private VoteNode(string text, HashSet<string> voters, VoteNode parent)
+        {
+            Parent = parent;
+            Text = text;
             AddVoters(voters);
-            SetLine();
         }
 
         /// <summary>
@@ -33,68 +45,116 @@ namespace NetTally.Output
         public void AddVoters(HashSet<string> voters)
         {
             if (voters != null)
+            {
                 Voters.UnionWith(voters);
+                AllVoters.UnionWith(voters);
+            }
         }
 
         /// <summary>
         /// Add a child node to the current node.
         /// Update this node's voter list to include any new voters from the child.
         /// </summary>
-        /// <param name="node">New child vote node.</param>
-        public void AddChild(VoteNode node)
+        /// <param name="text">Text of the child node.</param>
+        /// <param name="voters">Voters for the child node.</param>
+        public void AddChild(string text, HashSet<string> voters)
         {
-            Voters.UnionWith(node.Voters);
-            Children.Add(node);
-            SetLine();
+            VoteNode child = new VoteNode(text, voters, this);
+            Children.Add(child);
+            AllVoters.UnionWith(child.Voters);
         }
 
         /// <summary>
-        /// Update the Line property when the voter list changes, so that it
-        /// always points at the first voter (for those votes that will use
-        /// the compact reference format).
+        /// Function to get a line for one of the compact mode displays,
+        /// based on whether this is a parent or child node.
         /// </summary>
-        private void SetLine()
+        /// <param name="displayMode">The display mode currently being used.</param>
+        /// <returns>Returns a string representation of the current vote node.</returns>
+        public string GetLine(DisplayMode displayMode)
         {
             var lines = Utility.Text.GetStringLines(Text);
+            if (lines.Count == 0)
+                return string.Empty;
 
-            if (lines.Count == 1)
+            string results = string.Empty;
+
+            string planname = VoteString.GetPlanName(lines[0]);
+            bool isPlan = planname != null && VoteCounter.Instance.HasPlan(planname);
+
+
+            if (HasChildren || (!HasParent && lines.Count == 1))
             {
-                Line = lines.First();
+                // Parent node, or solitary node with 1 line.
+                results = VoteString.ModifyVoteLine(lines[0], marker: VoterCount.ToString());
             }
-            else
+            else if (HasParent && lines.Count == 1)
             {
-                string planname = VoteString.GetPlanName(lines[0]);
-                bool isPlan = planname != null && VoteCounter.Instance.HasPlan(planname);
-                Line = GetCondensedLine(isPlan);
+                // Child node with 1 line
+                if (displayMode == DisplayMode.Compact)
+                {
+                    results = VoteString.ModifyVoteLine(lines[0], prefix: "", marker: VoterCount.ToString());
+                }
+                else
+                {
+                    results = VoteString.ModifyVoteLine(lines[0], prefix: "-", marker: VoterCount.ToString());
+                }
             }
-        }
+            else if (!HasChildren)
+            {
+                // Other nodes without children (typically child nodes).
 
-        /// <summary>
-        /// Create the text to be used in the Line property, as a condensed
-        /// link format to the original voter.
-        /// </summary>
-        /// <returns>Returns the condensed line.</returns>
-        private string GetCondensedLine(bool isPlan)
-        {
-            StringBuilder sb = new StringBuilder();
+                StringBuilder sb = new StringBuilder();
 
-            if (!isPlan)
-                sb.Append("-");
+                if (HasParent && displayMode == DisplayMode.CompactNoVoters)
+                    sb.Append("-");
 
-            sb.Append("[X]");
+                sb.Append("[");
+                sb.Append(VoterCount);
+                sb.Append("]");
 
-            // Don't need to list any task, because it's guaranteed to be listed in the
-            // parent line that this is a child of.
+                // Only explicitly add tasks to parent nodes
+                if (!HasParent)
+                {
+                    string task = VoteString.GetVoteTask(lines[0]);
+                    if (task != string.Empty)
+                        sb.Append($"[{task}]");
+                }
 
-            string firstVoter = VoteInfo.GetFirstVoter(Voters);
-            string link;
 
-            VoteType voteType = Utility.Text.IsPlanName(firstVoter) ? VoteType.Plan : VoteType.Vote;
-            link = VoteInfo.GetVoterUrl(firstVoter, voteType);
+                sb.Append(" Plan: ");
+                string firstVoter = VoteInfo.GetFirstVoter(Voters);
+                sb.Append(firstVoter);
 
-            sb.Append($" Plan: {firstVoter} — {link}");
+                // Only add the link if we're not showing the voters
+                if (displayMode == DisplayMode.CompactNoVoters)
+                {
+                    string link;
 
-            return sb.ToString();
+                    VoteType voteType = Utility.Text.IsPlanName(firstVoter) ? VoteType.Plan : VoteType.Vote;
+                    link = VoteInfo.GetVoterUrl(firstVoter, voteType);
+
+                    sb.Append(" — ");
+                    sb.Append(link);
+                }
+
+                results = sb.ToString();
+            }
+
+            // Child nodes in compact mode will be put in spoilers.
+            // Remove BBCode and change the [10] count to (10).
+            if (HasParent && displayMode == DisplayMode.Compact)
+            {
+                string cleanString = VoteString.RemoveBBCode(results);
+
+                Regex bracketRegex = new Regex(@"\[(\d+)\]");
+                Match m = bracketRegex.Match(cleanString);
+                if (m.Success)
+                {
+                    results = bracketRegex.Replace(cleanString, $"({m.Groups[1].Value})", 1);
+                }
+            }
+
+            return results;
         }
     }
 }
