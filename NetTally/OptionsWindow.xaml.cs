3b0e6083cd88477bff411ee9f7550fbaf7a0f333
David Smith <dsmith@datasync.com>
2015-12-18 13:26:56 -0600

    Cleanup. Set DataContext. Set Binding. Add functionality for close and reset.

--- NetTally/OptionsWindow.xaml.cs
+++ NetTally/OptionsWindow.xaml.cs
@@ -1,16 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace NetTally
 {
@@ -22,11 +10,19 @@ namespace NetTally
         public OptionsWindow()
         {
             InitializeComponent();
+            DataContext = AdvancedOptions.Instance;
         }
 
-        private void button_Click(object sender, RoutedEventArgs e)
+        private void closeButton_Click(object sender, RoutedEventArgs e)
         {
+            Close();
+        }
 
+        private void resetAllButton_Click(object sender, RoutedEventArgs e)
+        {
+            allowRankedVotes.IsChecked = true;
+            ignoreSymbols.IsChecked = true;
+            trimExtendedText.IsChecked = false;
         }
     }
 }
