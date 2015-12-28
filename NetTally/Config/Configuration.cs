703a9ac9b5636e4f5a45119b905f1a2cf1f48793
David Smith <dsmith@datasync.com>
2015-12-28 17:57:18 -0600

    Put old configuration properties back in place, so that it won't generate an exception when trying to read old config files.  These properties are not saved.

--- NetTally/Config/Configuration.cs
+++ NetTally/Config/Configuration.cs
@@ -406,6 +406,26 @@ namespace NetTally
             get { return (string)this["CustomThreadmarkFilters"]; }
             set { this["CustomThreadmarkFilters"] = value; }
         }
+
+
+        // Obsolete configuration properties.  Left in place solely so that if they
+        // exist, they don't cause load errors.  We do not save the results when
+        // saving a new config instance.
+
+        [ConfigurationProperty("UseVotePartitions", DefaultValue = false)]
+        public bool UseVotePartitions
+        {
+            get { return (bool)this["UseVotePartitions"]; }
+            set { this["UseVotePartitions"] = value; }
+        }
+
+        [ConfigurationProperty("PartitionByLine", DefaultValue = true)]
+        public bool PartitionByLine
+        {
+            get { return (bool)this["PartitionByLine"]; }
+            set { this["PartitionByLine"] = value; }
+        }
+
     }
 
 }
