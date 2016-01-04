5cb9e7e7ac3a97840b83bbf77ed5d6f171fcc431
David Smith <dsmith@datasync.com>
2016-01-04 01:53:17 -0600

    Update copyright year.

--- NetTally/Properties/AssemblyInfo.cs
+++ NetTally/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Wayward Gamers")]
 [assembly: AssemblyProduct("NetTally")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright ©  2015-2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
