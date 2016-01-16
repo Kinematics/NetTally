e9b4d0d4d1d204dc12ffe19e1ec921f7ed79ea7d
David Smith <dsmith@datasync.com>
2016-01-16 15:10:39 -0600

    Remove Settings value of Upgraded.

--- NetTally/Config/Configuration.cs
+++ NetTally/Config/Configuration.cs
@@ -2,7 +2,6 @@
 using System.Configuration;
 using System.IO;
 using System.Linq;
-using System.Text.RegularExpressions;
 using System.Windows;
 
 namespace NetTally
--- NetTally/Properties/Settings.Designer.cs
+++ NetTally/Properties/Settings.Designer.cs
@@ -22,17 +22,5 @@ namespace NetTally.Properties {
                 return defaultInstance;
             }
         }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
-        public bool Upgraded {
-            get {
-                return ((bool)(this["Upgraded"]));
-            }
-            set {
-                this["Upgraded"] = value;
-            }
-        }
     }
 }
