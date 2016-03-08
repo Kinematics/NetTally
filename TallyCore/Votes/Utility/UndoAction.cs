173746aa1b8718f4e468c22eac5ffdc3e08e8275
David Smith <dsmith@datasync.com>
2016-03-08 13:42:45 -0600

    Create a storage class for holding the info needed to undo vote merge actions.

--- TallyCore/Votes/Utility/UndoAction.cs
+++ TallyCore/Votes/Utility/UndoAction.cs
@@ -8,5 +8,26 @@ namespace NetTally.Votes
 {
     public class UndoAction
     {
+        public VoteType VoteType { get; }
+        public string FromVote { get; }
+        public string ToVote { get; }
+        public string DeletedVote { get; }
+        HashSet<string> FromVoters { get; }
+        HashSet<string> ToVoters { get; }
+        HashSet<string> DeletedVoters { get; }
+
+        public UndoAction(VoteType voteType, string fromVote = null, string toVote = null, string deletedVote = null,
+            HashSet<string> fromVoters = null, HashSet<string> toVoters = null, HashSet<string> deletedVoters = null)
+        {
+            VoteType = voteType;
+
+            FromVote = fromVote;
+            ToVote = toVote;
+            DeletedVote = deletedVote;
+
+            FromVoters = new HashSet<string>(fromVoters ?? Enumerable.Empty<string>());
+            ToVoters = new HashSet<string>(toVoters ?? Enumerable.Empty<string>());
+            DeletedVoters = new HashSet<string>(deletedVoters ?? Enumerable.Empty<string>());
+        }
     }
 }
