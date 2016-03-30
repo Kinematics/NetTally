998fee97ecd99d4a2da2da1c92fed5de97608f46
David Smith <dsmith@datasync.com>
2016-03-30 16:32:42 -0500

    Revise advanced options so that the default of all formatting options is false. Mark old versions as obsolete.

--- TallyCore/Global/AdvancedOptions.cs
+++ TallyCore/Global/AdvancedOptions.cs
@@ -36,15 +36,23 @@ namespace NetTally
         DisplayMode displayMode = DisplayMode.Normal;
 
         bool allowRankedVotes = true;
+
+        [Obsolete("Invert usage")]
+        bool allowVoteLabelPlanNames = true;
+        bool forbidVoteLabelPlanNames = false;
+        [Obsolete("Invert usage")]
         bool ignoreSymbols = true;
+        bool whitespaceAndPunctuationIsSignificant = false;
+        bool disableProxyVotes = false;
         bool ignoreSpoilers = false;
         bool trimExtendedText = false;
+
         bool globalSpoilers = false;
-        bool allowVoteLabelPlanNames = true;
-        bool disableProxyVotes = false;
 
         bool debugMode = false;
+        #endregion
 
+        #region General Options
         /// <summary>
         /// Whether or not to parse ranked votes in a tally.
         /// </summary>
@@ -57,11 +65,41 @@ namespace NetTally
                 OnPropertyChanged();
             }
         }
+        #endregion
+
+        #region Formatting Options
+        /// <summary>
+        /// Flag whether to allow label lines on votes to be plan names.
+        /// </summary>
+        [Obsolete("Invert usage")]
+        public bool AllowVoteLabelPlanNames
+        {
+            get { return allowVoteLabelPlanNames; }
+            set
+            {
+                allowVoteLabelPlanNames = value;
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// Flag whether to allow label lines on votes to be plan names.
+        /// </summary>
+        public bool ForbidVoteLabelPlanNames
+        {
+            get { return forbidVoteLabelPlanNames; }
+            set
+            {
+                forbidVoteLabelPlanNames = value;
+                OnPropertyChanged();
+            }
+        }
 
         /// <summary>
         /// Whether or not to ignore whitespace and symbols when
         /// doing vote and voter comparisons.
         /// </summary>
+        [Obsolete("Invert usage")]
         public bool IgnoreSymbols
         {
             get { return ignoreSymbols; }
@@ -73,28 +111,28 @@ namespace NetTally
         }
 
         /// <summary>
-        /// Whether or not to trim extended text from vote lines.
+        /// Whether or not whitespace and punctuation is considered significant when
+        /// doing vote and voter comparisons.
         /// </summary>
-        public bool TrimExtendedText
+        public bool WhitespaceAndPunctuationIsSignificant
         {
-            get { return trimExtendedText; }
+            get { return whitespaceAndPunctuationIsSignificant; }
             set
             {
-                trimExtendedText = value;
+                whitespaceAndPunctuationIsSignificant = value;
                 OnPropertyChanged();
             }
         }
 
         /// <summary>
-        /// Enum of the type of display composition methodology to use for the output display.
-        /// Recalculates the display if changed.
+        /// Flag whether to disable proxy votes (voting for another user to import their vote to your own).
         /// </summary>
-        public DisplayMode DisplayMode
+        public bool DisableProxyVotes
         {
-            get { return displayMode; }
+            get { return disableProxyVotes; }
             set
             {
-                displayMode = value;
+                disableProxyVotes = value;
                 OnPropertyChanged();
             }
         }
@@ -113,6 +151,35 @@ namespace NetTally
         }
 
         /// <summary>
+        /// Whether or not to trim extended text from vote lines.
+        /// </summary>
+        public bool TrimExtendedText
+        {
+            get { return trimExtendedText; }
+            set
+            {
+                trimExtendedText = value;
+                OnPropertyChanged();
+            }
+        }
+        #endregion
+
+        #region Output Options
+        /// <summary>
+        /// Enum of the type of display composition methodology to use for the output display.
+        /// Recalculates the display if changed.
+        /// </summary>
+        public DisplayMode DisplayMode
+        {
+            get { return displayMode; }
+            set
+            {
+                displayMode = value;
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
         /// Flag whether to always put spoiler tags around all forms of display output.
         /// </summary>
         public bool GlobalSpoilers
@@ -124,33 +191,9 @@ namespace NetTally
                 OnPropertyChanged();
             }
         }
+        #endregion
 
-        /// <summary>
-        /// Flag whether to allow label lines on votes to be plan names.
-        /// </summary>
-        public bool AllowVoteLabelPlanNames
-        {
-            get { return allowVoteLabelPlanNames; }
-            set
-            {
-                allowVoteLabelPlanNames = value;
-                OnPropertyChanged();
-            }
-        }
-
-        /// <summary>
-        /// Flag whether to disable proxy votes (voting for another user to import their vote to your own).
-        /// </summary>
-        public bool DisableProxyVotes
-        {
-            get { return disableProxyVotes; }
-            set
-            {
-                disableProxyVotes = value;
-                OnPropertyChanged();
-            }
-        }
-
+        #region Miscellaneous Options
         /// <summary>
         /// Whether the program should be running in its debug mode.
         /// </summary>
