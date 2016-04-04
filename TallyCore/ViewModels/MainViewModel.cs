a6a984a236157320d002c4b4cb5ebb873f73663a
David Smith <dsmith@datasync.com>
2016-04-03 20:36:14 -0500

    Add starting view model.

---
+++ TallyCore/ViewModels/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NetTally;
+using NetTally.Utility;
+
+namespace NetTally.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public ObservableCollectionExt<string> VoteCollection { get; }
+        public ObservableCollectionExt<string> VoterCollection { get; }
+        public QuestCollection QuestCollection { get; }
+
+        public MainViewModel(QuestCollectionWrapper config)
+        {
+
+        }
+    }
+}
---
+++ TallyCore/ViewModels/ViewModelBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetTally.ViewModels
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        #region Implement INotifyPropertyChanged interface
+        /// <summary>
+        /// Event for INotifyPropertyChanged.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Function to raise events when a property has been changed.
+        /// </summary>
+        /// <param name="propertyName">The name of the property that was modified.</param>
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
