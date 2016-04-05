e91846c3b7091f02928aa53378064264a7b6417c
David Smith <dsmith@datasync.com>
2016-04-05 16:46:13 -0500

    Add equality overrides to the struct.

--- TallyCore/Web/Cache/CachedPage.cs
+++ TallyCore/Web/Cache/CachedPage.cs
@@ -38,5 +38,44 @@ namespace NetTally.Web
             DocString = html;
             Doc = null;
         }
+
+        #region Equality overrides
+        public override int GetHashCode()
+        {
+            if (!string.IsNullOrEmpty(DocString))
+            {
+                return DocString.GetHashCode();
+            }
+            if (Doc != null)
+            {
+                return Doc.GetHashCode();
+            }
+
+            return Timestamp.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is CachedPage))
+                return false;
+
+            return Equals((CachedPage)obj);
+        }
+
+        public bool Equals(CachedPage other)
+        {
+            if (!string.IsNullOrEmpty(DocString) && DocString == other.DocString)
+                return true;
+
+            if (Doc != null && Doc == other.Doc)
+                return true;
+
+            return false;
+        }
+
+        public static bool operator ==(CachedPage page1, CachedPage page2) => page1.Equals(page2);
+
+        public static bool operator !=(CachedPage page1, CachedPage page2) => !page1.Equals(page2);
+        #endregion
     }
 }
