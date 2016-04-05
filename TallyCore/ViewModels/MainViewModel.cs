e9ca67a4b8a8a4133bf522cecbcb2d7d2b0521cc
David Smith <dsmith@datasync.com>
2016-04-04 21:24:50 -0500

    Implement the main view model. Currently handles everything for the main and option windows, but not the vote management window.

--- TallyCore/ViewModels/MainViewModel.cs
+++ TallyCore/ViewModels/MainViewModel.cs
@@ -1,22 +1,286 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+
 using NetTally;
 using NetTally.Utility;
 
 namespace NetTally.ViewModels
 {
-    public class MainViewModel : ViewModelBase
+    public class MainViewModel : ViewModelBase, IDisposable
     {
-        public ObservableCollectionExt<string> VoteCollection { get; }
-        public ObservableCollectionExt<string> VoterCollection { get; }
-        public QuestCollection QuestCollection { get; }
-
         public MainViewModel(QuestCollectionWrapper config)
         {
+            if (config == null)
+                throw new ArgumentNullException(nameof(config));
 
+            QuestList = config.QuestCollection;
+            QuestList.Sort();
+            SelectedQuest = QuestList[config.CurrentQuest];
+
+            BindCheckForNewRelease();
+            BindTally();
         }
+
+        #region IDisposable
+        bool _disposed = false;
+
+        ~MainViewModel()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true); //I am calling you from Dispose, it's safe
+            GC.SuppressFinalize(this); //Hey, GC: don't bother calling finalize later
+        }
+
+        protected virtual void Dispose(bool itIsSafeToAlsoFreeManagedObjects)
+        {
+            if (_disposed)
+                return;
+
+            if (itIsSafeToAlsoFreeManagedObjects)
+            {
+                tally.Dispose();
+            }
+
+            _disposed = true;
+        }
+        #endregion
+
+        #region Section: Check for New Release
+        /// Fields for this section
+        CheckForNewRelease checkForNewRelease = new CheckForNewRelease();
+
+        /// <summary>
+        /// Bind event watcher to the class that checks for new releases of the program.
+        /// </summary>
+        private void BindCheckForNewRelease()
+        {
+            checkForNewRelease.PropertyChanged += CheckForNewRelease_PropertyChanged;
+            checkForNewRelease.Update();
+        }
+
+        /// <summary>
+        /// Handles the PropertyChanged event of the CheckForNewRelease control.
+        /// </summary>
+        private void CheckForNewRelease_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == "NewRelease")
+            {
+                OnPropertyChanged("NewReleaseAvailable");
+            }
+        }
+
+        /// <summary>
+        /// Flag indicating whether there is a newer release of the program available.
+        /// </summary>
+        public bool NewReleaseAvailable => checkForNewRelease.NewRelease;
+        #endregion
+
+        #region Section: Options        
+        /// <summary>
+        /// Gets the user-readable list of display modes, for use in the view.
+        /// </summary>
+        public List<string> DisplayModes { get; } = Enumerations.EnumDescriptionsList<DisplayMode>().ToList();
+
+        /// <summary>
+        /// Gets the user-readable list of partition modes, for use in the view.
+        /// </summary>
+        public List<string> PartitionModes { get; } = Enumerations.EnumDescriptionsList<PartitionMode>().ToList();
+
+        /// <summary>
+        /// Public link to the advanced options instance, for data binding.
+        /// </summary>
+        public AdvancedOptions Options => AdvancedOptions.Instance;
+        #endregion
+
+        #region Section: Quest collection
+        /// <summary>
+        /// List of quests for binding.
+        /// </summary>
+        public QuestCollection QuestList { get; set; }
+
+        /// <summary>
+        /// The currently selected quest.
+        /// </summary>
+        IQuest selectedQuest;
+        public IQuest SelectedQuest
+        {
+            get { return selectedQuest; }
+            set
+            {
+                UnbindQuest(selectedQuest);
+                selectedQuest = value;
+                BindQuest(selectedQuest);
+                OnPropertyChanged();
+            }
+        }
+
+        private void BindQuest(IQuest quest)
+        {
+            if (quest != null)
+            {
+                quest.PropertyChanged += SelectedQuest_PropertyChanged;
+            }
+        }
+
+        private void UnbindQuest(IQuest quest)
+        {
+            if (quest != null)
+            {
+                quest.PropertyChanged -= SelectedQuest_PropertyChanged;
+            }
+        }
+
+        private void SelectedQuest_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == "DisplayName")
+            {
+                QuestList.Sort();
+            }
+        }
+
+        public bool AddNewQuest()
+        {
+            IQuest newEntry = QuestList.AddNewQuest();
+
+            if (newEntry == null)
+                return false;
+
+            SelectedQuest = newEntry;
+
+            return true;
+        }
+
+        public bool RemoveQuest()
+        {
+            if (SelectedQuest == null)
+                return false;
+
+            int index = QuestList.IndexOf(SelectedQuest);
+            QuestList.Remove(SelectedQuest);
+
+            if (index >= QuestList.Count)
+                index = QuestList.Count - 1;
+
+            if (index < 0)
+                SelectedQuest = null;
+            else
+                SelectedQuest = QuestList[index];
+
+            return true;
+        }
+        #endregion
+
+        #region Section: Tally & Results Binding
+        /// Tally class object.
+        Tally tally = new Tally();
+
+        /// <summary>
+        /// Bind event watcher to the class that handles running the tallies.
+        /// </summary>
+        private void BindTally()
+        {
+            tally.PropertyChanged += Tally_PropertyChanged;
+        }
+
+        /// <summary>
+        /// Handles the PropertyChanged event of the Tally control.
+        /// </summary>
+        private void Tally_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == "TallyIsRunning")
+            {
+                OnPropertyChanged("TallyIsRunning");
+            }
+            else if (e.PropertyName == "TallyResults")
+            {
+                OnPropertyChanged("Output");
+            }
+        }
+
+        /// <summary>
+        /// Flag whether the tally is currently running.
+        /// </summary>
+        public bool TallyIsRunning => tally.TallyIsRunning;
+
+        /// <summary>
+        /// The string containing the current tally progress or results.
+        /// Creates a notification event if the contents change.
+        /// </summary>
+        public string Output => tally.TallyResults;
+        #endregion
+
+        #region Section: Tally Commands
+        CancellationTokenSource cts;
+
+        /// <summary>
+        /// Runs a tally on the currently selected quest.
+        /// </summary>
+        /// <returns></returns>
+        public async Task Tally()
+        {
+            try
+            {
+                using (cts = new CancellationTokenSource())
+                {
+                    try
+                    {
+                        await tally.Run(SelectedQuest, cts.Token);
+                    }
+                    catch (OperationCanceledException)
+                    {
+                        // Got a cancel request somewhere.  No special handling needed.
+                    }
+                    catch (Exception)
+                    {
+                        // Re-throw exceptions if we didn't request a cancellation.
+                        if (!cts.IsCancellationRequested)
+                        {
+                            // If an exception happened and we haven't already requested a cancellation, do so.
+                            cts.Cancel();
+                            throw;
+                        }
+                    }
+                }
+            }
+            finally
+            {
+                cts = null;
+            }
+        }
+
+        /// <summary>
+        /// Cancels the currently running tally, if any.
+        /// </summary>
+        public void Cancel()
+        {
+            cts?.Cancel();
+        }
+
+        /// <summary>
+        /// Requests that the tally class clear its cache.
+        /// </summary>
+        public void ClearCache()
+        {
+            tally.ClearPageCache();
+        }
+
+        /// <summary>
+        /// Requests that the tally class update its current results.
+        /// </summary>
+        public void Update()
+        {
+            tally.UpdateResults();
+        }
+
+        #endregion
     }
 }
