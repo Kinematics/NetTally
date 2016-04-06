2bf12e310e24f97487f530bd903f061a0c09918f
David Smith <dsmith@datasync.com>
2016-04-06 15:30:17 -0500

    Expand the base view model to include a generic SetProperty function, and properly handle synchronization when raising events.

--- TallyCore/ViewModels/ViewModelBase.cs
+++ TallyCore/ViewModels/ViewModelBase.cs
@@ -4,13 +4,19 @@ using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace NetTally.ViewModels
 {
     public class ViewModelBase : INotifyPropertyChanged
     {
-        #region Implement INotifyPropertyChanged interface
+        /// <summary>
+        /// The thread context that this class was originally created in, so that we
+        /// can send notifications back on that same thread.
+        /// </summary>
+        SynchronizationContext _synchronizationContext = SynchronizationContext.Current;
+
         /// <summary>
         /// Event for INotifyPropertyChanged.
         /// </summary>
@@ -18,12 +24,52 @@ namespace NetTally.ViewModels
 
         /// <summary>
         /// Function to raise events when a property has been changed.
+        /// Make sure we're in the proper synchronization context before sending.
         /// </summary>
         /// <param name="propertyName">The name of the property that was modified.</param>
         protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            PropertyChangedEventArgs e = new PropertyChangedEventArgs(propertyName);
+
+            if (SynchronizationContext.Current == _synchronizationContext)
+            {
+                // Execute the PropertyChanged event on the current thread
+                RaisePropertyChanged(e);
+            }
+            else
+            {
+                // Raises the PropertyChanged event on the creator thread
+                _synchronizationContext.Send(RaisePropertyChanged, e);
+            }
         }
-        #endregion
+
+        /// <summary>
+        /// Function to actually invoke the delegate, after synchronization is checked.
+        /// </summary>
+        /// <param name="param">The parameter.</param>
+        private void RaisePropertyChanged(object param)
+        {
+            // We are in the creator thread, call the base implementation directly
+            PropertyChanged?.Invoke(this, (PropertyChangedEventArgs)param);
+        }
+
+        /// <summary>
+        /// Generic handling to set property values and raise the property changed event.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="storage">The storage.</param>
+        /// <param name="value">The value.</param>
+        /// <param name="propertyName">Name of the property.</param>
+        /// <returns></returns>
+        protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (Object.Equals(storage, value))
+                return false;
+
+            storage = value;
+            OnPropertyChanged(propertyName);
+
+            return true;
+        }
     }
 }
