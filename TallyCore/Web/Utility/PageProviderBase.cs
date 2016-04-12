1fb15e3cb76ba670fc32d687e2650a3b161b2532
David Smith <dsmith@datasync.com>
2016-04-11 22:58:21 -0500

    Refactor the page provider class to create a base abstract class to derive other classes from. Rebuild as WebPageProvider3.

---
+++ TallyCore/Web/Utility/PageProviderBase.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using NetTally.Utility;
+
+namespace NetTally.Web
+{
+    public enum PageRequestStatusType
+    {
+        None,
+        Requested,
+        LoadedFromCache,
+        Loaded,
+        Retry,
+        Error,
+        Failed,
+        Cancelled,
+    }
+
+    public abstract class PageProviderBase : IDisposable, IPageProvider
+    {
+        #region Fields
+        // Maximum number of simultaneous connections allowed, to guard against hammering the server.
+        // Setting it to 5 or higher causes it to hang for several seconds on the last page when
+        // loading SB and SV pages.
+        protected const int maxSimultaneousConnections = 4;
+        protected readonly SemaphoreSlim ss = new SemaphoreSlim(maxSimultaneousConnections);
+
+        protected HttpClientHandler ClientHandler { get; }
+
+        protected WebCache Cache { get; } = WebCache.Instance;
+
+        protected string UserAgent { get; } = $"{ProductInfo.Name} ({ProductInfo.Version})";
+
+        protected IClock Clock { get; }
+        #endregion
+
+        #region Constructors
+        protected PageProviderBase()
+            : this(null, null, null)
+        {
+        }
+
+        protected PageProviderBase(HttpClientHandler handler, WebCache cache, IClock clock)
+        {
+            ClientHandler = handler ?? new HttpClientHandler();
+            Cache = cache ?? WebCache.Instance;
+            Clock = clock ?? new DefaultClock();
+        }
+        #endregion
+
+        #region Disposal
+        protected bool _disposed = false;
+
+        ~PageProviderBase()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true); //I am calling you from Dispose, it's safe
+            GC.SuppressFinalize(this); //Hey, GC: don't bother calling finalize later
+        }
+
+        protected virtual void Dispose(bool itIsSafeToAlsoFreeManagedObjects)
+        {
+            if (_disposed)
+                return;
+
+            if (itIsSafeToAlsoFreeManagedObjects)
+            {
+                ClientHandler?.Dispose();
+                ss.Dispose();
+            }
+
+            _disposed = true;
+        }
+        #endregion
+
+        #region Event handlers
+        public event EventHandler<MessageEventArgs> StatusChanged;
+
+        /// <summary>
+        /// Function to raise events when page load status has been updated.
+        /// </summary>
+        /// <param name="message">The message to send to any listeners.</param>
+        protected void OnStatusChanged(string message)
+        {
+            if (string.IsNullOrEmpty(message))
+                return;
+
+            StatusChanged?.Invoke(this, new MessageEventArgs(message));
+        }
+        #endregion
+
+        #region Notification functions
+
+        protected void NotifyStatusChange(PageRequestStatusType status, string url, string shortDescrip, Exception e, bool suppress)
+        {
+            if (status == PageRequestStatusType.Requested)
+            {
+                NotifyRequest(url, suppress);
+                return;
+            }
+
+            if (status == PageRequestStatusType.Cancelled)
+            {
+                NotifyCancel();
+                return;
+            }
+
+            if (status == PageRequestStatusType.Error)
+            {
+                NotifyError(shortDescrip, e);
+                return;
+            }
+
+            NotifyResult(status, shortDescrip, suppress);
+        }
+
+
+        /// <summary>
+        /// Send status update when requesting a page URL.
+        /// </summary>
+        /// <param name="url">The URL.</param>
+        /// <param name="suppress">if set to <c>true</c> [suppress].</param>
+        private void NotifyRequest(string url, bool suppress)
+        {
+            if (!suppress && !string.IsNullOrEmpty(url))
+            {
+                OnStatusChanged($"{url}\n");
+            }
+        }
+
+        /// <summary>
+        /// Sends a status update that the tally was cancelled.
+        /// </summary>
+        private void NotifyCancel()
+        {
+            OnStatusChanged($"Tally cancelled!\n");
+        }
+
+        /// <summary>
+        /// Sends a status update indicating that there was an error.
+        /// </summary>
+        /// <param name="shortDescrip">The short descrip.</param>
+        /// <param name="e">The e.</param>
+        private void NotifyError(string shortDescrip, Exception e)
+        {
+            if (string.IsNullOrEmpty(shortDescrip))
+                return;
+
+            OnStatusChanged($"{shortDescrip}: {e?.Message ?? "(unknown error)"}");
+        }
+
+        /// <summary>
+        /// Sends a status update for a load attempt result.
+        /// </summary>
+        /// <param name="status">The status.</param>
+        /// <param name="shortDescrip">The short descrip.</param>
+        /// <param name="suppress">if set to <c>true</c> [suppress].</param>
+        private void NotifyResult(PageRequestStatusType status, string shortDescrip, bool suppress)
+        {
+            if (string.IsNullOrEmpty(shortDescrip) || suppress)
+                return;
+
+            StringBuilder sb = new StringBuilder();
+
+            switch (status)
+            {
+                case PageRequestStatusType.Retry:
+                    sb.Append("Retrying: ");
+                    sb.Append(shortDescrip);
+                    break;
+                case PageRequestStatusType.Failed:
+                    sb.Append("Failed to load: ");
+                    sb.Append(shortDescrip);
+                    break;
+                case PageRequestStatusType.LoadedFromCache:
+                    sb.Append(shortDescrip);
+                    sb.Append(" loaded from memory!");
+                    break;
+                case PageRequestStatusType.Loaded:
+                    sb.Append(shortDescrip);
+                    sb.Append(" loaded!");
+                    break;
+                default:
+                    return;
+            }
+
+            sb.Append("\n");
+            OnStatusChanged(sb.ToString());
+        }
+
+        public abstract Task<HtmlDocument> GetPage(string url, string shortDescrip, CachingMode caching, CancellationToken token, bool shouldCache, bool suppressNotifyMessages = false);
+        public abstract void ClearPageCache();
+        public abstract void DoneLoading();
+        #endregion
+    }
+}
---
+++ TallyCore/Web/WepPageProvider3.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using NetTally.Utility;
+
+namespace NetTally.Web
+{
+    public class WebPageProvider3 : PageProviderBase
+    {
+        #region Fields
+        HttpClient client;
+        const int retryLimit = 5;
+        #endregion
+
+        #region Constructor/Disposal
+        public WebPageProvider3()
+            : this(null, null, null)
+        {
+
+        }
+
+        public WebPageProvider3(HttpClientHandler handler)
+            : this(handler, null, null)
+        {
+
+        }
+
+        public WebPageProvider3(HttpClientHandler handler, WebCache cache)
+            : this(handler, cache, null)
+        {
+
+        }
+
+        public WebPageProvider3(HttpClientHandler handler, IClock clock)
+            : this(handler, null, clock)
+        {
+
+        }
+
+        public WebPageProvider3(HttpClientHandler handler, WebCache cache, IClock clock)
+            : base(handler, cache, clock)
+        {
+            SetupHandler();
+            SetupClient();
+        }
+
+        protected override void Dispose(bool itIsSafeToAlsoFreeManagedObjects)
+        {
+            if (_disposed)
+                return;
+
+            base.Dispose(itIsSafeToAlsoFreeManagedObjects);
+
+            if (itIsSafeToAlsoFreeManagedObjects)
+            {
+                client?.Dispose();
+            }
+
+        }
+        #endregion
+
+        #region Setup
+        private void SetupHandler()
+        {
+            ClientHandler.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+        }
+
+        private void SetupClient()
+        {
+            client = new HttpClient(ClientHandler);
+
+            client.MaxResponseContentBufferSize = 1000000;
+            client.Timeout = TimeSpan.FromSeconds(10);
+            client.DefaultRequestHeaders.Add("Accept", "text/html");
+            client.DefaultRequestHeaders.Add("User-Agent", UserAgent);
+            client.DefaultRequestHeaders.Add("Connection", "Keep-Alive");
+
+            // Native client handler breaks if we set the accept-encoding.
+            // It handles auto-compression on its own.
+            var handlerInfo = ClientHandler.GetType().GetTypeInfo();
+            if (handlerInfo.FullName != "ModernHttpClient.NativeMessageHandler")
+                client.DefaultRequestHeaders.Add("Accept-Encoding", "gzip,deflate");
+
+            // Have to set the BaseAddress for mobile client code to work properly.
+            client.BaseAddress = new Uri("http://forums.sufficientvelocity.com/");
+        }
+        #endregion
+
+        #region IPageProvider
+        /// <summary>
+        /// Allow manual clearing of the page cache.
+        /// </summary>
+        public override void ClearPageCache()
+        {
+            Cache.Clear();
+        }
+
+        /// <summary>
+        /// If we're notified that a given attempt to load pages is done, we can
+        /// tell the web page cache to expire old data.
+        /// </summary>
+        public override void DoneLoading()
+        {
+            Cache.ExpireCache(Clock.Now);
+        }
+
+        /// <summary>
+        /// Asynchronously load a specific page.
+        /// </summary>
+        /// <param name="url">The URL of the page to load.  Cannot be null.</param>
+        /// <param name="shortDescrip">A short description that can be used in status updates.  If null, no update will be given.</param>
+        /// <param name="caching">Indicator of whether to query the cache for the requested page.</param>
+        /// <param name="token">Cancellation token.</param>
+        /// <param name="shouldCache">Indicates whether the result of this page load should be cached.</param>
+        /// <returns>Returns an HTML document, if it can be loaded.</returns>
+        /// <exception cref="ArgumentNullException">If url is null or empty.</exception>
+        /// <exception cref="ArgumentException">If url is not a valid absolute url.</exception>
+        public override async Task<HtmlDocument> GetPage(string url, string shortDescrip, CachingMode caching, CancellationToken token, bool shouldCache, bool suppressNotifyMessages = false)
+        {
+            if (string.IsNullOrEmpty(url))
+                throw new ArgumentNullException(nameof(url));
+
+            if (!Uri.IsWellFormedUriString(url, UriKind.Absolute))
+                throw new ArgumentException($"Url is not valid: {url}", nameof(url));
+
+            Uri uri = new Uri(url);
+            HtmlDocument htmldoc;
+            string result = null;
+            string failureDescrip = null;
+
+            if (TryGetCachedPage(url, shortDescrip, caching, suppressNotifyMessages, out htmldoc))
+                return htmldoc;
+
+            NotifyStatusChange(PageRequestStatusType.Requested, url, shortDescrip, null, suppressNotifyMessages);
+
+            // Limit to no more than N parallel requests
+            await ss.WaitAsync(token).ConfigureAwait(false);
+
+            try
+            {
+                Cookie cookie = ForumCookies.GetCookie(uri);
+                if (cookie != null)
+                {
+                    ClientHandler.CookieContainer.Add(uri, cookie);
+                }
+
+                int tries = 0;
+                HttpResponseMessage response;
+
+                while (result == null && tries < retryLimit && token.IsCancellationRequested == false)
+                {
+                    if (tries > 0)
+                    {
+                        // If we have to retry loading the page, give it a short delay.
+                        await Task.Delay(TimeSpan.FromSeconds(4)).ConfigureAwait(false);
+                        NotifyStatusChange(PageRequestStatusType.Retry, url, shortDescrip, null, suppressNotifyMessages);
+                    }
+                    tries++;
+
+                    try
+                    {
+                        using (response = await client.GetAsync(uri, token).ConfigureAwait(false))
+                        {
+                            if (response.IsSuccessStatusCode)
+                            {
+                                result = await response.Content.ReadAsStringAsync();
+                            }
+                            else if (IsFailure(response))
+                            {
+                                failureDescrip = GetFailureMessage(response, shortDescrip, url);
+                                tries = retryLimit;
+                            }
+                            else if (response.StatusCode == HttpStatusCode.Moved ||
+                                     response.StatusCode == HttpStatusCode.MovedPermanently ||
+                                     response.StatusCode == HttpStatusCode.Redirect ||
+                                     response.StatusCode == HttpStatusCode.TemporaryRedirect)
+                            {
+                                url = response.Content.Headers.ContentLocation.AbsoluteUri;
+                            }
+                        }
+                    }
+                    catch (HttpRequestException e)
+                    {
+                        NotifyStatusChange(PageRequestStatusType.Error, url, shortDescrip, e, suppressNotifyMessages);
+                        throw;
+                    }
+
+                }
+            }
+            finally
+            {
+                ss.Release();
+            }
+
+            if (token.IsCancellationRequested)
+            {
+                return null;
+            }
+
+            if (result == null)
+            {
+                if (string.IsNullOrEmpty(failureDescrip))
+                    NotifyStatusChange(PageRequestStatusType.Failed, url, shortDescrip, null, suppressNotifyMessages);
+                else
+                    NotifyStatusChange(PageRequestStatusType.Failed, url, failureDescrip, null, suppressNotifyMessages);
+
+                return null;
+            }
+
+            if (shouldCache)
+                Cache.Add(url, result);
+
+            htmldoc = new HtmlDocument();
+            htmldoc.LoadHtml(result);
+
+            NotifyStatusChange(PageRequestStatusType.Loaded, url, shortDescrip, null, suppressNotifyMessages);
+
+            return htmldoc;
+        }
+        #endregion
+
+        #region Utility Functions        
+        /// <summary>
+        /// Tries to get the cached version of the requested page.
+        /// </summary>
+        /// <param name="url">The URL.</param>
+        /// <param name="shortDescrip">The short descrip.</param>
+        /// <param name="caching">The caching.</param>
+        /// <param name="suppressNotifyMessages">if set to <c>true</c> [suppress notify messages].</param>
+        /// <param name="htmldoc">The htmldoc.</param>
+        /// <returns>Returns true if it found the requested page.</returns>
+        private bool TryGetCachedPage(string url, string shortDescrip, CachingMode caching, bool suppressNotifyMessages, out HtmlDocument htmldoc)
+        {
+            htmldoc = null;
+
+            if (caching == CachingMode.BypassCache)
+                return false;
+
+            htmldoc = Cache.Get(url);
+
+            if (htmldoc == null)
+                return false;
+
+            NotifyStatusChange(PageRequestStatusType.LoadedFromCache, url, shortDescrip, null, suppressNotifyMessages);
+
+            return true;
+        }
+
+        /// <summary>
+        /// Determines whether the specified HTTP response is a failure.
+        /// </summary>
+        /// <param name="response">The response.</param>
+        /// <returns>Returns true if it's a failure response code.</returns>
+        private bool IsFailure(HttpResponseMessage response)
+        {
+            if ((int)response.StatusCode >= 400 && (int)response.StatusCode < 600)
+                return true;
+
+            return false;
+        }
+
+        /// <summary>
+        /// Gets the failure message for a given response code.
+        /// </summary>
+        /// <param name="response">The response.</param>
+        /// <param name="shortDescrip">The short descrip.</param>
+        /// <param name="url">The URL.</param>
+        /// <returns></returns>
+        private string GetFailureMessage(HttpResponseMessage response, string shortDescrip, string url)
+        {
+            string failureDescrip = "";
+
+            if (Enum.IsDefined(typeof(HttpStatusCode), response.StatusCode))
+            {
+                failureDescrip = $"{shortDescrip}\nReason: {response.ReasonPhrase} ({response.StatusCode})\nURL: {url}";
+            }
+            else
+            {
+                // Fail all 400/500 level responses
+                // Includes 429 (Too Many Requests), proposed standard not in the standard enum list
+                failureDescrip = $"{shortDescrip}\nReason: {response.ReasonPhrase} ({(int)response.StatusCode})\nURL: {url}";
+            }
+
+            if (response.StatusCode == HttpStatusCode.Forbidden ||
+                response.StatusCode == HttpStatusCode.Unauthorized)
+            {
+                failureDescrip += "\nConsider contacting the site administrator.";
+            }
+
+            return failureDescrip;
+        }
+        #endregion
+    }
+}
