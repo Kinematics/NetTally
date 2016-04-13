110569d8ee720eadd29ac08c18dfe8d6476a03ce
David Smith <dsmith@datasync.com>
2016-04-13 12:18:41 -0500

    Add a function in the platform assembly that can handle generating proper unicode-aware hashcodes for strings, given a set of compare options.

---
+++ NetTally/Misc/UnicodeHashFunction.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Globalization;
+
+namespace NetTally
+{
+    public static class UnicodeHashFunction
+    {
+        /// <summary>
+        /// Hashes the provided string using the given CompareOptions.
+        /// Doing this allows all custom compare options to be applied in determining the hash value.
+        /// </summary>
+        /// <param name="str">The string.</param>
+        /// <param name="info">The CompareInfo object doing the unicode-aware comparison.</param>
+        /// <param name="options">The options to apply to the comparison.</param>
+        /// <returns>Returns the hash code for the string.</returns>
+        public static int HashFunction(string str, CompareInfo info, CompareOptions options)
+        {
+            if (info == null)
+                throw new ArgumentNullException(nameof(info));
+
+            if (string.IsNullOrEmpty(str))
+                return 0;
+
+            SortKey sortOrder = info.GetSortKey(str, options);
+
+            int hash = GetByteArrayHash(sortOrder.KeyData);
+
+            return hash;
+        }
+
+        /// <summary>
+        /// Generates a hash code for an array of bytes.
+        /// </summary>
+        /// <param name="keyData">The key data.</param>
+        /// <returns>Returns a hash code value.</returns>
+        private static int GetByteArrayHash(byte[] keyData)
+        {
+            unchecked
+            {
+                const int p = 16777619;
+                int hash = (int)2166136261;
+
+                for (int i = 0; i < keyData.Length; i++)
+                    hash = (hash ^ keyData[i]) * p;
+
+                hash += hash << 13;
+                hash ^= hash >> 7;
+                hash += hash << 3;
+                hash ^= hash >> 17;
+                hash += hash << 5;
+                return hash;
+            }
+        }
+    }
+}
