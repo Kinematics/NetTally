828335f5feafd773db0912c632639dd4f44f4659
David Smith <dsmith@datasync.com>
2016-05-13 16:05:22 -0500

    Implement Borda fractional ranking.

--- TallyCore/VoteCounting/RankVoteCounting/BordaFractionRankVoteCounter.cs
+++ TallyCore/VoteCounting/RankVoteCounting/BordaFractionRankVoteCounter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,7 +26,59 @@ namespace NetTally.VoteCounting
         /// <returns>Returns a ranking list of winning votes.</returns>
         protected override RankResults RankTask(GroupedVotesByTask task)
         {
-            throw new NotImplementedException();
+            var groupVotes = GroupRankVotes.GroupVotesByVoteAndRank(task);
+
+            var rankedVotes = from vote in groupVotes
+                              select new { Vote = vote.VoteContent, Rank = RankVote(vote.Ranks) };
+
+            var orderedVotes = rankedVotes.OrderByDescending(a => a.Rank);
+
+            foreach (var orderedVote in orderedVotes)
+            {
+                Debug.WriteLine($"- {orderedVote.Vote} [{orderedVote.Rank}]");
+            }
+
+            return orderedVotes.Select(a => a.Vote).ToList();
+        }
+
+        /// <summary>
+        /// Ranks the vote.
+        /// </summary>
+        /// <param name="ranks">Votes with associated ranks, for the voters who ranked the vote with a given value.</param>
+        /// <returns>Returns a numeric evaluation of the overall rank of the vote.</returns>
+        private double RankVote(IEnumerable<RankedVoters> ranks)
+        {
+            double voteValue = 0;
+
+            foreach (var r in ranks)
+            {
+                voteValue += ValueOfRank(r.Rank) * r.Voters.Count();
+            }
+
+            return voteValue;
+        }
+
+        /// <summary>
+        /// Get the numeric value of a given rank. 
+        /// </summary>
+        /// <param name="rank">The rank being evaluated.</param>
+        /// <returns></returns>
+        /// <exception cref="System.ArgumentNullException"></exception>
+        /// <exception cref="System.ArgumentOutOfRangeException"></exception>
+        private double ValueOfRank(string rank)
+        {
+            if (string.IsNullOrEmpty(rank))
+                throw new ArgumentNullException(nameof(rank));
+
+            int rankAsInt = int.Parse(rank);
+
+            if (rankAsInt < 1 || rankAsInt > 9)
+                throw new ArgumentOutOfRangeException(nameof(rank));
+
+            // Ranks valued at 1 for #1, then 1/N for each higher N.
+            double rankValue = 1.0 / rankAsInt;
+
+            return rankValue;
         }
     }
 }
