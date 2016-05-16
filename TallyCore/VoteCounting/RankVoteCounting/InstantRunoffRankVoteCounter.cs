63663f9e49efec9fa0407c7998f4aa5393cafb77
David Smith <dsmith@datasync.com>
2016-05-16 01:20:50 -0500

    Remove unnecessary usings.

--- TallyCore/VoteCounting/RankVoteCounting/InstantRunoffRankVoteCounter.cs
+++ TallyCore/VoteCounting/RankVoteCounting/InstantRunoffRankVoteCounter.cs
@@ -2,18 +2,12 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using NetTally.Utility;
 
 namespace NetTally.VoteCounting
 {
     // List of preference results ordered by winner
     using RankResults = List<string>;
-    // Task (string), Ordered list of ranked votes
-    using RankResultsByTask = Dictionary<string, List<string>>;
-    // Vote (string), collection of voters
-    using SupportedVotes = Dictionary<string, HashSet<string>>;
     // Task (string group), collection of votes (string vote, hashset of voters)
     using GroupedVotesByTask = IGrouping<string, KeyValuePair<string, HashSet<string>>>;
 
