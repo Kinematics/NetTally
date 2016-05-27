52bbd2212e3279f5a10156776528c7686a654db1
David Smith <dsmith@datasync.com>
2016-05-27 14:30:31 -0500

    Update version for bugfix branch.

--- NetTally/Properties/AssemblyInfo.cs
+++ NetTally/Properties/AssemblyInfo.cs
@@ -53,4 +53,4 @@ using System.Windows;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.7.0.0")]
 [assembly: AssemblyFileVersion("1.7.3.1")]
-[assembly: AssemblyInformationalVersion("1.7.3a")]
+[assembly: AssemblyInformationalVersion("1.7.3.1")]
--- TallyCore/Properties/AssemblyInfo.cs
+++ TallyCore/Properties/AssemblyInfo.cs
@@ -34,4 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.7.0.0")]
 [assembly: AssemblyFileVersion("1.7.3.1")]
-[assembly: AssemblyInformationalVersion("1.7.3a")]
+[assembly: AssemblyInformationalVersion("1.7.3.1")]
