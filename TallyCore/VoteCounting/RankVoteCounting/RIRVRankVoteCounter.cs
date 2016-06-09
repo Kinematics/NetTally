8f5e98f3fb627f96642d92e965a6724276fd78f4
David Smith <dsmith@datasync.com>
2016-06-09 15:11:08 -0500

    Refactor RIR some. Add comments.

--- TallyCore/VoteCounting/RankVoteCounting/RIRVRankVoteCounter.cs
+++ TallyCore/VoteCounting/RankVoteCounting/RIRVRankVoteCounter.cs
@@ -10,23 +10,41 @@ namespace NetTally.VoteCounting
     // Task (string group), collection of votes (string vote, hashset of voters)
     using GroupedVotesByTask = IGrouping<string, KeyValuePair<string, HashSet<string>>>;
 
-
+    /// <summary>
+    /// Rated Instant Runoff voting scores all vote options, taking the top two,
+    /// and does an instant runoff between them.
+    /// This uses the Wilson score for scoring votes.
+    /// The scoring aspect allows relative preference to be taken into account,
+    /// but relative preference is ignored for the runoff phase, which merely
+    /// checks for which of A or B is most often preferred over the other.
+    /// This avoids the flaws of standard instant runoff voting by incorporating
+    /// score ratings into the evaluation.
+    /// </summary>
+    /// <seealso cref="NetTally.VoteCounting.BaseRankVoteCounter" />
     public class RIRVRankVoteCounter : BaseRankVoteCounter
     {
+        /// <summary>
+        /// Implementation to generate the ranking list for the provided set
+        /// of votes for a specific task.
+        /// </summary>
+        /// <param name="task">The task that the votes are grouped under.</param>
+        /// <returns>Returns a ranking list of winning votes.</returns>
         protected override RankResults RankTask(GroupedVotesByTask task)
         {
             Debug.WriteLine(">>Rated Instant Runoff<<");
 
             List<string> winningChoices = new List<string>();
 
-            var groupVotes = GroupRankVotes.GroupByVoteAndRank(task);
+            // The groupVotes are used for getting the Wilson score
+            var rankedVotes = GroupRankVotes.GroupByVoteAndRank(task);
+            // The voterRankings are used for the runoff
             var voterRankings = GroupRankVotes.GroupByVoterAndRank(task);
+            // The full choices list is just to keep track of how many we have left.
             var allChoices = GetAllChoices(voterRankings);
-
 
             for (int i = 1; i <= 9; i++)
             {
-                string winner = GetWinningVote(voterRankings, groupVotes);
+                string winner = GetWinningVote(voterRankings, rankedVotes);
 
                 if (winner == null)
                     break;
@@ -40,41 +58,72 @@ namespace NetTally.VoteCounting
                     break;
 
                 voterRankings = RemoveChoiceFromVotes(voterRankings, winner);
-                groupVotes = RemoveChoiceFromRanks(groupVotes, winner);
+                rankedVotes = RemoveChoiceFromRanks(rankedVotes, winner);
             }
 
             return winningChoices;
         }
 
-        private string GetWinningVote(IEnumerable<VoterRankings> voterRankings, IEnumerable<RankGroupedVoters> groupVotes)
+        /// <summary>
+        /// Gets the winning vote.
+        /// </summary>
+        /// <param name="voterRankings">The voter rankings.</param>
+        /// <param name="rankedVotes">The votes, ranked.</param>
+        /// <returns></returns>
+        private string GetWinningVote(IEnumerable<VoterRankings> voterRankings, IEnumerable<RankGroupedVoters> rankedVotes)
         {
-            var rankedVotes = from vote in groupVotes
+            string option1;
+            string option2;
+
+            GetTopTwoRatedOptions(rankedVotes, out option1, out option2);
+
+            return GetOptionWithHigherPrefCount(voterRankings, option1, option2);
+        }
+
+        /// <summary>
+        /// Gets the top two rated options.
+        /// </summary>
+        /// <param name="rankedVotes">The group votes.</param>
+        /// <param name="option1">The top rated option. Null if there aren't any options available.</param>
+        /// <param name="option2">The second rated option.  Null if there is only one option available.</param>
+        private void GetTopTwoRatedOptions(IEnumerable<RankGroupedVoters> rankedVotes, out string option1, out string option2)
+        {
+            var scoredVotes = from vote in rankedVotes
                               select new { Vote = vote.VoteContent, Rank = RankScoring.LowerWilsonScore(vote.Ranks) };
 
-            var orderedVotes = rankedVotes.OrderByDescending(a => a.Rank);
+            var orderedVotes = scoredVotes.OrderByDescending(a => a.Rank);
 
             var topTwo = orderedVotes.Take(2);
 
             if (!topTwo.Any())
-                return null;
+            {
+                option1 = null;
+                option2 = null;
+            }
+            else if (topTwo.Count() == 1)
+            {
+                option1 = topTwo.First().Vote;
+                option2 = null;
+            }
+            else
+            {
+                option1 = topTwo.First().Vote;
+                option2 = topTwo.Last().Vote;
+            }
 
-            if (topTwo.Count() == 1)
-                return topTwo.First().Vote;
-
-            string option1 = topTwo.First().Vote;
-            string option2 = topTwo.Last().Vote;
-            bool option1ScoreHigher = topTwo.First().Rank > topTwo.Last().Rank;
-
-
-            Debug.Write($"[{option1}, {option2}] ");
-
-            string preferredOption = GetOptionWithHigherPrefCount(voterRankings, option1, option2, option1ScoreHigher);
-
-            return preferredOption;
+            Debug.Write($"[{option1 ?? ""}, {option2 ?? ""}] ");
         }
 
-
-        private string GetOptionWithHigherPrefCount(IEnumerable<VoterRankings> voterRankings, string option1, string option2, bool option1ScoreHigher)
+        /// <summary>
+        /// Gets the option with higher preference count.
+        /// This is the runoff portion of the vote evaluation.  Whichever option has more
+        /// people that prefer it over the other, wins.
+        /// </summary>
+        /// <param name="voterRankings">The voter rankings.  This allows seeing which option each voter preferred.</param>
+        /// <param name="option1">The first option up for consideration.</param>
+        /// <param name="option2">The second option up for consideration.</param>
+        /// <returns>Returns the winning option.</returns>
+        private string GetOptionWithHigherPrefCount(IEnumerable<VoterRankings> voterRankings, string option1, string option2)
         {
             int count1 = 0;
             int count2 = 0;
@@ -109,19 +158,16 @@ namespace NetTally.VoteCounting
                 }
             }
 
-            if (count1 > count2)
-            {
-                return option1;
-            }
+            // If count1==count2, we use the higher scored option, which
+            // will necessarily be option1.  Therefore all ties will be
+            // in favor of option1, and the only thing we need to check
+            // for is if option2 wins explicitly.
             if (count2 > count1)
             {
                 return option2;
             }
-            if (option1ScoreHigher)
-            {
-                return option1;
-            }
-            return option2;
+
+            return option1;
         }
 
 
@@ -138,13 +184,26 @@ namespace NetTally.VoteCounting
         }
 
         /// <summary>
+        /// Removes the specified vote choice from the list of ranked votes.
+        /// </summary>
+        /// <param name="rankedVotes">The ranked votes.</param>
+        /// <param name="choice">The choice being removed.</param>
+        /// <returns></returns>
+        private IEnumerable<RankGroupedVoters> RemoveChoiceFromRanks(IEnumerable<RankGroupedVoters> rankedVotes, string choice)
+        {
+            var res = rankedVotes.Where(a => a.VoteContent != choice);
+
+            return res.ToList();
+        }
+
+        /// <summary>
         /// Removes a list of choices from voter rankings.
         /// These are the choices that have already won a rank spot.
         /// </summary>
         /// <param name="voterRankings">The voter rankings.</param>
         /// <param name="choice">The already chosen choices.</param>
         /// <returns>Returns the results as a list.</returns>
-        private List<VoterRankings> RemoveChoiceFromVotes(IEnumerable<VoterRankings> voterRankings, string choice)
+        private IEnumerable<VoterRankings> RemoveChoiceFromVotes(IEnumerable<VoterRankings> voterRankings, string choice)
         {
             var res = from voter in voterRankings
                       select new VoterRankings
@@ -152,37 +211,6 @@ namespace NetTally.VoteCounting
                           Voter = voter.Voter,
                           RankedVotes = voter.RankedVotes
                               .Where(v => choice != v.Vote)
-                              .OrderBy(v => v.Rank)
-                              .Select((a, b) => new RankedVote { Vote = a.Vote, Rank = b + 1 })
-                              .ToList()
-                      };
-
-            return res.ToList();
-        }
-
-        private List<RankGroupedVoters> RemoveChoiceFromRanks(IEnumerable<RankGroupedVoters> groupVotes, string winner)
-        {
-            var res = groupVotes.Where(a => a.VoteContent != winner);
-
-            return res.ToList();
-        }
-
-
-        /// <summary>
-        /// Removes a list of choices from voter rankings.
-        /// These are the choices that have already won a rank spot.
-        /// </summary>
-        /// <param name="voterRankings">The voter rankings.</param>
-        /// <param name="chosenChoices">The already chosen choices.</param>
-        /// <returns>Returns the results as a list.</returns>
-        private List<VoterRankings> RemoveChoicesFromVotes(IEnumerable<VoterRankings> voterRankings, List<string> chosenChoices)
-        {
-            var res = from voter in voterRankings
-                      select new VoterRankings
-                      {
-                          Voter = voter.Voter,
-                          RankedVotes = voter.RankedVotes
-                              .Where(v => chosenChoices.Contains(v.Vote) == false)
                               .OrderBy(v => v.Rank)
                               .Select((a, b) => new RankedVote { Vote = a.Vote, Rank = b + 1 })
                               .ToList()
--- TallyCore/VoteCounting/RankVoteCounting/WilsonRankVoteCounter.cs
+++ TallyCore/VoteCounting/RankVoteCounting/WilsonRankVoteCounter.cs
@@ -11,10 +11,13 @@ namespace NetTally.VoteCounting
     using GroupedVotesByTask = IGrouping<string, KeyValuePair<string, HashSet<string>>>;
 
     /// <summary>
-    /// Borda is being removed as a valid option from the list of rank vote options.
-    /// Aside from systemic failures of the method itself, it cannot give proper
-    /// valuation to unranked options, which intrinsically makes it a bad fit
-    /// for handling user-entered quest voting schemes.
+    /// Wilson vote scoring uses the lower bounds of a Bournoulli analysis of the vote
+    /// rankings to get the 95% minimum confidence interval.
+    /// This means that a voted item with only a few supporters will have a low score
+    /// due to a high error margin, while a score with more supporters will have a
+    /// higher relative confidence rating.
+    /// This improves on the Borda scoring, which has no means of compensating for
+    /// votes that are ranked by less than 100% of the voter base.
     /// </summary>
     /// <seealso cref="NetTally.VoteCounting.BaseRankVoteCounter" />
     public class WilsonRankVoteCounter : BaseRankVoteCounter
@@ -29,6 +32,8 @@ namespace NetTally.VoteCounting
         {
             Debug.WriteLine(">>Wilson Scoring<<");
 
+            // Can calculating the score easily by having all the rankings for
+            // each vote grouped together.
             var groupVotes = GroupRankVotes.GroupByVoteAndRank(task);
 
             var rankedVotes = from vote in groupVotes
@@ -36,11 +41,13 @@ namespace NetTally.VoteCounting
 
             var orderedVotes = rankedVotes.OrderByDescending(a => a.Rank);
 
+            // Display the votes and their ratings for debugging purposes.
             foreach (var orderedVote in orderedVotes)
             {
                 Debug.WriteLine($"- {orderedVote.Vote} [{orderedVote.Rank:f5}]");
             }
 
+            // Only return the list of vote options themselves.
             return orderedVotes.Select(a => a.Vote).ToList();
         }
     }
