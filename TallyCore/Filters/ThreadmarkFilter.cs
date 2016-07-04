b21df0d9d7c839554c9cdca1679588e7748d09ad
David Smith <dsmith@datasync.com>
2016-07-04 18:28:02 -0500

    Remove old threadmark filter class.

--- TallyCore/Filters/ThreadmarkFilter.cs
+++ TallyCore/Filters/ThreadmarkFilter.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 using System.Text.RegularExpressions;
 using NetTally.Filters;
 
@@ -34,76 +33,6 @@ namespace NetTally.Adapters
             }
 
             return null;
-        }
-    }
-
-
-    public class ThreadmarkFilter1
-    {
-        static readonly Regex omakeRegex = new Regex(@"\bomake\b", RegexOptions.IgnoreCase);
-        readonly bool useCustomRegex;
-        Regex customRegex;
-
-        /// <summary>
-        /// Constructor.
-        /// </summary>
-        /// <param name="quest">Quest to use as a basis for constructing a custom filter regex.</param>
-        public ThreadmarkFilter1(IQuest quest)
-        {
-            if (quest != null)
-            {
-                useCustomRegex = quest.UseCustomThreadmarkFilters;
-
-                if (useCustomRegex)
-                {
-                    CreateCustomRegex(quest.CustomThreadmarkFilters);
-                }
-            }
-        }
-
-        /// <summary>
-        /// Create a custom regex from the provided filter string.
-        /// </summary>
-        /// <param name="customThreadmarkFilters">A string with a comma-delimited
-        /// list of words that can be used to create a custom threadmark filter.</param>
-        private void CreateCustomRegex(string customThreadmarkFilters)
-        {
-            if (string.IsNullOrEmpty(customThreadmarkFilters))
-                return;
-
-            string safeCustomThreadmarkFilters = Utility.StringUtility.SafeString(customThreadmarkFilters);
-
-            var splits = safeCustomThreadmarkFilters.Split(',');
-
-            var options = splits.Aggregate((s, t) => s.Trim() + "|" + t.Trim());
-
-            string rString = $@"\b({options})\b";
-
-            try
-            {
-                customRegex = new Regex(rString, RegexOptions.IgnoreCase);
-            }
-            catch (ArgumentException)
-            {
-                customRegex = null;
-            }
-        }
-
-        /// <summary>
-        /// Filter the provided title string based on either the default or
-        /// the custom filter regex.
-        /// </summary>
-        /// <param name="title">The string to test.</param>
-        /// <returns>Returns true if the provided string matches the currently active filter.</returns>
-        public bool Filter(string title)
-        {
-            if (string.IsNullOrEmpty(title))
-                return false;
-
-            if (useCustomRegex && customRegex != null)
-                return customRegex.Match(title).Success;
-
-            return omakeRegex.Match(title).Success;
         }
     }
 }
