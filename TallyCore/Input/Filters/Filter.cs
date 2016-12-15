04c58f99da95a2108497ba1952a88608f2ff4173
David Smith <dsmith@datasync.com>
2016-12-14 22:00:05 -0600

    Implement a simplified Filter class, to replace TaskFilter and ThreadmarkFilter. Add unit tests for Filter class.

---
+++ TallyCore/Input/Filters/Filter.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using NetTally.Utility;
+
+namespace NetTally.Filters
+{
+    /// <summary>
+    /// Class to handle user-defined filters for different elements of a tally.
+    /// </summary>
+    public class Filter
+    {
+        public const string EmptyLine = "^$";
+        readonly Regex filterRegex;
+
+        /// <summary>
+        /// Filter constructor.
+        /// </summary>
+        /// <param name="filterString">The primary, user-defined string to filter on.</param>
+        /// <param name="defaultString">The default, program-provided string to filter on.</param>
+        public Filter(string filterString, string defaultString)
+        {
+            filterRegex = CreateRegex(filterString, defaultString);
+        }
+
+        /// <summary>
+        /// Filter constructor.
+        /// </summary>
+        /// <param name="filterRegex">An explicit regex to use for filtering.</param>
+        public Filter(Regex filterRegex)
+        {
+            this.filterRegex = filterRegex;
+        }
+
+        /// <summary>
+        /// Function to test whether a provided string is matched by the current filter.
+        /// </summary>
+        /// <param name="input">The string to check against the filter.</param>
+        /// <returns>Returns true if the filter matches some part of the input string.</returns>
+        public bool Match(string input)
+        {
+            if (filterRegex == null)
+                return false;
+
+            return filterRegex.Match(input).Success;
+        }
+
+        /// <summary>
+        /// Function to create a regex based on a user-provided string of values to check for.
+        /// User-provided string is assumed to be comma-delimited, but may be a full regex
+        /// by itself (as long as it doesn't have any commas).
+        /// If both the primary and default test strings are empty, uses the EmptyLine for the
+        /// regex instead.
+        /// </summary>
+        /// <param name="filterString">The primary set of values for the regex.</param>
+        /// <param name="defaultString">The default/backup values to test for.</param>
+        /// <returns>Returns the constructed regex.</returns>
+        public static Regex CreateRegex(string filterString, string defaultString)
+        {
+            if (string.IsNullOrEmpty(filterString) && string.IsNullOrEmpty(defaultString))
+                return new Regex(EmptyLine);
+
+            string safeFilterString = filterString.RemoveUnsafeCharacters();
+
+            var splits = safeFilterString.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+            StringBuilder sb = new StringBuilder();
+            string bar = "";
+
+            // Convert comma-separated options to | regex options.
+            foreach (var split in splits)
+            {
+                string s = split.Trim();
+                if (!string.IsNullOrEmpty(s))
+                {
+                    sb.Append($"{bar}{s}");
+                    bar = "|";
+                }
+            }
+
+            // Add the default string value at the end.
+            sb.Append($"{bar}{defaultString}");
+
+            string sbString = sb.ToString();
+
+            try
+            {
+                return new Regex($@"\b({sbString})\b", RegexOptions.IgnoreCase);
+            }
+            catch (ArgumentException)
+            {
+                return null;
+            }
+        }
+
+    }
+}
---
+++ TallyUnitTest/FilterTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NetTally.Filters;
+
+namespace NetTally.Tests
+{
+    [TestClass]
+    public class FilterTests
+    {
+        [TestMethod]
+        public void TestNullFilter()
+        {
+            Filter filter = new Filter(null);
+            Assert.IsFalse(filter.Match(""));
+            Assert.IsFalse(filter.Match("stuff"));
+            Assert.IsFalse(filter.Match("lots of stuff (omake)"));
+            Assert.IsFalse(filter.Match("[x][Task] Some vote"));
+        }
+
+        [TestMethod]
+        public void TestBlankFilter()
+        {
+            Filter filter = new Filter("", null);
+            Assert.IsTrue(filter.Match(""));
+            Assert.IsFalse(filter.Match("stuff"));
+            Assert.IsFalse(filter.Match("lots of stuff (omake)"));
+            Assert.IsFalse(filter.Match("[x][Task] Some vote"));
+        }
+
+        [TestMethod]
+        public void TestDefaultOmakeFilter()
+        {
+            Filter filter = new Filter("", "omake");
+            Assert.IsFalse(filter.Match(""));
+            Assert.IsFalse(filter.Match("stuff"));
+            Assert.IsTrue(filter.Match("lots of stuff (omake)"));
+            Assert.IsFalse(filter.Match("[x][Task] Some vote"));
+        }
+
+        [TestMethod]
+        public void TestOmakeStuffFilter()
+        {
+            Filter filter = new Filter("stuff", "omake");
+            Assert.IsFalse(filter.Match(""));
+            Assert.IsTrue(filter.Match("stuff"));
+            Assert.IsTrue(filter.Match("lots of stuffing (omake)"));
+            Assert.IsFalse(filter.Match("[x][Task] Some vote"));
+        }
+
+        [TestMethod]
+        public void TestTaskFilter()
+        {
+            Filter filter = new Filter("My Task", Filter.EmptyLine);
+            Assert.IsFalse(filter.Match("Task"));
+            Assert.IsTrue(filter.Match("My Task"));
+            Assert.IsFalse(filter.Match(""));
+            Assert.IsFalse(filter.Match("Fountain"));
+        }
+
+        [TestMethod]
+        public void TestMultiValueFilter()
+        {
+            Filter filter = new Filter("stuff, stuffing", "omake");
+            Assert.IsFalse(filter.Match(""));
+            Assert.IsTrue(filter.Match("stuff"));
+            Assert.IsTrue(filter.Match("lots of stuffing"));
+            Assert.IsTrue(filter.Match("lots of stuffing (omake)"));
+            Assert.IsFalse(filter.Match("[x][Task] Some vote"));
+        }
+    }
+}
