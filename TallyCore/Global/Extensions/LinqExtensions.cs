b63cc45193f6b0435aeaa070aef31e79e7061f93
David Smith <dsmith@datasync.com>
2016-12-20 20:20:35 -0600

    Add a linq extension to allow running an async task with a timeout.

--- TallyCore/Global/Extensions/LinqExtensions.cs
+++ TallyCore/Global/Extensions/LinqExtensions.cs
@@ -2,12 +2,39 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using NetTally.Utility;
 
 namespace NetTally
 {
     public static class LinqExtensions
     {
+        /// <summary>
+        /// Function to allow setting a timeout on an async function that doesn't natively permit it.
+        /// From: http://stackoverflow.com/a/22078975/770213
+        /// </summary>
+        /// <typeparam name="TResult">The return type of the task being awaited.</typeparam>
+        /// <param name="task">The task to await.</param>
+        /// <param name="timeout">The timeout to wait for the task to complete.</param>
+        /// <returns>Returns the awaited task, if it completed in less than the timeout period.</returns>
+        public static async Task<TResult> TimeoutAfter<TResult>(this Task<TResult> task, TimeSpan timeout)
+        {
+            var timeoutCancellationTokenSource = new CancellationTokenSource();
+
+            var completedTask = await Task.WhenAny(task, Task.Delay(timeout, timeoutCancellationTokenSource.Token));
+            if (completedTask == task)
+            {
+                timeoutCancellationTokenSource.Cancel();
+                return await task;  // Very important in order to propagate exceptions
+            }
+            else
+            {
+                throw new TimeoutException("The operation has timed out.");
+            }
+        }
+
+
         /// <summary>
         /// Sorts the specified collection.
         /// </summary>
