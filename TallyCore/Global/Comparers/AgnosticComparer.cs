4849243d582f9c379072058f03f7a6c47a58f7e4
David Smith <dsmith@datasync.com>
2016-12-23 22:48:03 -0600

    Observe notifications of options changing rather than querying the value on every get request.  Tidy up.

--- TallyCore/Global/Comparers/AgnosticComparer.cs
+++ TallyCore/Global/Comparers/AgnosticComparer.cs
@@ -1,11 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Globalization;
 
 namespace NetTally.Utility
 {
     public static class Agnostic
     {
+        private static IEqualityComparer<string> currentComparer;
+
+        private static IEqualityComparer<string> StringComparer1 { get; set; }
+
+        private static IEqualityComparer<string> StringComparer2 { get; set; }
+
         /// <summary>
         /// Initialize the agnostic string comparers using the provided hash function.
         /// Injects the function from the non-PCL assembly, to get around PCL limitations.
@@ -18,25 +25,25 @@ namespace NetTally.Utility
                 CompareOptions.IgnoreCase | CompareOptions.IgnoreNonSpace | CompareOptions.IgnoreWidth, hashFunction);
             StringComparer2 = new CustomStringComparer(CultureInfo.InvariantCulture.CompareInfo,
                 CompareOptions.IgnoreSymbols | CompareOptions.IgnoreCase | CompareOptions.IgnoreNonSpace | CompareOptions.IgnoreWidth, hashFunction);
+
+            AdvancedOptions.Instance.PropertyChanged += AdvancedOptions_PropertyChanged;
+
+            currentComparer = AdvancedOptions.Instance.WhitespaceAndPunctuationIsSignificant ? StringComparer1 : StringComparer2;
+        }
+
+        private static void AdvancedOptions_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == "WhitespaceAndPunctuationIsSignificant")
+            {
+                currentComparer = AdvancedOptions.Instance.WhitespaceAndPunctuationIsSignificant ? StringComparer1 : StringComparer2;
+            }
         }
 
         /// <summary>
         /// A string comparer object that allows comparison between strings that
         /// can ignore lots of annoying user-entered variances.
         /// </summary>
-        public static IEqualityComparer<string> StringComparer
-        {
-            get
-            {
-                var comparer = AdvancedOptions.Instance.WhitespaceAndPunctuationIsSignificant ? StringComparer1 : StringComparer2;
-                if (comparer == null)
-                    throw new InvalidOperationException("Agnostic string comparers have not been initialized.");
-                return comparer;
-            }
-        }
-
-        private static IEqualityComparer<string> StringComparer1 { get; set; }
-
-        private static IEqualityComparer<string> StringComparer2 { get; set; }
+        public static IEqualityComparer<string> StringComparer =>
+            currentComparer ?? throw new InvalidOperationException("Agnostic string comparers have not been initialized.");
     }
 }
