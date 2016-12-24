2b5200936eea5442de6694df907cb448910f21b1
David Smith <dsmith@datasync.com>
2016-12-23 21:54:43 -0600

    Change namespace for modified collection type.

--- TallyCore/Global/Collections/ObservableCollectionExt.cs
+++ TallyCore/Global/Collections/ObservableCollectionExt.cs
@@ -6,7 +6,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Threading;
 
-namespace NetTally.Extensions
+namespace NetTally.Collections
 {
     /// <summary>
     /// An extension of the ObservableCollection class that allows specialized
