6e334e2bfc916eeae1a50538e043d650bf7d4499
David Smith <dsmith@datasync.com>
2017-01-10 20:30:52 -0600

    Copy the default unicode hash function into the Tests project, and reference that instead of the one from the main assembly.

--- TallyUnitTest/VotingRecordsTests.cs
+++ TallyUnitTest/VotingRecordsTests.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using NetTally.Platform;
+using NetTally.Tests.Platform;
 using NetTally.Votes.Experiment;
 using NetTally.Utility;
 
