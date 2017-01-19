8498c925a1f6d6428ea0c286cd3af8f3ddf2108c
David Smith <dsmith@datasync.com>
2017-01-19 07:51:49 -0600

    Comment, to track usage of different marker types.

--- TallyCore/Votes/Experiment/Counting/VotingConstructor.cs
+++ TallyCore/Votes/Experiment/Counting/VotingConstructor.cs
@@ -4,6 +4,12 @@ using System.Threading;
 
 namespace NetTally.Votes.Experiment
 {
+    // X marker can only contain other X
+    // +/- marker can only contain other +/-
+    // # rank marker can only contain X (entire block is given a single rank)
+    // + score marker can contain X (copy parent score) or other + score
+
+
     public static class VotingConstructor
     {
         internal static List<string> GetWorkingVote(Post post)
