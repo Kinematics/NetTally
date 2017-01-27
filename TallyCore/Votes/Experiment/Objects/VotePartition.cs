78d2b11b975e24b6e02d5a301373310ad2effd5d
David Smith <dsmith@datasync.com>
2017-01-27 01:12:44 -0600

    Add a higher level abstraction of a vote line.

---
+++ TallyCore/Votes/Experiment/Objects/VotePartition.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NetTally.Utility;
+
+namespace NetTally.Votes.Experiment
+{
+    public class VotePartition
+    {
+        public PartitionMode PartitionMode { get; private set; }
+        private readonly List<VoteLine> voteLines = new List<VoteLine>();
+        public IReadOnlyList<VoteLine> VoteLines { get { return voteLines; } }
+
+        public VotePartition()
+        {
+
+        }
+
+
+        public static List<VotePartition> GetPartitionsFrom(Vote vote, PartitionMode partitionMode)
+        {
+            List<VotePartition> partitions = new List<VotePartition>();
+
+
+            return partitions;
+        }
+    }
+}
