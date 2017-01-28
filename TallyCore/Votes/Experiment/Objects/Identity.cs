00324f53be59da82f6355212284add19e8aa9c71
David Smith <dsmith@datasync.com>
2017-01-28 17:44:16 -0600

    Add an identity class.

---
+++ TallyCore/Votes/Experiment/Objects/Identity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace NetTally.Votes.Experiment
+{
+    public class Identity
+    {
+        public string Name { get; }
+        public string PostID { get; }
+
+        public Identity(string name, string postID)
+        {
+            if (string.IsNullOrEmpty(name))
+                throw new ArgumentNullException(nameof(name));
+            if (string.IsNullOrEmpty(postID))
+                throw new ArgumentNullException(nameof(postID));
+
+            Name = name;
+            PostID = postID;
+        }
+    }
+}
