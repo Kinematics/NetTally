53a0472fc2cc257e6f72b4b4085e6b5c0518d79e
David Smith <dsmith@datasync.com>
2017-01-28 18:19:21 -0600

    Add an identity number for variant names.

--- TallyCore/Votes/Experiment/Objects/Identity.cs
+++ TallyCore/Votes/Experiment/Objects/Identity.cs
@@ -7,8 +7,11 @@ namespace NetTally.Votes.Experiment
         public string Name { get; }
         public string PostID { get; }
         public bool IsPlan { get; }
+        public int Number { get; }
 
-        public string FullName => IsPlan ? $"◈{Name}" : Name;
+        private string Variant => Number > 0 ? $" ({Number})" : "";
+        public string VariantName => IsPlan ? $"{Name}{Variant}" : Name;
+        public string FullName => IsPlan ? $"◈{Name}{Variant}" : Name;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="Identity"/> class.
@@ -17,7 +20,7 @@ namespace NetTally.Votes.Experiment
         /// <param name="postID">The post identifier.</param>
         /// <param name="isPlan">if set to <c>true</c> [is plan].</param>
         /// <exception cref="System.ArgumentNullException"/>
-        public Identity(string name, string postID, bool isPlan = false)
+        public Identity(string name, string postID, bool isPlan = false, int number = 0)
         {
             if (string.IsNullOrEmpty(name))
                 throw new ArgumentNullException(nameof(name));
@@ -27,6 +30,7 @@ namespace NetTally.Votes.Experiment
             Name = name;
             PostID = postID;
             IsPlan = isPlan;
+            Number = number;
         }
 
         public override int GetHashCode()
