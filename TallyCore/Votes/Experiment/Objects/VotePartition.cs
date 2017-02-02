745636badcce98f5b02b4e65df82af0a23984e92
David Smith <dsmith@datasync.com>
2017-02-01 20:21:50 -0600

    Fill in VotePartition class.

--- TallyCore/Votes/Experiment/Objects/VotePartition.cs
+++ TallyCore/Votes/Experiment/Objects/VotePartition.cs
@@ -1,23 +1,84 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
-using NetTally.Utility;
+using System.Linq;
 
 namespace NetTally.Votes.Experiment
 {
     public class VotePartition
     {
-        public PartitionMode PartitionMode { get; private set; }
-
+        #region Properties and fields
         private readonly List<VoteLine> voteLines = new List<VoteLine>();
         public IReadOnlyList<VoteLine> VoteLines { get { return voteLines; } }
+        public bool IsEmpty => !voteLines.Any();
 
         public string Task { get; set; }
+        #endregion
 
+        #region Constructors
+        /// <summary>
+        /// Construct an empty <see cref="VotePartition"/>
+        /// </summary>
         public VotePartition()
         {
-
+            Task = string.Empty;
         }
+
+        /// <summary>
+        /// Construct new <see cref="VotePartition"/>
+        /// </summary>
+        /// <param name="voteLine">The vote line that the partition is composed of.</param>
+        /// <exception cref="ArgumentNullException"/>
+        public VotePartition(VoteLine voteLine)
+        {
+            AddLine(voteLine);
+        }
+
+        /// <summary>
+        /// Construct new <see cref="VotePartition"/>
+        /// </summary>
+        /// <param name="voteLines">The vote lines that the partition is composed of.</param>
+        /// <exception cref="ArgumentNullException"/>
+        public VotePartition(IEnumerable<VoteLine> voteLines)
+        {
+            AddLines(voteLines);
+        }
+        #endregion
+
+        #region Public modification methods
+        /// <summary>
+        /// Add a vote line to the current partition.
+        /// </summary>
+        /// <param name="line">The line to add.</param>
+        /// <exception cref="ArgumentNullException"/>
+        public void AddLine(VoteLine line)
+        {
+            if (line == null)
+                throw new ArgumentNullException(nameof(line));
+
+            if (!voteLines.Any())
+                Task = line.Task;
+
+            voteLines.Add(line);
+        }
+
+        /// <summary>
+        /// Add a list of vote lines to the current partition.
+        /// </summary>
+        /// <param name="line">The lines to add.</param>
+        /// <exception cref="ArgumentNullException"/>
+        public void AddLines(IEnumerable<VoteLine> lines)
+        {
+            if (lines == null)
+                throw new ArgumentNullException(nameof(lines));
+            if (!lines.Any())
+                return;
+
+            if (!voteLines.Any())
+                Task = lines.First().Task;
+
+            voteLines.AddRange(lines);
+        }
+        #endregion
 
 
         public static List<VotePartition> GetPartitionsFrom(Vote vote, PartitionMode partitionMode)
