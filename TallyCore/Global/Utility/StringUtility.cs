860cda98a3b4b7742d5a29d0392d656972f6cfc9
David Smith <dsmith@datasync.com>
2017-02-06 00:33:45 -0600

    Add utility method to remove accented characters without resorting to string.Normalize, which isn't available in .NET Standard prior to 2.0. Add tests.  Runtime is about 4.5 microseconds per word when there are replacements, and 1 microsecond per word when there are not.

--- TallyCore/Global/Utility/StringUtility.cs
+++ TallyCore/Global/Utility/StringUtility.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Text;
 
 namespace NetTally.Utility
 {
@@ -21,5 +22,158 @@ namespace NetTally.Utility
         /// <returns>Returns true if the name starts with the plan name marker.</returns>
         public static bool IsPlanName(this string name) => name?.StartsWith(PlanNameMarker, StringComparison.Ordinal) ?? false;
 
+        /// <summary>
+        /// Lookup table for non-latin characters that doesn't require string.Normalize (which isn't available
+        /// in .NET Standard below 2.0).
+        /// </summary>
+        static Dictionary<string, string> nonlatin_characters = new Dictionary<string, string>
+        {
+            { "äæǽ", "ae" },
+            { "öœ", "oe" },
+            { "ü", "ue" },
+            { "Ä", "Ae" },
+            { "Ü", "Ue" },
+            { "Ö", "Oe" },
+            { "ÀÁÂÃÄÅǺĀĂĄǍΑΆẢẠẦẪẨẬẰẮẴẲẶА", "A" },
+            { "àáâãåǻāăąǎªαάảạầấẫẩậằắẵẳặа", "a" },
+            { "Б", "B" },
+            { "б", "b" },
+            { "ÇĆĈĊČ", "C" },
+            { "çćĉċč", "c" },
+            { "Д", "D" },
+            { "д", "d" },
+            { "ÐĎĐΔ", "Dj" },
+            { "ðďđδ", "dj" },
+            { "ÈÉÊËĒĔĖĘĚΕΈẼẺẸỀẾỄỂỆЕЭ", "E" },
+            { "èéêëēĕėęěέεẽẻẹềếễểệеэ", "e" },
+            { "Ф", "F" },
+            { "ф", "f" },
+            { "ĜĞĠĢΓГҐ", "G" },
+            { "ĝğġģγгґ", "g" },
+            { "ĤĦ", "H" },
+            { "ĥħ", "h" },
+            { "ÌÍÎÏĨĪĬǏĮİΗΉΊΙΪỈỊИЫ", "I" },
+            { "ìíîïĩīĭǐįıηήίιϊỉịиыї", "i" },
+            { "Ĵ", "J" },
+            { "ĵ", "j" },
+            { "ĶΚК", "K" },
+            { "ķκк", "k" },
+            { "ĹĻĽĿŁΛЛ", "L" },
+            { "ĺļľŀłλл", "l" },
+            { "М", "M" },
+            { "м", "m" },
+            { "ÑŃŅŇΝН", "N" },
+            { "ñńņňŉνн", "n" },
+            { "ÒÓÔÕŌŎǑŐƠØǾΟΌΩΏỎỌỒỐỖỔỘỜỚỠỞỢО", "O" },
+            { "òóôõōŏǒőơøǿºοόωώỏọồốỗổộờớỡởợо", "o" },
+            { "П", "P" },
+            { "п", "p" },
+            { "ŔŖŘΡР", "R" },
+            { "ŕŗřρр", "r" },
+            { "ŚŜŞȘŠΣС", "S" },
+            { "śŝşșšſσςсʒ", "s" },
+            { "ȚŢŤŦτТ", "T" },
+            { "țţťŧт", "t" },
+            { "ÙÚÛŨŪŬŮŰŲƯǓǕǗǙǛŨỦỤỪỨỮỬỰУ", "U" },
+            { "ùúûũūŭůűųưǔǖǘǚǜυύϋủụừứữửựу", "u" },
+            { "ÝŸŶΥΎΫỲỸỶỴЙ", "Y" },
+            { "ýÿŷỳỹỷỵй", "y" },
+            { "В", "V" },
+            { "в", "v" },
+            { "Ŵ", "W" },
+            { "ŵ", "w" },
+            { "ŹŻŽΖЗ", "Z" },
+            { "źżžζз", "z" },
+            { "ÆǼ", "AE" },
+            { "ß", "ss" },
+            { "Ĳ", "IJ" },
+            { "ĳ", "ij" },
+            { "Œ", "OE" },
+            { "ƒ", "f" },
+            { "ξ", "ks" },
+            { "π", "p" },
+            { "β", "v" },
+            { "μ", "m" },
+            { "ψ", "ps" },
+            { "Ё", "Yo" },
+            { "ё", "yo" },
+            { "Є", "Ye" },
+            { "є", "ye" },
+            { "Ї", "Yi" },
+            { "Ж", "Zh" },
+            { "ж", "zh" },
+            { "Х", "Kh" },
+            { "х", "kh" },
+            { "Ц", "Ts" },
+            { "ц", "ts" },
+            { "Ч", "Ch" },
+            { "ч", "ch" },
+            { "Ш", "Sh" },
+            { "ш", "sh" },
+            { "Щ", "Shch" },
+            { "щ", "shch" },
+            { "ЪъЬь", "" },
+            { "Ю", "Yu" },
+            { "ю", "yu" },
+            { "Я", "Ya" },
+            { "я", "ya" },
+        };
+
+        /// <summary>
+        /// Return the simplified form of a character after removing diacriticals.
+        /// </summary>
+        /// <param name="c">The character to transform.</param>
+        /// <returns>Returns the simplified latin form of a given character.</returns>
+        public static string RemoveDiacritics(this char c)
+        {
+            try
+            {
+                UInt16 cb = Convert.ToUInt16(c);
+                
+                // Latin characters get returned as-is.
+                if (cb < 128)
+                    return c.ToString();
+
+                // Spacing and combining characters are elided.
+                if (cb >= 0x2b0 && cb <= 0x36f)
+                    return string.Empty;
+
+                // Other characters are checked against the above dictionary,
+                // and converted to a representative latin value.
+                foreach (KeyValuePair<string, string> entry in nonlatin_characters)
+                {
+                    if (entry.Key.IndexOf(c) != -1)
+                    {
+                        return entry.Value;
+                    }
+                }
+            }
+            catch (OverflowException)
+            {
+                // Shouldn't be possible, since chars are stored in 16-bit values.
+                return string.Empty;
+            }
+
+            return c.ToString();
+        }
+
+        /// <summary>
+        /// Return the simplified latin form of a string, after removing diacriticals.
+        /// </summary>
+        /// <param name="s">The string to transform.</param>
+        /// <returns></returns>
+        public static string RemoveDiacritics(this string s)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (char c in s)
+            {
+                var r = c.RemoveDiacritics();
+                if (!string.IsNullOrEmpty(r))
+                    sb.Append(r);
+            }
+
+            return sb.ToString();
+        }
     }
 }
---
+++ TallyUnitTest/UtilityTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NetTally.Utility;
+
+namespace NetTally.Tests
+{
+    [TestClass]
+    public class UtilityTests
+    {
+        [TestMethod]
+        public void RemoveDiacriticals()
+        {
+            using (new RegionProfiler("diacriticals1"))
+            {
+                Assert.AreEqual("resume", "resume".RemoveDiacritics());
+            }
+
+            using (new RegionProfiler("diacriticals2 (17)"))
+            {
+                Assert.AreEqual("resume", "résumé".RemoveDiacritics());
+                Assert.AreEqual("Resume", "Résumé".RemoveDiacritics());
+                Assert.AreEqual("Comme ci, comme ca", "Comme ci, comme ça".RemoveDiacritics());
+                Assert.AreEqual("habia", "había".RemoveDiacritics());
+                Assert.AreEqual("naive", "naïve".RemoveDiacritics());
+                Assert.AreEqual("NAIVE", "NAİVE".RemoveDiacritics());
+                Assert.AreEqual("hrabe", "hrábě".RemoveDiacritics());
+                Assert.AreEqual("normal", "normal".RemoveDiacritics());
+                Assert.AreEqual("Elaeudanla Teiteia", "Elaeudanla Téïtéïa".RemoveDiacritics());
+                Assert.AreEqual("Laetitia", "Lætitia".RemoveDiacritics());
+                Assert.AreEqual("AEsir", "Æsir".RemoveDiacritics());
+                Assert.AreEqual("AEro", "Ærø".RemoveDiacritics());
+                Assert.AreEqual("aetate", "ætate".RemoveDiacritics());
+                Assert.AreEqual("Wasserschloss", "Waſſerschloſʒ".RemoveDiacritics());
+                Assert.AreEqual("Wasserschloss", "Wasserschloß".RemoveDiacritics());
+                Assert.AreEqual("beissen", "beißen".RemoveDiacritics());
+                Assert.AreEqual("gruessen", "grüßen".RemoveDiacritics());
+            }
+
+            using (new RegionProfiler("diacriticals3"))
+            {
+                Assert.AreEqual("While the snow and wind were blocked by the ward around the area, the blizzard still raging outside of the transparent barrier, that just made things more tolerable rather than warm. The cold never bothered me anyway, at least at this level, but Bao-Bao looked as though warming up before getting into a dangerous situation might be for the best.",
+                    "While the snow and wind were blocked by the ward around the area, the blizzard still raging outside of the transparent barrier, that just made things more tolerable rather than warm. The cold never bothered me anyway, at least at this level, but Bao-Bao looked as though warming up before getting into a dangerous situation might be for the best.".RemoveDiacritics());
+            }
+        }
+    }
+}
