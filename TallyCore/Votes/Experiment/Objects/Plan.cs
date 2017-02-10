4005887b5f56cd244f32c09cfb8b1a646fa7d09c
David Smith <dsmith@datasync.com>
2017-02-10 02:02:01 -0600

    Various tidying and simplification.

--- TallyCore/Votes/Experiment/Objects/Plan.cs
+++ TallyCore/Votes/Experiment/Objects/Plan.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
-using NetTally.Extensions;
-using NetTally.Utility;
 
 namespace NetTally.Votes.Experiment
 {
@@ -12,23 +10,92 @@ namespace NetTally.Votes.Experiment
     /// </summary>
     public class Plan
     {
-        #region Constructor
-        public Plan(PlanType planType, string name, Identity identity, List<VoteLine> lines)
-        {
-            if (string.IsNullOrEmpty(name))
-                throw new ArgumentNullException(nameof(name));
-
-            Lines = lines ?? throw new ArgumentNullException(nameof(lines));
-
-            PlanType = planType;
-            Identity = new Identity(name, identity, IdentityType.Plan);
-        }
-        #endregion
-
         #region Properties
         public Identity Identity { get; }
         public PlanType PlanType { get; }
-        public List<VoteLine> Lines { get; }
+        public VotePartition Content { get; }
+
+        private List<VotePartition> partitionedContent = new List<VotePartition>();
+        public IReadOnlyList<VotePartition> PartitionedContent { get { return partitionedContent; } }
+        #endregion
+
+        #region Constructor        
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Plan"/> class.
+        /// </summary>
+        /// <param name="planName">Name of the plan.</param>
+        /// <param name="sourceIdentity">The identity of the originating post.</param>
+        /// <param name="content">The content (<see cref="VotePartition"/>) of the plan.</param>
+        /// <param name="planType">Type of the plan.</param>
+        public Plan(string planName, Identity sourceIdentity, VotePartition content, PlanType planType)
+        {
+            if (string.IsNullOrEmpty(planName))
+                throw new ArgumentNullException(nameof(planName));
+
+            Identity = new Identity(planName, sourceIdentity, IdentityType.Plan);
+            Content = content ?? throw new ArgumentNullException(nameof(content));
+            PlanType = planType;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Plan" /> class.
+        /// </summary>
+        /// <param name="identity">The identity object for the plan.</param>
+        /// <param name="content">The content (<see cref="VotePartition"/>) of the plan.</param>
+        /// <param name="planType">Type of the plan.</param>
+        public Plan(Identity identity, VotePartition content, PlanType planType)
+        {
+            Identity = identity ?? throw new ArgumentNullException(nameof(identity));
+            Content = content ?? throw new ArgumentNullException(nameof(content));
+            PlanType = planType;
+        }
+        #endregion
+
+        #region Public methods        
+        /// <summary>
+        /// Sets the partitions that the content was broken into, based on partition mode settings.
+        /// </summary>
+        /// <param name="partitions">The partitions to store.</param>
+        public void SetContentPartitions(IEnumerable<VotePartition> partitions)
+        {
+            partitionedContent.Clear();
+            partitionedContent.AddRange(partitions);
+        }
+        #endregion
+
+        #region Equality comparisons
+        public override bool Equals(object obj)
+        {
+            if (obj is Plan otherPlan)
+            {
+                return Identity == otherPlan.Identity && Content == otherPlan.Content;
+            }
+
+            return false;
+        }
+
+        public static bool operator ==(Plan left, Plan right)
+        {
+            if (ReferenceEquals(left, null))
+            {
+                return ReferenceEquals(right, null);
+            }
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Plan left, Plan right)
+        {
+            if (ReferenceEquals(left, null))
+            {
+                return !ReferenceEquals(right, null);
+            }
+            return !left.Equals(right);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
         #endregion
 
         #region Static class usage
@@ -63,7 +130,8 @@ namespace NetTally.Votes.Experiment
 
                 if (m.Groups["base"].Success && voteGroup.First().MarkerType == MarkerType.Vote && voteGroup.Count() > 1)
                 {
-                    plans.Add(new Plan(PlanType.Base, m.Groups["planname"].Value, vote.Post.Identity, voteGroup.ToList()));
+                    var partition = new VotePartition(voteGroup, VoteType.Plan);
+                    plans.Add(new Plan(m.Groups["planname"].Value, vote.Post.Identity, partition, PlanType.Base));
                     voteGrouping = voteGrouping.Skip(1);
                 }
                 else
@@ -85,12 +153,14 @@ namespace NetTally.Votes.Experiment
                     var labeledGroups = voteGrouping.TakeWhile(g => g.First().MarkerType == MarkerType.Vote ||
                         g.First().MarkerType == MarkerType.Approval);
 
-                    PlanType labelType = labeledGroups.Skip(1).Any() ? PlanType.Label : PlanType.SingleLine;
+                    var flattenedLines = labeledGroups.SelectMany(a => a).ToList();
+                    var partition = new VotePartition(flattenedLines, VoteType.Plan);
+
+                    PlanType planType = labeledGroups.Skip(1).Any() ? PlanType.Label : PlanType.SingleLine;
 
                     voteGrouping = voteGrouping.Skip(labeledGroups.Count());
 
-                    plans.Add(new Plan(labelType, m.Groups["planname"].Value, vote.Post.Identity,
-                        labeledGroups.SelectMany(a => a).ToList()));
+                    plans.Add(new Plan(m.Groups["planname"].Value, vote.Post.Identity, partition, planType));
                 }
             }
 
@@ -100,7 +170,8 @@ namespace NetTally.Votes.Experiment
                 Match m = anyPlanRegex.Match(voteGroup.Key);
                 if (m.Success && voteGroup.Skip(1).Any())
                 {
-                    plans.Add(new Plan(PlanType.Content, m.Groups["planname"].Value, vote.Post.Identity, voteGroup.ToList()));
+                    var partition = new VotePartition(voteGroup, VoteType.Plan);
+                    plans.Add(new Plan(m.Groups["planname"].Value, vote.Post.Identity, partition, PlanType.Content));
                 }
             }
 
@@ -108,44 +179,6 @@ namespace NetTally.Votes.Experiment
 
             // Return all collected plans
             return plans;
-        }
-        #endregion
-
-        #region Equality comparisons
-        public override bool Equals(object obj)
-        {
-            if (obj is Plan otherPlan)
-            {
-                if (Lines.Count != otherPlan.Lines.Count)
-                    return false;
-
-                return Lines.SequenceEqual(otherPlan.Lines);
-            }
-
-            return false;
-        }
-
-        public static bool operator ==(Plan left, Plan right)
-        {
-            if (ReferenceEquals(left, null))
-            {
-                return ReferenceEquals(right, null);
-            }
-            return left.Equals(right);
-        }
-
-        public static bool operator !=(Plan left, Plan right)
-        {
-            if (ReferenceEquals(left, null))
-            {
-                return !ReferenceEquals(right, null);
-            }
-            return !left.Equals(right);
-        }
-
-        public override int GetHashCode()
-        {
-            return base.GetHashCode();
         }
         #endregion
 
