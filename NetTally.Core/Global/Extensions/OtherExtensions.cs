81742d7024cf4c3a862cb0fc8982ddb1b4f9900e
David Smith <dsmith@datasync.com>
2017-04-12 11:10:45 -0500

    Fix the TimeoutAfter function to take a CancellationToken so that it can be recognize being manually cancelled by the user.

--- NetTally.Core/Global/Extensions/OtherExtensions.cs
+++ NetTally.Core/Global/Extensions/OtherExtensions.cs
@@ -18,20 +18,29 @@ namespace NetTally.Extensions
         /// <typeparam name="TResult">The return type of the task being awaited.</typeparam>
         /// <param name="task">The task to await.</param>
         /// <param name="timeout">The timeout to wait for the task to complete.</param>
+        /// <param name="token">A cancellation token for user-initiated cancellations.</param>
         /// <returns>Returns the awaited task, if it completed in less than the timeout period.</returns>
-        public static async Task<TResult> TimeoutAfter<TResult>(this Task<TResult> task, TimeSpan timeout)
+        public static async Task<TResult> TimeoutAfter<TResult>(this Task<TResult> task, TimeSpan timeout, CancellationToken token)
         {
-            var timeoutCancellationTokenSource = new CancellationTokenSource();
+            using (CancellationTokenSource timeoutTokenSource = new CancellationTokenSource(timeout))
+            {
+                using (CancellationTokenSource linkedTokenSource = CancellationTokenSource.CreateLinkedTokenSource(token, timeoutTokenSource.Token))
+                {
+                    var completedTask = await Task.WhenAny(task, Task.Delay(timeout, linkedTokenSource.Token));
 
-            var completedTask = await Task.WhenAny(task, Task.Delay(timeout, timeoutCancellationTokenSource.Token));
-            if (completedTask == task)
-            {
-                timeoutCancellationTokenSource.Cancel();
-                return await task;  // Very important in order to propagate exceptions
-            }
-            else
-            {
-                throw new TimeoutException("The operation has timed out.");
+                    if (completedTask == task)
+                    {
+                        return await task;  // Very important in order to propagate exceptions
+                    }
+                    else if (token.IsCancellationRequested)
+                    {
+                        throw new OperationCanceledException(token);
+                    }
+                    else
+                    {
+                        throw new TimeoutException("The operation has timed out.");
+                    }
+                }
             }
         }
 
