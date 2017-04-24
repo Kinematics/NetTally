b32366c3d838263a02bf0b12736905f1d09b5c73
David Smith <dsmith@datasync.com>
2017-04-24 15:40:50 -0500

    Update version to 1.8.4

--- NetTally/Properties/AssemblyInfo.cs
+++ NetTally/Properties/AssemblyInfo.cs
@@ -52,7 +52,7 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.8.0.0")]
-[assembly: AssemblyFileVersion("1.8.3.0")]
-[assembly: AssemblyInformationalVersion("1.8.3")]
+[assembly: AssemblyFileVersion("1.8.4.0")]
+[assembly: AssemblyInformationalVersion("1.8.4")]
 [assembly: NeutralResourcesLanguage("en")]
 
