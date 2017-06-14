1c593bf2ed28666a99636c396be081066aaee6be
David Smith <dsmith@datasync.com>
2017-06-14 15:59:57 -0500

    Add a class for native method calls to the OS.

---
+++ NetTally/NativeMethods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace NetTally
+{
+    internal class NativeMethods
+    {
+        public const int HWND_BROADCAST = 0xffff;
+        public static readonly int WM_NETTALLYUPDATE = RegisterWindowMessage("WM_NETTALLYUPDATE");
+
+        [DllImport("user32")]
+        public static extern bool PostMessage(IntPtr hwnd, int msg, IntPtr wparam, IntPtr lparam);
+        [DllImport("user32")]
+        public static extern int RegisterWindowMessage(string message);
+    }
+}
