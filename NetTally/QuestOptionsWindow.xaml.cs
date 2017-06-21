756323e43b47225ef45b6cef412947c25c7b6612
David Smith <dsmith@datasync.com>
2017-06-21 18:13:49 -0500

    Move code from QuestOptionsNew to original QuestOptionsWindow.

--- NetTally/QuestOptionsWindow.xaml.cs
+++ NetTally/QuestOptionsWindow.xaml.cs
@@ -12,6 +12,29 @@ namespace NetTally
             InitializeComponent();
         }
 
+        private void resetFiltersButton_Click(object sender, RoutedEventArgs e)
+        {
+            useCustomPostFilters.IsChecked = false;
+            useCustomTaskFilters.IsChecked = false;
+            useCustomThreadmarkFilters.IsChecked = false;
+            useCustomUsernameFilters.IsChecked = false;
+
+            customPostFilters.Text = "";
+            customTaskFilters.Text = "";
+            customThreadmarkFilters.Text = "";
+            customUsernameFilters.Text = "";
+        }
+
+        private void resetOptionsButton_Click(object sender, RoutedEventArgs e)
+        {
+            forbidVoteLabelPlanNames.IsChecked = false;
+            whitespaceAndPunctuationIsSignificant.IsChecked = false;
+            disableProxyVotes.IsChecked = false;
+            forcePinnedProxyVotes.IsChecked = false;
+            ignoreSpoilers.IsChecked = false;
+            trimExtendedText.IsChecked = false;
+        }
+
         private void closeButton_Click(object sender, RoutedEventArgs e)
         {
             Close();
