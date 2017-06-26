62706ffc9aad180183c47d81875d34025aef1fd3
David Smith <dsmith@datasync.com>
2017-06-26 01:55:33 -0500

    Update version to 1.9.9.

--- NetTally/Properties/AssemblyInfo.cs
+++ NetTally/Properties/AssemblyInfo.cs
@@ -52,7 +52,7 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.9.0.0")]
-[assembly: AssemblyFileVersion("1.9.8.0")]
-[assembly: AssemblyInformationalVersion("1.9.8")]
+[assembly: AssemblyFileVersion("1.9.9.0")]
+[assembly: AssemblyInformationalVersion("1.9.9")]
 [assembly: NeutralResourcesLanguage("en")]
 
