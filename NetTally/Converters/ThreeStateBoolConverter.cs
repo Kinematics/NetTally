8466b3bf3800e7761cf4fd5c7937bad16453634e
David Smith <dsmith@datasync.com>
2017-06-28 17:44:37 -0500

    Add a converter for converting between bool? and BoolEx.

---
+++ NetTally/Converters/ThreeStateBoolConverter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace NetTally.Converters
+{
+    [ValueConversion(typeof(BoolEx), typeof(bool?))]
+    public class ThreeStateBoolConverter : IValueConverter
+    {
+        /// <summary>
+        /// Convert from source (property BoolEx) to target (control bool?).
+        /// </summary>
+        /// <returns>Returns whether the specified target control value should be on or off.</returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            switch (value)
+            {
+                case BoolEx b:
+                    switch (b)
+                    {
+                        case BoolEx.True:
+                            return true;
+                        case BoolEx.False:
+                            return false;
+                        default:
+                            return null;
+                    }
+                default:
+                    throw new ArgumentException("Value is not a BoolEx.");
+            }
+        }
+
+        /// <summary>
+        /// Convert from target (control bool?) to source (property BoolEx).
+        /// </summary>
+        /// <returns>Returns what the source property value should be set to based on the target value.</returns>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            switch (value)
+            {
+                case null:
+                    return BoolEx.Unknown;
+                case bool b:
+                    return b ? BoolEx.True : BoolEx.False;
+                default:
+                    throw new ArgumentException("Value is not a bool.");
+            }
+        }
+    }
+}
