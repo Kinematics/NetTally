cf17f8342ef9b5b9be602b98979fa77fc54c47d5
David Smith <dsmith@datasync.com>
2017-06-29 21:16:21 -0500

    Update version to 1.9.10

--- NetTally.Console/Properties/AssemblyInfo.cs
+++ NetTally.Console/Properties/AssemblyInfo.cs
@@ -24,7 +24,7 @@ using System.Resources;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.9.0.0")]
-[assembly: AssemblyFileVersion("1.9.9.0")]
-[assembly: AssemblyInformationalVersion("1.9.9")]
+[assembly: AssemblyFileVersion("1.9.10.0")]
+[assembly: AssemblyInformationalVersion("1.9.10")]
 [assembly: NeutralResourcesLanguage("en")]
 
