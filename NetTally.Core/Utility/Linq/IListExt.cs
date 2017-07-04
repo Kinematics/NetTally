11662343f1a1921190adc9883f189858aa2e069b
David Smith <dsmith@datasync.com>
2017-07-03 23:14:44 -0500

    Add comments. Tidy.

--- NetTally.Core/Utility/Linq/IListExt.cs
+++ NetTally.Core/Utility/Linq/IListExt.cs
@@ -5,11 +5,14 @@ namespace NetTally.Extensions
 {
     public static class IListExt
     {
-        public static void Swap<T>(
-            this IList<T> list,
-            int firstIndex,
-            int secondIndex
-            )
+        /// <summary>
+        /// Swap two values in a list.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list">The list to swap data in.</param>
+        /// <param name="firstIndex">The first index value being swapped.</param>
+        /// <param name="secondIndex">The second index value being swapped.</param>
+        public static void Swap<T>(this IList<T> list, int firstIndex, int secondIndex)
         {
             if (list == null)
                 throw new ArgumentNullException(nameof(list));
@@ -18,6 +21,7 @@ namespace NetTally.Extensions
                 return;
             if (firstIndex < 0 || firstIndex >= list.Count || secondIndex < 0 || secondIndex >= list.Count)
                 return;
+
             T temp = list[firstIndex];
             list[firstIndex] = list[secondIndex];
             list[secondIndex] = temp;
