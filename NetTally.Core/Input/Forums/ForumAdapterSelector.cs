d7d7e6e2becdc1ceb1486bb2047c09bcb96ae1a7
David Smith <dsmith@datasync.com>
2019-03-20 16:44:15 -0500

    Add a function to allow getting a forum adapter based on a forum type, in addition to getting one based on URI.

--- NetTally.Core/Input/Forums/ForumAdapterSelector.cs
+++ NetTally.Core/Input/Forums/ForumAdapterSelector.cs
@@ -11,15 +11,49 @@ namespace NetTally.Forums
     /// </summary>
     class ForumAdapterSelector
     {
+        /// <summary>
+        /// A dictionary cache for created forum adapters.
+        /// </summary>
         static readonly Dictionary<ForumType, IForumAdapter> cachedAdapters = new Dictionary<ForumType, IForumAdapter>();
 
         /// <summary>
-        /// Gets a forum adapter to match the provided forum type and Uri.
+        /// Get a forum adapter to match the provided forum type.
         /// </summary>
         /// <param name="forumType">The type of forum being requested.</param>
+        /// <returns>Returns a forum adapter matching the requested forum type.</returns>
+        public static IForumAdapter GetForumAdapter(ForumType forumType)
+        {
+            if (!cachedAdapters.TryGetValue(forumType, out IForumAdapter adapter))
+            {
+                switch (forumType)
+                {
+                    case ForumType.XenForo1:
+                        adapter = new XenForo1Adapter(Utility.DefaultUri.Default);
+                        break;
+                    case ForumType.vBulletin3:
+                        adapter = new vBulletin3Adapter(Utility.DefaultUri.Default);
+                        break;
+                    case ForumType.vBulletin4:
+                        adapter = new vBulletin4Adapter(Utility.DefaultUri.Default);
+                        break;
+                    case ForumType.vBulletin5:
+                        adapter = new vBulletin5Adapter(Utility.DefaultUri.Default);
+                        break;
+                    default:
+                        throw new ArgumentException($"Unknown forum type: {forumType}", nameof(forumType));
+                }
+
+                cachedAdapters[forumType] = adapter;
+            }
+
+            return adapter;
+        }
+
+        /// <summary>
+        /// Gets a forum adapter to match the provided Uri.
+        /// </summary>
         /// <param name="site">The Uri of the site the adapter is for.</param>
-        /// <returns>Returns a forum adapter matching the forum type, initialized to the provided Uri.</returns>
-        //public static IForumAdapter GetForumAdapter(ForumType forumType, Uri site)
+        /// <returns>Returns a forum adapter matching the forum type of the page at the provided Uri.</returns>
         public static async Task<IForumAdapter> GetForumAdapterAsync(Uri? site, CancellationToken token)
         {
             if (site == null)
