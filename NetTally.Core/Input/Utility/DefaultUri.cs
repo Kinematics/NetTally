93ade7cbfdb617725e09f9f1e4b8afe9e35d8b4d
David Smith <dsmith@datasync.com>
2019-03-20 16:43:32 -0500

    A default Uri that can be used in the program.

---
+++ NetTally.Core/Input/Utility/DefaultUri.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NetTally.Utility
+{
+    /// <summary>
+    /// A static class to allow setting a 'default' Uri where needed.
+    /// </summary>
+    public static class DefaultUri
+    {
+        /// <summary>
+        /// The static default Uri to be used in the program.
+        /// </summary>
+        public static Uri Default = new Uri(@"http://example.com/");
+    }
+}
