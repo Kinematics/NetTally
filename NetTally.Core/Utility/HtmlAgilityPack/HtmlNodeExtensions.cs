ed066c379504e425021eb045f04a1c4fe6de41d4
David Smith <dsmith@datasync.com>
2019-04-27 02:09:57 -0500

    Update Get* functions with ability to handle a request for an element without any classes. Note that the HAP library now has its own HasClass function.

--- NetTally.Core/Utility/HtmlAgilityPack/HtmlNodeExtensions.cs
+++ NetTally.Core/Utility/HtmlAgilityPack/HtmlNodeExtensions.cs
@@ -26,6 +26,7 @@ namespace NetTally.Extensions
         /// or
         /// class
         /// </exception>
+        [Obsolete("HAP library has implementation of this function.")]
         public static bool HasClass(this HtmlNode node, string @class)
         {
             if (string.IsNullOrEmpty(@class))
@@ -41,30 +42,38 @@ namespace NetTally.Extensions
         /// <param name="node">The object the extension method is being used on.</param>
         /// <param name="element">The name of the element we're looking for.
         /// If it's null or empty, any element will work.</param>
-        /// <param name="@class">The name of the class to search for.  Must be provided.</param>
+        /// <param name="@class">The name of the class to search for.  If this is null or empty,
+        /// searches for elements without any classes at all.</param>
         /// <returns>Returns the element with the specified class, if found.  Otherwise, null.</returns>
-        /// <exception cref="ArgumentNullException">If @class is null or empty.</exception>
         public static HtmlNode? GetChildWithClass(this HtmlNode node, string? element, string @class)
         {
-            if (string.IsNullOrEmpty(@class))
-                throw new ArgumentNullException(nameof(@class));
-
             IEnumerable<HtmlNode> children;
             if (string.IsNullOrEmpty(element))
                 children = node.ChildNodes;
             else
                 children = node.Elements(element);
 
-            return children.FirstOrDefault(n => n.HasClass(@class));
+            HtmlNode find;
+
+            if (string.IsNullOrEmpty(@class))
+            {
+                find = children.FirstOrDefault(n => string.IsNullOrEmpty(n.GetAttributeValue("class", "").Trim()));
+            }
+            else
+            {
+                find = children.FirstOrDefault(n => n.HasClass(@class));
+            }
+
+            return find;
         }
 
         /// <summary>
         /// Overloaded version to not require specifying the element name.
         /// </summary>
         /// <param name="node">The object the extension method is being used on.</param>
-        /// <param name="@class">The name of the class to search for.  Must be provided.</param>
+        /// <param name="@class">The name of the class to search for. If this is null or empty,
+        /// searches for elements without any classes at all.</param>
         /// <returns>Returns the element with the specified class, if found.  Otherwise, null.</returns>
-        /// <exception cref="ArgumentNullException">If @class is null or empty.</exception>
         public static HtmlNode? GetChildWithClass(this HtmlNode node, string @class) => node.GetChildWithClass(null, @class);
 
         /// <summary>
@@ -73,30 +82,38 @@ namespace NetTally.Extensions
         /// <param name="node">The object the extension method is being used on.</param>
         /// <param name="element">The name of the element we're looking for.
         /// If it's null or empty, any element will work.</param>
-        /// <param name="@class">The name of the class to search for.  Must be provided.</param>
+        /// <param name="@class">The name of the class to search for.  If this is null or empty,
+        /// searches for elements without any classes at all.</param>
         /// <returns>Returns a list with all found elements.</returns>
-        /// <exception cref="ArgumentNullException">If @class is null or empty.</exception>
         public static IEnumerable<HtmlNode> GetChildrenWithClass(this HtmlNode node, string? element, string @class)
         {
-            if (string.IsNullOrEmpty(@class))
-                throw new ArgumentNullException(nameof(@class));
-
             IEnumerable<HtmlNode> children;
             if (string.IsNullOrEmpty(element))
                 children = node.ChildNodes;
             else
                 children = node.Elements(element);
 
-            return children.Where(n => n.HasClass(@class));
+            IEnumerable<HtmlNode> find;
+
+            if (string.IsNullOrEmpty(@class))
+            {
+                find = children.Where(n => string.IsNullOrEmpty(n.GetAttributeValue("class", "").Trim()));
+            }
+            else
+            {
+                find = children.Where(n => n.HasClass(@class));
+            }
+
+            return find;
         }
 
         /// <summary>
         /// Overloaded version to not require specifying the element name.
         /// </summary>
         /// <param name="node">The object the extension method is being used on.</param>
-        /// <param name="@class">The name of the class to search for.  Must be provided.</param>
+        /// <param name="@class">The name of the class to search for.  If this is null or empty,
+        /// searches for elements without any classes at all.</param>
         /// <returns>Returns a list with all found elements.</returns>
-        /// <exception cref="ArgumentNullException">If @class is null or empty.</exception>
         public static IEnumerable<HtmlNode> GetChildrenWithClass(this HtmlNode node, string @class) => node.GetChildrenWithClass(null, @class);
 
         /// <summary>
@@ -106,30 +123,38 @@ namespace NetTally.Extensions
         /// <param name="node">The object the extension method is being used on.</param>
         /// <param name="element">The name of the element we're looking for.
         /// If it's null or empty, any element will work.</param>
-        /// <param name="@class">The name of the class to search for.  Must be provided.</param>
+        /// <param name="@class">The name of the class to search for.  If this is null or empty,
+        /// searches for elements without any classes at all.</param>
         /// <returns>Returns the element with the specified class, if found.  Otherwise, null.</returns>
-        /// <exception cref="ArgumentNullException">If @class is null or empty.</exception>
         public static HtmlNode? GetDescendantWithClass(this HtmlNode node, string? element, string @class)
         {
-            if (string.IsNullOrEmpty(@class))
-                throw new ArgumentNullException(nameof(@class));
-
             IEnumerable<HtmlNode> children;
             if (string.IsNullOrEmpty(element))
                 children = node.Descendants();
             else
                 children = node.Descendants(element);
 
-            return children.FirstOrDefault(n => n.HasClass(@class));
+            HtmlNode find;
+
+            if (string.IsNullOrEmpty(@class))
+            {
+                find = children.FirstOrDefault(n => string.IsNullOrEmpty(n.GetAttributeValue("class", "").Trim()));
+            }
+            else
+            {
+                find = children.FirstOrDefault(n => n.HasClass(@class));
+            }
+
+            return find;
         }
 
         /// <summary>
         /// Overloaded version to not require specifying the element name.
         /// </summary>
         /// <param name="node">The object the extension method is being used on.</param>
-        /// <param name="@class">The name of the class to search for.  Must be provided.</param>
+        /// <param name="@class">The name of the class to search for.  If this is null or empty,
+        /// searches for elements without any classes at all.</param>
         /// <returns>Returns the element with the specified class, if found.  Otherwise, null.</returns>
-        /// <exception cref="ArgumentNullException">If @class is null or empty.</exception>
         public static HtmlNode? GetDescendantWithClass(this HtmlNode node, string @class) => node.GetDescendantWithClass(null, @class);
 
         /// <summary>
@@ -143,25 +168,33 @@ namespace NetTally.Extensions
         /// <exception cref="ArgumentNullException">If @class is null or empty.</exception>
         public static IEnumerable<HtmlNode> GetDescendantsWithClass(this HtmlNode node, string? element, string @class)
         {
-            if (string.IsNullOrEmpty(@class))
-                throw new ArgumentNullException(nameof(@class));
-
             IEnumerable<HtmlNode> children;
             if (string.IsNullOrEmpty(element))
                 children = node.Descendants();
             else
                 children = node.Descendants(element);
 
-            return children.Where(n => n.HasClass(@class));
+            IEnumerable<HtmlNode> find;
+
+            if (string.IsNullOrEmpty(@class))
+            {
+                find = children.Where(n => string.IsNullOrEmpty(n.GetAttributeValue("class", "").Trim()));
+            }
+            else
+            {
+                find = children.Where(n => n.HasClass(@class));
+            }
+
+            return find;
         }
 
         /// <summary>
         /// Overloaded version to not require specifying the element name.
         /// </summary>
         /// <param name="node">The object the extension method is being used on.</param>
-        /// <param name="@class">The name of the class to search for.  Must be provided.</param>
+        /// <param name="@class">The name of the class to search for.  If this is null or empty,
+        /// searches for elements without any classes at all.</param>
         /// <returns>Returns a list with all found elements.</returns>
-        /// <exception cref="ArgumentNullException">If @class is null or empty.</exception>
         public static IEnumerable<HtmlNode> GetDescendantsWithClass(this HtmlNode node, string @class) => node.GetDescendantsWithClass(null, @class);
 
     }
