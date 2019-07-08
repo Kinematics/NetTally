df9edd872fcb4e3dd30fa20d6e7e36e18c004e24
David Smith <dsmith@datasync.com>
2019-07-08 18:33:46 -0500

    Add extension function for dictionaries.

--- NetTally.Core/Utility/Linq/CollectionsExt.cs
+++ NetTally.Core/Utility/Linq/CollectionsExt.cs
@@ -47,5 +47,10 @@ namespace NetTally.Extensions
                     collection.Move(src, i);
             }
         }
+
+        public static TValue? GetValueOrDefault<TKey, TValue> (this IDictionary<TKey, TValue> dictionary, TKey key) where TValue : class
+        {
+            return dictionary.TryGetValue(key, out TValue value) ? value : default;
+        }
     }
 }
