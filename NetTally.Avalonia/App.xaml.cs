cbfd7c6e752feea7cf35179add975f741209abdc
David Smith <dsmith@datasync.com>
2019-07-15 23:37:20 -0500

    Add basic project for Avalonia testing branch.

---
+++ NetTally.Avalonia/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Avalonia;
+using Avalonia.Markup.Xaml;
+
+namespace NetTally.Avalonia
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public override void Initialize()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
---
+++ NetTally.Avalonia/Views/UserControl1.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace NetTally.Avalonia.Views
+{
+    public class UserControl1 : UserControl
+    {
+        public UserControl1()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
