f5b379afa4766f33b59678d58049ae5938cd155a
David Smith <dsmith@datasync.com>
2019-07-23 02:00:20 -0500

    Rewrite console to avoid async issues, and update missing options. Allow it to call the ViewModel's DoRunTallyAsync function to run the tally.

--- NetTally.Console/Program.cs
+++ NetTally.Console/Program.cs
@@ -2,88 +2,127 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Threading;
+using System.Threading.Tasks;
 using NetTally.CustomEventArgs;
 using NetTally.ViewModels;
 using CommandLine;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace NetTally.CLI
 {
     class Program
     {
         #region Variables
-        static ManualResetEventSlim waiting = new ManualResetEventSlim(false);
-        static bool verbose = false;
+        static bool verbose;
 
         static IServiceProvider serviceProvider;
+        static Microsoft.Extensions.Logging.ILogger logger;
         #endregion
 
         #region Main entry point
+        /// <summary>
+        /// Main entry point for the console application.
+        /// </summary>
+        /// <param name="args">Arguments passed to the application.</param>
         static void Main(string[] args)
         {
             // Create a service collection and configure our dependencies
             var serviceCollection = new ServiceCollection();
+
             // Get the services provided by the core library.
-            NetTally.Startup.ConfigureServices(serviceCollection);
+            Startup.ConfigureServices(serviceCollection, LogLevel.Trace);
 
             // Build the IServiceProvider and set our reference to it
             serviceProvider = serviceCollection.BuildServiceProvider();
 
+            // Get a logger for debugging.
+            var loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
+            logger = loggerFactory.CreateLogger<Program>();
+            logger.LogDebug("Services defined, starting console app!");
+
             serviceProvider.GetRequiredService<ViewModelService>();
 
-            var arguements = Parser.Default.ParseArguments<Options>(args);
+            ViewModelService.MainViewModel.PropertyChanged += MainViewModel_PropertyChanged;
+            logger.LogTrace("Watching events from the main view model.");
 
-            arguements.WithParsed(o => RunWithOptions(o));
+            var arguments = Parser.Default.ParseArguments<Options>(args);
+            logger.LogTrace("Options parsed.");
 
-#if DEBUG
-            //Console.WriteLine("Press enter to close...");
-            //Console.ReadLine();
-#endif
+            arguments.WithParsed(o => RunWithOptions(o));
         }
         #endregion
 
-        #region General running code
+        #region Configure and Run
         /// <summary>
-        /// Function that initiates the tally using the provided options.
+        /// Set things up to run the tally using the options provided.
         /// </summary>
-        /// <param name="options"></param>
-        private static void RunWithOptions(Options options)
+        /// <param name="options">The options that were parsed from the commandline arguments.</param>
+        public static void RunWithOptions(Options options)
         {
+            logger.LogTrace("Entered RunWithOptions");
+
             verbose = options.Verbose;
 
             SetGlobalOptions(options);
 
             Quest quest = GetQuestWithOptions(options);
 
+            logger.LogTrace("Options set");
+
             RunTally(quest);
 
-            // Wait for a PropertyChanged update signal indicating the tally was completed, or a timeout.
-            waiting.Wait(TimeSpan.FromSeconds(60));
+        }
 
-            if (!waiting.IsSet && !verbose)
+        /// <summary>
+        /// The code to actually call the ViewModel API to run the tally.
+        /// </summary>
+        /// <param name="quest">The quest being tallied.</param>
+        private static void RunTally(Quest quest)
+        {
+            Thread.Sleep(30);
+
+            bool canAddQuest = ViewModelService.MainViewModel.AddQuestCommand.CanExecute(null);
+            logger.LogTrace($"Can Add Quest: {canAddQuest} (TallyIsRunning: {ViewModelService.MainViewModel.TallyIsRunning})");
+
+            if (canAddQuest)
             {
-                Console.Write("Tally attempt failed.");
+                ViewModelService.MainViewModel.AddQuestCommand.Execute(quest);
+
+                logger.LogTrace("Quest added");
+
+                ViewModelService.MainViewModel.SelectedQuest = quest;
+
+                logger.LogTrace("Quest selected");
+
+                bool canRunTally = ViewModelService.MainViewModel.RunTallyCommand.CanExecute(null);
+                logger.LogTrace($"Can Run Tally: {canRunTally}");
+
+                if (canRunTally)
+                {
+                    logger.LogTrace("Running Tally...");
+                    ViewModelService.MainViewModel.DoRunTallyAsync(null).Wait();
+                }
             }
         }
 
         /// <summary>
-        /// Run the tally for the provided quest.
+        /// Sets the global service options on the main view model.
         /// </summary>
-        /// <param name="quest">The quest to run the tally on.</param>
-        private static void RunTally(Quest quest)
+        /// <param name="options">The commandline options set when run.</param>
+        private static void SetGlobalOptions(Options options)
         {
-            if (ViewModelService.MainViewModel.AddQuestCommand.CanExecute(null))
-            {
-                ViewModelService.MainViewModel.AddQuestCommand.Execute(quest);
-                ViewModelService.MainViewModel.SelectedQuest = quest;
+            logger.LogTrace("Setting global options");
 
-                ViewModelService.MainViewModel.PropertyChanged += MainViewModel_PropertyChanged;
+            ViewModelService.MainViewModel.Options.DisplayMode = options.DisplayMode;
 
-                if (ViewModelService.MainViewModel.RunTallyCommand.CanExecute(null))
-                {
-                    ViewModelService.MainViewModel.RunTallyCommand.Execute(null);
-                }
-            }
+            ViewModelService.MainViewModel.Options.GlobalSpoilers = options.SpoilerAll;
+
+            ViewModelService.MainViewModel.Options.DisplayPlansWithNoVotes = options.Display0Votes;
+
+            ViewModelService.MainViewModel.Options.DisableWebProxy = options.DisableWebProxy;
+
+            ViewModelService.MainViewModel.Options.DebugMode = options.Debug;
         }
 
         /// <summary>
@@ -93,18 +132,28 @@ namespace NetTally.CLI
         /// <returns>Returns a completed Quest instance.</returns>
         private static Quest GetQuestWithOptions(Options options)
         {
+            logger.LogTrace("Setting up quest with options.");
+
             Quest quest = new Quest()
             {
                 ThreadName = options.Thread,
-                CustomThreadmarkFilters = options.ThreadmarkFilters,
-                CustomTaskFilters = options.TaskFilters,
-                PartitionMode = options.PartitionMode
+                PartitionMode = options.PartitionMode,
+                WhitespaceAndPunctuationIsSignificant = options.Whitespace,
+                CaseIsSignificant = options.Case,
+                ForcePlanReferencesToBeLabeled = options.MustLabelPlanReferences,
+                ForbidVoteLabelPlanNames = options.ForbidPlanLabels,
+                DisableProxyVotes = options.NoUserProxy,
+                ForcePinnedProxyVotes = options.ForcePinProxy,
+                IgnoreSpoilers = options.IgnoreSpoilers,
+                TrimExtendedText = options.Trim
             };
 
             if (options.StartPost.HasValue)
                 quest.StartPost = options.StartPost.Value;
             if (options.EndPost.HasValue)
                 quest.EndPost = options.EndPost.Value;
+
+            quest.CheckForLastThreadmark = !options.StartPost.HasValue && !options.EndPost.HasValue;
 
             if (!string.IsNullOrEmpty(options.ThreadmarkFilters))
             {
@@ -130,38 +179,10 @@ namespace NetTally.CLI
                 quest.CustomTaskFilters = options.TaskFilters;
             }
 
-            quest.CheckForLastThreadmark = !options.StartPost.HasValue && !options.EndPost.HasValue;
-
-            if (!string.IsNullOrEmpty(options.ThreadmarkFilters))
-                quest.UseCustomThreadmarkFilters = true;
-
-            if (!string.IsNullOrEmpty(options.TaskFilters))
-                quest.UseCustomTaskFilters = true;
-
-            quest.DisableProxyVotes = options.NoProxy;
-            quest.ForbidVoteLabelPlanNames = options.ForbidPlanLabels;
-            quest.ForcePinnedProxyVotes = options.PinProxy;
-            quest.IgnoreSpoilers = options.NoSpoilers;
-            quest.WhitespaceAndPunctuationIsSignificant = options.Whitespace;
-            quest.CaseIsSignificant = options.Case;
-            quest.TrimExtendedText = options.Trim;
-
             return quest;
         }
-
-        /// <summary>
-        /// Sets the global service options on the main view model.
-        /// </summary>
-        /// <param name="options">The commandline options set when run.</param>
-        private static void SetGlobalOptions(Options options)
-        {
-            ViewModelService.MainViewModel.Options.AllowRankedVotes = !options.NoRanks;
-
-            ViewModelService.MainViewModel.Options.DisplayMode = options.DisplayMode;
-
-            ViewModelService.MainViewModel.Options.DebugMode = options.Debug;
-        }
         #endregion
+
 
         #region Event watching
         /// <summary>
@@ -171,6 +192,8 @@ namespace NetTally.CLI
         /// <param name="e"></param>
         private static void MainViewModel_PropertyChanged(object sender, PropertyChangedEventArgs e)
         {
+            logger.LogTrace($"MainViewModel property changed event: {e.PropertyName}");
+
             if (e is PropertyDataChangedEventArgs<string> eData)
             {
                 if (ViewModelService.MainViewModel.TallyIsRunning && verbose)
@@ -186,8 +209,6 @@ namespace NetTally.CLI
 
                     if (verbose)
                         Console.Error.WriteLine("Tally completed!");
-
-                    waiting.Set();
                 }
             }
         }
