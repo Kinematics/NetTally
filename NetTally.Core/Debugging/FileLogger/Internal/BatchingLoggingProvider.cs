7c4f5cc1cae9f052987301929d7b3b88eceac16a
David Smith <dsmith@datasync.com>
2019-07-29 23:29:15 -0500

    Implement a FileLoggerProvider that provides batch logging on a background thread. Initialize the file logger during startup services. Provide a means of enabling more detailed logging when debug mode is on.

---
+++ NetTally.Core/Debugging/FileLogger/Internal/BatchingLoggingProvider.cs
@@ -0,0 +1,185 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in https://github.com/aspnet/Logging for license information.
+// https://github.com/aspnet/Logging/blob/2d2f31968229eddb57b6ba3d34696ef366a6c71b/src/Microsoft.Extensions.Logging.AzureAppServices/Internal/BatchingLoggerProvider.cs
+
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace NetTally.Debugging.FileLogger.Internal
+{
+    public abstract class BatchingLoggerProvider : ILoggerProvider, ISupportExternalScope
+    {
+        private readonly List<LogMessage> _currentBatch = new List<LogMessage>();
+        private readonly TimeSpan _interval;
+        private readonly int? _queueSize;
+        private readonly int? _batchSize;
+        private readonly IDisposable _optionsChangeToken;
+
+        private BlockingCollection<LogMessage> _messageQueue;
+        private Task? _outputTask;
+        private CancellationTokenSource? _cancellationTokenSource;
+
+        private bool _includeScopes;
+        private IExternalScopeProvider? _scopeProvider;
+
+        internal IExternalScopeProvider? ScopeProvider => _includeScopes ? _scopeProvider : null;
+
+        protected BatchingLoggerProvider(IOptionsMonitor<BatchingLoggerOptions> options)
+        {
+            // NOTE: Only IsEnabled and IncludeScopes are monitored
+
+            var loggerOptions = options.CurrentValue;
+            if (loggerOptions.BatchSize <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(loggerOptions.BatchSize), $"{nameof(loggerOptions.BatchSize)} must be a positive number.");
+            }
+            if (loggerOptions.FlushPeriod <= TimeSpan.Zero)
+            {
+                throw new ArgumentOutOfRangeException(nameof(loggerOptions.FlushPeriod), $"{nameof(loggerOptions.FlushPeriod)} must be longer than zero.");
+            }
+
+            _interval = loggerOptions.FlushPeriod;
+            _batchSize = loggerOptions.BatchSize;
+            _queueSize = loggerOptions.BackgroundQueueSize;
+
+            _messageQueue = new BlockingCollection<LogMessage>(new ConcurrentQueue<LogMessage>());
+
+            _optionsChangeToken = options.OnChange(UpdateOptions);
+            UpdateOptions(options.CurrentValue);
+        }
+
+        public bool IsEnabled { get; private set; }
+
+        private void UpdateOptions(BatchingLoggerOptions options)
+        {
+            var oldIsEnabled = IsEnabled;
+            IsEnabled = options.IsEnabled;
+            _includeScopes = options.IncludeScopes;
+
+            if (oldIsEnabled != IsEnabled)
+            {
+                if (IsEnabled)
+                {
+                    Start();
+                }
+                else
+                {
+                    Stop();
+                }
+            }
+
+        }
+
+        protected abstract Task WriteMessagesAsync(IEnumerable<LogMessage> messages, CancellationToken token);
+
+        private async Task ProcessLogQueue()
+        {
+            if (_cancellationTokenSource != null)
+            {
+                while (!_cancellationTokenSource.IsCancellationRequested)
+                {
+                    var limit = _batchSize ?? int.MaxValue;
+
+                    while (limit > 0 && _messageQueue.TryTake(out var message))
+                    {
+                        _currentBatch.Add(message);
+                        limit--;
+                    }
+
+                    if (_currentBatch.Count > 0)
+                    {
+                        try
+                        {
+                            await WriteMessagesAsync(_currentBatch, _cancellationTokenSource.Token);
+                        }
+                        catch
+                        {
+                            // ignored
+                        }
+
+                        _currentBatch.Clear();
+                    }
+
+                    await IntervalAsync(_interval, _cancellationTokenSource.Token);
+                }
+            }
+        }
+
+        protected virtual Task IntervalAsync(TimeSpan interval, CancellationToken cancellationToken)
+        {
+            return Task.Delay(interval, cancellationToken);
+        }
+
+        internal void AddMessage(DateTimeOffset timestamp, string message)
+        {
+            if (!_messageQueue.IsAddingCompleted && _cancellationTokenSource != null)
+            {
+                try
+                {
+                    _messageQueue.Add(new LogMessage { Message = message, Timestamp = timestamp }, _cancellationTokenSource.Token);
+                }
+                catch
+                {
+                    //cancellation token canceled or CompleteAdding called
+                }
+            }
+        }
+
+        private void Start()
+        {
+            _messageQueue = _queueSize == null ?
+                new BlockingCollection<LogMessage>(new ConcurrentQueue<LogMessage>()) :
+                new BlockingCollection<LogMessage>(new ConcurrentQueue<LogMessage>(), _queueSize.Value);
+
+            _cancellationTokenSource = new CancellationTokenSource();
+            _outputTask = Task.Run(ProcessLogQueue);
+        }
+
+        private void Stop()
+        {
+            if (_cancellationTokenSource != null)
+            {
+                _cancellationTokenSource.Cancel();
+                _cancellationTokenSource.Dispose();
+            }
+            _messageQueue.CompleteAdding();
+
+            try
+            {
+                _outputTask?.Wait(_interval);
+            }
+            catch (TaskCanceledException)
+            {
+            }
+            catch (AggregateException ex) when (ex.InnerExceptions.Count == 1 && ex.InnerExceptions[0] is TaskCanceledException)
+            {
+            }
+        }
+
+        public void Dispose()
+        {
+            _optionsChangeToken?.Dispose();
+            if (IsEnabled)
+            {
+                Stop();
+                _messageQueue.Dispose();
+                _cancellationTokenSource?.Dispose();
+            }
+        }
+
+        public ILogger CreateLogger(string categoryName)
+        {
+            return new BatchingLogger(this, categoryName);
+        }
+
+        void ISupportExternalScope.SetScopeProvider(IExternalScopeProvider scopeProvider)
+        {
+            _scopeProvider = scopeProvider;
+        }
+    }
+}
