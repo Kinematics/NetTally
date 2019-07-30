1e6ebef42a3d969b3c0b52d8eaf9c2c9d0fd39ce
David Smith <dsmith@datasync.com>
2019-07-29 23:29:15 -0500

    Implement a FileLoggerProvider that provides batch logging on a background thread. Initialize the file logger during startup services. Provide a means of enabling more detailed logging when debug mode is on.

---
+++ NetTally.Core/Debugging/FileLogger/PeriodicityOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace NetTally.Debugging.FileLogger
+{
+    public enum PeriodicityOptions
+    {
+        Daily,
+        Hourly,
+        Minutely,
+        Monthly
+    }
+}
--- NetTally.Core/Global/Startup.cs
+++ NetTally.Core/Global/Startup.cs
@@ -14,6 +14,8 @@ using NetTally.VoteCounting;
 using NetTally.VoteCounting.RankVotes;
 using NetTally.Votes;
 using NetTally.Web;
+using NetTally.Debugging.FileLogger;
+using System.IO;
 
 namespace NetTally
 {
@@ -22,7 +24,16 @@ namespace NetTally
         public static void ConfigureServices(IServiceCollection services, LogLevel defaultLoggingLevel = LogLevel.Debug)
         {
             // Logging system.
-            services.AddLogging(builder => builder.AddDebug());
+            services.AddLogging(builder => 
+                builder
+                .AddDebug()
+                .AddFile(options =>
+                        {
+                            options.LogDirectory = GetLoggingDirectoryPath();
+                            options.Periodicity = PeriodicityOptions.Daily;
+                            options.RetainedFileCountLimit = 7;
+                        })
+            );
             services.Configure<LoggerFilterOptions>(options => options.MinLevel = defaultLoggingLevel);
 
             services.AddSingleton<IGlobalOptions>(AdvancedOptions.Instance);
@@ -47,6 +58,47 @@ namespace NetTally
             services.AddSingleton<VoteConstructor>();
             services.AddSingleton<RankVoteCounterFactory>();
             services.AddTransient<ITextResultsProvider, TallyOutput>();
+        }
+
+        /// <summary>
+        /// Get a logging directory to initialize the FileLogger's options with.
+        /// </summary>
+        /// <returns>Returns a path to a directory to store log files in.</returns>
+        private static string GetLoggingDirectoryPath()
+        {
+            try
+            {
+                string path = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData);
+
+                if (Directory.Exists(path))
+                {
+                    path = Path.Combine(path, ProductInfo.Name);
+                    Directory.CreateDirectory(path);
+
+                    return path;
+                }
+            }
+            catch (Exception)
+            {
+            }
+
+            // If we don't have access to the AppData path, just fall back to a Logs subdirectory.
+            return "Logs";
+        }
+
+        public static LogLevel FileLogLevel = LogLevel.Information;
+
+        /// <summary>
+        /// Supplementary check for whether logging is enabled for the specified log level.
+        /// </summary>
+        /// <param name="logLevel">The log level of the message being logged.</param>
+        /// <returns>Returns true if logging the specified logLevel is valid. False if it shouldn't be logged.</returns>
+        public static bool IsFileLoggingEnabled(LogLevel logLevel)
+        {
+            if (AdvancedOptions.Instance.DebugMode)
+                return true;
+
+            return logLevel >= FileLogLevel;
         }
     }
 }
