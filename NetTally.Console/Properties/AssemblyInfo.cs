b916d7302a04281738c4297f10d9b39d385775e3
David Smith <dsmith@datasync.com>
2019-10-13 18:07:01 -0500

    Update version number.

--- NetTally.Console/Properties/AssemblyInfo.cs
+++ NetTally.Console/Properties/AssemblyInfo.cs
@@ -23,8 +23,8 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.0.0.0")]
-[assembly: AssemblyInformationalVersion("3.0.0")]
+[assembly: AssemblyVersion("3.0.3.0")]
+[assembly: AssemblyFileVersion("3.0.3.0")]
+[assembly: AssemblyInformationalVersion("3.0.3")]
 [assembly: NeutralResourcesLanguage("en")]
 
