fbd854fe9d99a523e3a488db31493ff91d784c61
Miz Mahem <MizMahem@gmail.com>
2020-10-14 17:50:54 -0500

    Replaced specific Enum converters with a multipurpose enum converter.

---
+++ NetTally.Avalonia/Converters/EnumConverter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace NetTally.Avalonia.Converters
+{
+    /// <summary>
+    /// Data binding conversion class to convert a PartitionMode enum to
+    /// an index value or back.
+    /// </summary>
+    public class EnumConverter : IValueConverter
+    {
+        /// <summary>
+        /// Gets a static instance of this converter.
+        /// </summary>
+        public static IValueConverter Instance { get; private set; }
+
+        /// <summary>
+        /// Initializes static members of the <see cref="EnumConverter"/> class.
+        /// </summary>
+        static EnumConverter() => Instance = new EnumConverter();
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EnumConverter"/> class.
+        /// </summary>
+        /// <remarks>Private because this class should only be created by the static constructor.</remarks>
+        private EnumConverter() { }
+
+        /// <summary>
+        /// Convert from source enum to target index (int).
+        /// </summary>
+        /// <returns>Returns the control index of the given enum, or -1 if invalid.</returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
+            value.GetType().IsEnum ? System.Convert.ChangeType(value, Enum.GetUnderlyingType(value.GetType())) : -1;
+
+        /// <summary>
+        /// Convert from index (int) to source enum.
+        /// </summary>
+        /// <returns>Returns the enum option for that index, or the default value (0) if invalid.</returns>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => 
+            targetType.IsEnum ? Enum.ToObject(targetType, value) : Enum.ToObject(targetType, 0);
+    }
+}
