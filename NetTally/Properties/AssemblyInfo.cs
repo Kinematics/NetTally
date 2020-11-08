7c18716082c7a65ee682cadde1699de58167e4e4
David Smith <dsmith@datasync.com>
2020-11-07 18:23:32 -0600

    Update version numbers.

--- NetTally/Properties/AssemblyInfo.cs
+++ NetTally/Properties/AssemblyInfo.cs
@@ -51,8 +51,8 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.1.2.0")]
-[assembly: AssemblyFileVersion("3.1.2.0")]
-[assembly: AssemblyInformationalVersion("3.1.2")]
+[assembly: AssemblyVersion("3.1.3.0")]
+[assembly: AssemblyFileVersion("3.1.3.0")]
+[assembly: AssemblyInformationalVersion("3.1.3")]
 [assembly: NeutralResourcesLanguage("en")]
 
