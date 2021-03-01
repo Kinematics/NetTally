1487c55e7bccab20d1f8876523a9f2ded30d3341
David Smith <dsmith@datasync.com>
2021-02-28 18:13:54 -0600

    Add record for Author.

---
+++ NetTally.Core/Types/Components/Author.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetTally.Types.Components
+{
+    public record Author
+    {
+        public string Name { get; }
+
+        /// <summary>
+        /// Constructor for an author.
+        /// The name must have non-whitespace characters.
+        /// </summary>
+        /// <param name="name">The name of the author.</param>
+        /// <exception cref="ArgumentException">Throws if name is null, empty, or only whitespace.</exception>
+        public Author(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException($"Invalid author name: '{name}'");
+
+            // Trim any surrounding whitespace, if it exists.
+            Name = name.Trim();
+        }
+    }
+}
