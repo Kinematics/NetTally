8b6fae0f954b8ebe0a4880037af1a00f7dd31406
David Smith <dsmith@datasync.com>
2022-12-19 14:31:39 -0600

    Add an observable class for global settings.

---
+++ NetTally.Core/Global/GlobalSettings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using CommunityToolkit.Mvvm.ComponentModel;
+using NetTally.Types.Enums;
+
+namespace NetTally.Global
+{
+    public partial class GlobalSettings : ObservableObject
+    {
+        [ObservableProperty]
+        DisplayMode displayMode = DisplayMode.Normal;
+        [ObservableProperty]
+        bool displayPlansWithNoVotes = false;
+        [ObservableProperty]
+        bool globalSpoilers = false;
+        [ObservableProperty]
+        RankVoteCounterMethod rankVoteCounterMethod = RankVoteCounterMethod.Default;
+        [ObservableProperty]
+        BoolEx allowUsersToUpdatePlans = BoolEx.Unknown;
+
+        [ObservableProperty]
+        bool disableWebProxy = false;
+        [ObservableProperty]
+        bool debugMode = false;
+        [ObservableProperty]
+        bool trackPostAuthorsUniquely = false;
+    }
+}
