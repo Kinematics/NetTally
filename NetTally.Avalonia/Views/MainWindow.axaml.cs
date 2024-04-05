d3b8bb4f86a9b3d9fb6a2e99ecaa639c87ce9a7d
David Smith <dsmith@datasync.com>
2024-04-05 13:58:32 -0500

    Log as debug instead of info.

--- NetTally.Avalonia/Views/MainWindow.axaml.cs
+++ NetTally.Avalonia/Views/MainWindow.axaml.cs
@@ -64,7 +64,7 @@ namespace NetTally.Avalonia.Views
         /// <param name="e">The <see cref="PropertyChangedEventArgs"/> instance containing the event data.</param>
         private async void MainViewModel_PropertyChanged(object? sender, PropertyChangedEventArgs e)
         {
-            logger.LogInformation("Received notification of property change from MainViewModel: {PropertyName}.", e.PropertyName);
+            logger.LogDebug("Received notification of property change from MainViewModel: {PropertyName}.", e.PropertyName);
 
             // If a new quest was added, load the QuestOptions dialog to
             // allow setting the URL and display name.
