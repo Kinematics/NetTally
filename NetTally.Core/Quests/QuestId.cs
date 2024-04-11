57d54d9e6c4315fde463ee3c6d6af6772c9311bc
David Smith <dsmith@datasync.com>
2024-04-10 23:18:35 -0500

    Add a record type to handle QuestId as a strong data type.

---
+++ NetTally.Core/Quests/QuestId.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace NetTally.Quests;
+public readonly record struct QuestId(Guid Id)
+{
+    public static readonly QuestId Empty = new(Guid.Empty);
+    public static QuestId NewQuestId() => new(Guid.NewGuid());
+}
