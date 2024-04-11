f7e51069a8774b2186b95d54bd152d9a444be5a1
David Smith <dsmith@datasync.com>
2024-04-11 02:03:19 -0500

    Mark the class with the JSON converter attribute.

--- NetTally.Core/Quests/QuestId.cs
+++ NetTally.Core/Quests/QuestId.cs
@@ -3,6 +3,8 @@ using System.Text.Json.Serialization;
 using System.Text.Json;
 
 namespace NetTally.Quests;
+
+[JsonConverter(typeof(QuestIdJsonConverter))]
 public readonly record struct QuestId(Guid Id)
 {
     public static readonly QuestId Empty = new(Guid.Empty);
@@ -23,10 +25,7 @@ public class QuestIdJsonConverter : JsonConverter<QuestId>
 
     public override void Write(Utf8JsonWriter writer, QuestId value, JsonSerializerOptions options)
     {
-        if (value == QuestId.Empty)
-            writer.WriteNullValue();
-        else
-            JsonSerializer.Serialize(writer, value.Id, options);
+        writer.WriteStringValue(value.Id);
     }
 }
 
