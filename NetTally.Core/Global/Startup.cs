6388a18da10a823808a3a2ff6e8ad0fcfce1f915
David Smith <dsmith@datasync.com>
2024-04-11 21:23:13 -0500

    Remove old views and view model.

--- NetTally.Core/Global/Startup.cs
+++ NetTally.Core/Global/Startup.cs
@@ -23,7 +23,6 @@ namespace NetTally
             services.AddSingleton<IGeneralInputOptions>(AdvancedOptions.Instance);
             services.AddSingleton<IGeneralOutputOptions>(AdvancedOptions.Instance);
 
-            services.AddSingleton<ViewModel>();
             services.AddSingleton<ICache<string>, PageCache>();
             services.AddSingleton<IClock, SystemClock>();
             services.AddSingleton<IHash, NormalHash>();
