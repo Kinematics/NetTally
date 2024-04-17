22689e8d685794fdf845adeb242d025ad7628582
David Smith <dsmith@datasync.com>
2024-04-17 12:01:14 -0500

    Add a class for public const strings.

---
+++ NetTally.Core/Data/StringData.cs
@@ -0,0 +1,7 @@
+﻿namespace NetTally.Data;
+internal class StringData
+{
+    public const string OmakeFilter = @"\bomake\b";
+    public const string NewThreadEntry = "https://www.example.com/threads/fake-thread.00000";
+    public const string NewThreadDisplayName = "~Placeholder~";
+}
