76e7e979bd8bb02bc4a7bc335a0465cda12288e8
David Smith <dsmith@datasync.com>
2024-04-18 00:45:21 -0500

    Add global usings for type aliases.

---
+++ NetTally.Core/Data/GlobalUsings.cs
@@ -0,0 +1,8 @@
+﻿global using VotesByVoter = 
+    System.Collections.Generic.Dictionary<
+        NetTally.Tally.Components.Origin,
+        System.Collections.Generic.List<
+            NetTally.Tally.Components.VoteLineBlock>>;
+global using VoteStorageEntry = System.Collections.Generic.KeyValuePair<
+    NetTally.Tally.Components.VoteLineBlock,
+    NetTally.Votes.VoterStorage>;
