0dd05193e835a001a73bfb2eade728c3537b8af6
David Smith <dsmith@datasync.com>
2024-04-17 21:39:29 -0500

    Adjust namespace.

--- NetTally.Core/Global/CheckForNewRelease.cs
+++ NetTally.Core/Global/CheckForNewRelease.cs
@@ -11,7 +11,7 @@ using NetTally.SystemInfo;
 using NetTally.Types.Enums;
 using NetTally.Web;
 
-namespace NetTally
+namespace NetTally.Global
 {
     public partial class CheckForNewRelease : ObservableObject, IDisposable
     {
