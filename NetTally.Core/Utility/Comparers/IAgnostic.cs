e8d5ebce2d4b1e71fdb1dc3479d93d5efc73a0f2
David Smith <dsmith@datasync.com>
2024-04-19 18:03:06 -0500

    Delete unused interface.

--- NetTally.Core/Utility/Comparers/IAgnostic.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.ComponentModel;
-
-namespace NetTally.Utility.Comparers
-{
-    public interface IAgnostic
-    {
-        void ComparisonPropertyChanged(Quest quest, PropertyChangedEventArgs e);
-    }
-}
