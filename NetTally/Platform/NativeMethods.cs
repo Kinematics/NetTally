8afd873816a48851a1a971d6b2d80d83cd90aeea
David Smith <dsmith@datasync.com>
2024-04-23 13:22:14 -0500

    Convert from DllIMport to LibraryImport.

--- NetTally/Platform/NativeMethods.cs
+++ NetTally/Platform/NativeMethods.cs
@@ -3,14 +3,16 @@ using System.Runtime.InteropServices;
 
 namespace NetTally
 {
-    internal class NativeMethods
+    internal partial class NativeMethods
     {
         public const int HWND_BROADCAST = 0xffff;
         public static readonly int WM_NETTALLYUPDATE = RegisterWindowMessage("WM_NETTALLYUPDATE");
 
-        [DllImport("user32")]
-        public static extern bool PostMessage(IntPtr hwnd, int msg, IntPtr wparam, IntPtr lparam);
-        [DllImport("user32")]
-        public static extern int RegisterWindowMessage(string message);
+        [LibraryImport("user32")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static partial bool PostMessage(IntPtr hwnd, int msg, IntPtr wparam, IntPtr lparam);
+
+        [LibraryImport("user32", StringMarshalling = StringMarshalling.Utf16)]
+        public static partial int RegisterWindowMessage(string message);
     }
 }
