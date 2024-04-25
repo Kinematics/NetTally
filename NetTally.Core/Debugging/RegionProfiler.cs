679205ac99667ec5d9367ee68ed61f809a048919
David Smith <dsmith@datasync.com>
2024-04-25 00:24:31 -0500

    Tidy

--- NetTally.Core/Debugging/RegionProfiler.cs
+++ NetTally.Core/Debugging/RegionProfiler.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using NetTally.Data;
 using NetTally.Systems;
 
 namespace NetTally
