57892e683fa57d7ce08b6dccd88d18762aaa9703
David Smith <dsmith@datasync.com>
2024-05-02 15:24:01 -0500

    Add comparer for Post type. Extract vote lines on Post construction.

--- NetTally.Core/Tally/ComponentsF/Posts/Post.cs
+++ NetTally.Core/Tally/ComponentsF/Posts/Post.cs
@@ -1,13 +1,39 @@
-﻿namespace NetTally.Tally.ComponentsF.Posts;
-public record PostType(OriginType Origin, string Text);
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using NetTally.Tally.ComponentsF.Votes;
+using NetTally.Utility.Comparers;
+
+namespace NetTally.Tally.ComponentsF.Posts;
+public record PostType(OriginType Origin, string Text, List<VoteLineType> VoteLines)
+{
+    public bool HasVote => VoteLines.Count > 0;
+}
 
 public static class Post
 {
     public static PostType? Create(OriginType origin, string text)
     {
-        if (string.IsNullOrEmpty(text))
-            return null;
+        if (origin == null) return null;
+        if (string.IsNullOrEmpty(text)) return null;
 
-        return new PostType(origin, text);
+        var voteLines = VoteParser.ExtractVoteLines(text);
+
+        return new PostType(origin, text, voteLines);
+    }
+}
+
+public class PostComparer : IEqualityComparer<PostType>
+{
+    public bool Equals(PostType? x, PostType? y)
+    {
+        if (x == null || y == null) return false;
+
+        return OriginComparer.AreEqual(x.Origin, y.Origin) &&
+            Agnostic.InsensitiveComparer.Equals(x.Text, y.Text);
+    }
+
+    public int GetHashCode([DisallowNull] PostType obj)
+    {
+        return OriginComparer.GetHashCodeFor(obj.Origin);
     }
 }
