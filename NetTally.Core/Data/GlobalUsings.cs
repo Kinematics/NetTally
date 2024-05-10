ab3bb09e7bbfee5d73f40f3a8d2ed8bf0d0cecc2
David Smith <dsmith@datasync.com>
2024-05-09 23:24:20 -0500

    Add global aliases for functional types.

--- NetTally.Core/Data/GlobalUsings.cs
+++ NetTally.Core/Data/GlobalUsings.cs
@@ -2,8 +2,7 @@
 global using VotesByVoter = 
     System.Collections.Generic.Dictionary<
         NetTally.Tally.Components.Origin,
-        System.Collections.Generic.List<
-            NetTally.Tally.Components.VoteLineBlock>>;
+        System.Collections.Generic.List<NetTally.Tally.Components.VoteLineBlock>>;
 
 // Individual dictionary element from VoteStorage:
 global using VoteStorageEntry = System.Collections.Generic.KeyValuePair<
@@ -18,13 +17,41 @@ global using VoterStorageEntry = System.Collections.Generic.KeyValuePair<
 // List of VoterStorage elements:
 global using OrderedVoterStorage = System.Collections.Generic.List<
     System.Collections.Generic.KeyValuePair<
-        NetTally.Tally.Components.Origin, NetTally.Tally.Components.VoteLineBlock>>;
-
-// Grouping of VoteStorage elements by task:
-global using VotesGroupedByTask = System.Linq.IGrouping<
-    string, System.Collections.Generic.KeyValuePair<
-        NetTally.Tally.Components.VoteLineBlock, NetTally.Votes.VoterStorage>>;
+        NetTally.Tally.Components.Origin,
+        NetTally.Tally.Components.VoteLineBlock>>;
 
 global using FilteredVoterStorage = System.Collections.Generic.IEnumerable<
     System.Collections.Generic.KeyValuePair<
-        NetTally.Tally.Components.Origin, NetTally.Tally.Components.VoteLineBlock>>;
+        NetTally.Tally.Components.Origin,
+        NetTally.Tally.Components.VoteLineBlock>>;
+
+// Grouping of VoteStorage elements by task:
+global using VotesGroupedByTask = System.Linq.IGrouping<
+    string,
+    System.Collections.Generic.KeyValuePair<
+        NetTally.Tally.Components.VoteLineBlock,
+        NetTally.Votes.VoterStorage>>;
+
+
+
+// Individual dictionary element from VoteStorageF:
+global using VoteStorageEntryF = System.Collections.Generic.KeyValuePair<
+    NetTally.Tally.ComponentsF.Votes.VoteBlockType,
+    NetTally.Tally.ComponentsF.Storage.VoterStorage>;
+
+// Individual dictionary element from VoterStorageF:
+global using VoterStorageEntryF = System.Collections.Generic.KeyValuePair<
+    NetTally.Tally.ComponentsF.Posts.OriginType,
+    NetTally.Tally.ComponentsF.Votes.VoteBlockType>;
+
+// List of VoterStorage elements (ordered):
+global using OrderedVoterStorageF = System.Collections.Generic.List<
+    System.Collections.Generic.KeyValuePair<
+        NetTally.Tally.ComponentsF.Posts.OriginType,
+        NetTally.Tally.ComponentsF.Votes.VoteBlockType>>;
+
+// List of VoterStorage elements:
+global using FilteredVoterStorageF = System.Collections.Generic.IEnumerable<
+    System.Collections.Generic.KeyValuePair<
+        NetTally.Tally.ComponentsF.Posts.OriginType,
+        NetTally.Tally.ComponentsF.Votes.VoteBlockType>>;
