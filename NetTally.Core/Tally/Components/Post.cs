b11ce307017d278c8b937d92fe1ab059ded18f18
David Smith <dsmith@datasync.com>
2024-05-10 20:55:06 -0500

    Don't throw an exception when receiving empty text for a post. Some post filtering ends up discarding all content.

--- NetTally.Core/Tally/Components/Post.cs
+++ NetTally.Core/Tally/Components/Post.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
-using Microsoft.Extensions.Hosting;
 using NetTally.Enums;
 using NetTally.Utility;
 using NetTally.Votes;
@@ -24,10 +23,9 @@ namespace NetTally.Tally.Components
         public Post(Origin origin, string text)
         {
             ArgumentNullException.ThrowIfNull(origin);
-            ArgumentException.ThrowIfNullOrWhiteSpace(text);
 
             Origin = origin;
-            Text = text;
+            Text = text ?? "";
             VoteLines = GetPostAnalysisResults(Text);
         }
 
