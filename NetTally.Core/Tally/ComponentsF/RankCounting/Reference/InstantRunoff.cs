b105e6243eb4b8b15462902687618bbacf19bfc6
David Smith <dsmith@datasync.com>
2024-05-17 18:46:20 -0500

    Add copies of rank counting classes for functional types.

---
+++ NetTally.Core/Tally/ComponentsF/RankCounting/Reference/InstantRunoff.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NetTally.Extensions;
+using NetTally.Tally.Components;
+
+namespace NetTally.VoteCounting.RankVotes.Reference
+{
+    /// <summary>
+    /// Implement ranking votes using the standard instant runoff method.
+    /// Each round, the least liked of the top-ranked choices is removed.
+    /// </summary>
+    public class InstantRunoff : InstantRunoffBase
+    {
+        /// <summary>
+        /// Gets the least preferred choice.
+        /// In the standard Instant Runoff, this is the vote with the fewest
+        /// number of top-ranked votes.
+        /// </summary>
+        /// <param name="localRankings">The vote rankings.</param>
+        /// <returns>Returns the vote string for the least preferred vote.</returns>
+        protected override VoteLineBlock GetLeastPreferredChoice(
+            VotesByVoter voterPreferences)
+        {
+            var highestRankings = voterPreferences.GroupBy(v => v.Value.First());
+
+            var leastPreferred = highestRankings.MinBy(r => r.Count());
+
+            if (leastPreferred == null)
+            {
+                return VoteLineBlock.Empty;
+            }
+
+            return leastPreferred.Key;
+        }
+    }
+}
