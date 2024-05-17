32ae95b64332e6a336d129c5015de8bc77f1f614
David Smith <dsmith@datasync.com>
2024-05-17 00:33:56 -0500

    Copy interface for functional types.

---
+++ NetTally.Core/Tally/ComponentsF/RankCounting/IRankVoteCounter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using NetTally.Tally.ComponentsF.Storage;
+
+namespace NetTally.Tally.ComponentsF.RankCounting;
+
+/// <summary>
+/// Vote counter interface for ranked votes.
+/// </summary>
+public interface IRankVoteCounter
+{
+    // TODO: is rank needed in the tuple, considering a List<> is explicitly ordered?
+    List<((int rank, double rankScore) ranking, VoteStorageEntryF vote)>
+        CountVotesForTask(VoteStorage taskVotes);
+}
