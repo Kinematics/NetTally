31114aa08cb96836d2abdc8721c7ad525888ef6f
David Smith <dsmith@datasync.com>
2024-05-17 23:58:15 -0500

    Add an explicit inverting bool converter for simplicity.

---
+++ NetTally/Converters/InvertBoolConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace NetTally.Converters;
+
+[ValueConversion(typeof(bool), typeof(bool))]
+public class InvertBoolConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (targetType != typeof(bool))
+            throw new InvalidOperationException("The target must be a boolean");
+
+        if (value is bool b)
+            return !b;
+
+        throw new ArgumentException("Value is not a bool.");
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotSupportedException();
+    }
+}
