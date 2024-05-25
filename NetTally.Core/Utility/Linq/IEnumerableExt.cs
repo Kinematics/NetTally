4c4d039bd2c9d0fe81b91e72f6930789898b3fbd
David Smith <dsmith@datasync.com>
2024-05-25 15:10:21 -0500

    Rename TakeAction extension to ForEach, same as the List<> function.

--- NetTally.Core/Utility/Linq/IEnumerableExt.cs
+++ NetTally.Core/Utility/Linq/IEnumerableExt.cs
@@ -453,7 +453,7 @@ namespace NetTally.Extensions
             List<T> unprocessed = [.. values.Where(v => !tryProcess(v))];
 
             if (unprocessed.Count == count)
-                unprocessed.TakeAction(v => unprocessedAction(v));
+                unprocessed.ForEach(v => unprocessedAction(v));
             else
                 unprocessed.TryProcess(tryProcess, unprocessedAction);
         }
@@ -464,7 +464,7 @@ namespace NetTally.Extensions
         /// <typeparam name="T">The type elements being enumerated.</typeparam>
         /// <param name="values">The elements being enumerated.</param>
         /// <param name="action">The action to apply to each element.</param>
-        public static void TakeAction<T>(this IEnumerable<T> values, Action<T> action)
+        public static void ForEach<T>(this IEnumerable<T> values, Action<T> action)
         {
             foreach (var value in values)
                 action(value);
