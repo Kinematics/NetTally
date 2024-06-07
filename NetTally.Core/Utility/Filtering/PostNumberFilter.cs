ee32d2edf59b68d9d1cfa9b6aa744186f7810ee0
David Smith <dsmith@datasync.com>
2024-06-07 13:36:56 -0500

    Refactor creation logic. Break passed-in value as comma-separated values in order to make sure each regex test doesn't get partial values.

--- NetTally.Core/Utility/Filtering/PostNumberFilter.cs
+++ NetTally.Core/Utility/Filtering/PostNumberFilter.cs
@@ -1,12 +1,17 @@
 ﻿using System.Text.RegularExpressions;
 
 namespace NetTally.Utility.Filtering;
+/// <summary>
+/// Factory class to create a filter for testing post numbers.
+/// </summary>
 public static partial class PostNumberFilter
 {
     private static readonly Regex postFilterRegex = PostFilterRegex();
+    private static readonly char[] separator = [','];
+    private static readonly Range FailRange = new(0, 0);
 
-    [GeneratedRegex(@"(?<range>(?<r1>\d+)\s*-\s*(?<r2>\d+))|(?<num>\d+)",
-        RegexOptions.None, 50)]
+    [GeneratedRegex(@"^((?<range>(?<r1>\d+)\s*-\s*(?<r2>\d+))|(?<num>\d+))$",
+        RegexOptions.ExplicitCapture, 50)]
     private static partial Regex PostFilterRegex();
 
     public static IAdaptingFilter<Range, long> Create(string value)
@@ -21,42 +26,45 @@ public static partial class PostNumberFilter
         bool invert = value[0] == '!';
         if (invert)
         {
-            value = value[1..].TrimStart();
+            value = value[1..];
 
-            if (string.IsNullOrEmpty(value))
+            if (string.IsNullOrWhiteSpace(value))
             {
                 return AdaptingListFilter<Range, long>.AlwaysBlock;
             }
         }
 
-        List<Range> ranges = [];
-
-        MatchCollection ms = postFilterRegex.Matches(value);
-
-        foreach (Match mm in ms)
-        {
-            if (mm.Groups["range"].Success)
-            {
-                if (int.TryParse(mm.Groups["r1"].Value, out int startRange) &&
-                    int.TryParse(mm.Groups["r2"].Value, out int endRange))
-                {
-                    Range range = new(startRange, endRange);
-                    ranges.Add(range);
-                }
-            }
-            else if (mm.Groups["num"].Success)
-            {
-                if (int.TryParse(mm.Groups["num"].Value, out int num))
-                {
-                    Range range = new(num, num);
-                    ranges.Add(range);
-                }
-            }
-        }
+        var ranges = value
+            .Split(separator, StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
+            .Select(s => postFilterRegex.Match(s))
+            .Where(m => m.Success)
+            .Select(ConvertToRange)
+            .Where(r => r.Start.Value != FailRange.Start.Value);
 
         return invert
             ? AdaptingListFilter<Range, long>.Whitelist(ranges, IsValueInRange)
             : AdaptingListFilter<Range, long>.Blacklist(ranges, IsValueInRange);
+    }
+
+    private static Range ConvertToRange(Match m)
+    {
+        if (m.Groups["range"].Success)
+        {
+            if (int.TryParse(m.Groups["r1"].Value, out int startRange) &&
+                int.TryParse(m.Groups["r2"].Value, out int endRange))
+            {
+                return new(startRange, endRange);
+            }
+        }
+        else if (m.Groups["num"].Success)
+        {
+            if (int.TryParse(m.Groups["num"].Value, out int num))
+            {
+                return new(num, num);
+            }
+        }
+
+        return FailRange;
     }
 
     private static bool IsValueInRange(Range range, long item)
