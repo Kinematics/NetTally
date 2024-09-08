7435f8c70d53233203094ad176a7278a3987250f
David Smith <dsmith@datasync.com>
2024-09-08 11:05:38 -0500

    Increment.

--- NetTally/App.xaml.cs
+++ NetTally/App.xaml.cs
@@ -1,6 +1,6 @@
 ﻿/*
     NetTally
-    Copyright(C) 2015  David Smith <dsmith@datasync.com>
+    Copyright(C) 2015  David Smith <dsmith@datasync.com> 
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
