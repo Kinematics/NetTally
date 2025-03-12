dab613c6647677b1ed80f5b2aa62c0c858e3e856
David Smith <dsmith@datasync.com>
2025-03-11 19:49:12 -0500

    Add rewrite of ForumReader class.

---
+++ NetTally.Core/Input/Forums/Reading/ForumReader2.cs
@@ -0,0 +1,201 @@
+﻿using HtmlAgilityPack;
+using Microsoft.Extensions.Logging;
+using NetTally.Configure;
+using NetTally.CustomEventArgs;
+using NetTally.Enums;
+using NetTally.Extensions;
+using NetTally.Input.Forums.ForumAdapters;
+using NetTally.Tally.Components.Posts;
+using NetTally.Tally.Components.Threads;
+using NetTally.Web;
+
+namespace NetTally.Input.Forums.Reading;
+
+public class ForumReader2(
+    ILogger<ForumReader2> logger,
+    ForumAdapterFactory forumAdapterFactory,
+    IQuestsInfo questsInfo,
+    IPageProvider pageProvider) : IForumReader
+{
+    #region Event passing
+    private void PageProvider_StatusChanged(object? sender, MessageEventArgs e)
+    {
+        if (!string.IsNullOrEmpty(e.Message))
+        {
+            StatusChanged?.Invoke(sender, e);
+        }
+    }
+
+    private void SendNotificationMessage(string message)
+    {
+        var e = new MessageEventArgs(message);
+        StatusChanged?.Invoke(this, e);
+    }
+
+    /// <summary>
+    /// Event handler hook for status messages.
+    /// </summary>
+    public event EventHandler<MessageEventArgs>? StatusChanged;
+    #endregion
+
+    public async Task<(IEnumerable<string>, IEnumerable<Post>)> ReadQuestAsync(
+        Quest quest,
+        CancellationToken token)
+    {
+        logger.LogDebug("Reading Quest {quest}", quest.ThreadName);
+
+        var questPosts = GetQuestSources(quest)
+            .SelectAsync(q => GetPostsFromQuest(q, token), token)
+            .ConfigureAwait(false);
+
+        List<string> titles = [];
+        List<Post> posts = [];
+
+        await foreach (var (Title, Posts) in questPosts)
+        {
+            titles.Add(Title);
+            posts.AddRange(Posts);
+        }
+
+        return (titles, posts);
+    }
+
+    /// <summary>
+    /// Gets a list of all quest sources associated with the specified quest.
+    /// </summary>
+    /// <param name="quest">The quest with potential alternate sources.</param>
+    /// <returns>All quest sources to be tallied.</returns>
+    public IEnumerable<Quest> GetQuestSources(Quest quest)
+    {
+        return [quest, .. questsInfo.GetLinkedQuests(quest)];
+    }
+
+    private async Task<(string Title, List<Post> Posts)> GetPostsFromQuest(
+        Quest quest,
+        CancellationToken token)
+    {
+        try
+        {
+            pageProvider.StatusChanged += PageProvider_StatusChanged;
+
+            IForumAdapter adapter = await quest.GetForumAdapter(forumAdapterFactory, token)
+               .ConfigureAwait(ConfigureAwaitOptions.None);
+
+            var threadInfo = await adapter.GetThreadInfoAsync(quest, pageProvider, token)
+                .ConfigureAwait(ConfigureAwaitOptions.None);
+
+            logger.LogDebug("Thread information acquired for {questDisplayName}.\n({threadData})",
+                quest.DisplayName, threadInfo);
+
+            var pages = await GetPagesFromQuest(quest, adapter, threadInfo, token);
+
+            var posts = pages.SelectMany((p, i) => GetPostsFromPage(quest, p, i, adapter, threadInfo))
+                .ToList();
+
+            string title = CraftTitle(threadInfo, posts);
+
+            return (title, posts);
+        }
+        finally
+        {
+            pageProvider.StatusChanged -= PageProvider_StatusChanged;
+        }
+    }
+
+    private async Task<IEnumerable<HtmlDocument?>> GetPagesFromQuest(
+        Quest quest,
+        IForumAdapter adapter,
+        ThreadInfo threadInfo,
+        CancellationToken token)
+    {
+        var pages = GetPagesToLoad(quest, adapter, threadInfo);
+
+        var pageLoads = pages.Select(p =>
+            pageProvider.GetHtmlDocumentAsync(
+                p.url,
+                $"Page {p.pageNum}",
+                p.cacheMode,
+                SuppressNotifications.No,
+                token));
+
+        var finished = await Task.WhenAll(pageLoads)
+            .ConfigureAwait(ConfigureAwaitOptions.None);
+
+        logger.LogDebug("Got {Count} pages loading {questDisplayName}.", finished.Length, quest.DisplayName);
+
+        return finished;
+    }
+
+    private static IEnumerable<(string url, int pageNum, CachingMode cacheMode)> GetPagesToLoad(
+        Quest quest,
+        IForumAdapter adapter,
+        ThreadInfo threadInfo)
+    {
+        int firstPage = threadInfo.ThreadRange.GetStartPage();
+        int lastPage = threadInfo.ThreadRange.GetEndPage();
+        int pageCount = lastPage - firstPage + 1;
+
+        if (pageCount < 1)
+            return [];
+
+        var urls = Enumerable.Range(firstPage, pageCount)
+            .Select(pageNum => (adapter.GetUrlForPage(quest, pageNum),
+                    pageNum,
+                    pageNum == lastPage ? CachingMode.NoCache : CachingMode.ReadWrite));
+
+        return urls;
+    }
+
+
+    private static IEnumerable<Post> GetPostsFromPage(
+        Quest quest,
+        HtmlDocument? page,
+        int index,
+        IForumAdapter adapter,
+        ThreadInfo threadInfo)
+    {
+        if (page is null)
+            return [];
+
+        var posts = adapter.GetPosts(page, quest, threadInfo.ThreadRange.GetStartPage() + index)
+            .Where(p => KeepPost(p, quest, threadInfo))
+            .DistinctBy(p => p.Origin) // remove sticky posts
+            .OrderBy(p => p.Origin.PostNumber.Value);
+
+        return posts;
+    }
+
+    private static bool KeepPost(
+        Post post,
+        Quest quest,
+        ThreadInfo threadInfo)
+    {
+        if (!post.HasVote)
+            return false;
+
+        if (post.IsBeforeStart(threadInfo.ThreadRange) || post.IsAfterEnd(threadInfo.ThreadRange))
+            return false;
+
+        if (post.Origin.Author == threadInfo.Author)
+            return false;
+
+        if (post.MatchesUsernameFilter(quest))
+            return false;
+
+        if (post.MatchesPostNumberFilter(quest))
+            return false;
+
+        return true;
+    }
+
+    private static string CraftTitle(ThreadInfo threadInfo, List<Post> posts)
+    {
+        long min = posts.Min(p => p.Origin.PostNumber.Value);
+        long max = posts.Max(p => p.Origin.PostNumber.Value);
+
+        string title = $"{threadInfo.Title} [{(posts.Count > 0 ? $"Posts: {min}-{max}" : "No Votes")}]";
+
+        return title;
+    }
+
+}
