96c3b62fe9fe59239bb3d38db0c9fc2fd2ab06f3
David Smith <dsmith@datasync.com>
2025-03-21 11:01:30 -0500

    Move predicate into filtering section where it is used.

--- NetTally.Core/Utility/Filtering/AdaptingListFilter.cs
+++ NetTally.Core/Utility/Filtering/AdaptingListFilter.cs
@@ -1,6 +1,4 @@
-﻿using NetTally.Utility.Delegates;
-
-namespace NetTally.Utility.Filtering;
+﻿namespace NetTally.Utility.Filtering;
 
 /// <summary>
 /// An item filter that determines whether an object is allowed by
