8c850ab66bf11a1673816c55389fd93ecca9267e
David Smith <dsmith@datasync.com>
2025-08-14 23:52:34 -0500

    Convert files to file-scoped namespace.

--- NetTally.Avalonia/App.axaml.cs
+++ NetTally.Avalonia/App.axaml.cs
@@ -8,92 +8,91 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using NetTally.Avalonia.Views;
 
-namespace NetTally.Avalonia
+namespace NetTally.Avalonia;
+
+public class App : Application
 {
-    public class App : Application
+    private readonly ILogger<App> logger;
+
+    public App()
     {
-        private readonly ILogger<App> logger;
+        AppX.Initialize(SetupUIServices);
 
-        public App()
-        {
-            AppX.Initialize(SetupUIServices);
+        var loggerFactory = AppX.Services.GetRequiredService<ILoggerFactory>();
+        logger = loggerFactory.CreateLogger<App>();
 
-            var loggerFactory = AppX.Services.GetRequiredService<ILoggerFactory>();
-            logger = loggerFactory.CreateLogger<App>();
+        // Create handlers for unhandled exceptions
+        AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
 
-            // Create handlers for unhandled exceptions
-            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
-
-            logger.LogDebug("Application constructor completed.");
-        }
-
-        #region Hosting/DI setup
-        /// <summary>
-        /// Register UI views and navigation service.
-        /// </summary>
-        /// <param name="services">The DI service collection being built at startup.</param>
-        private void SetupUIServices(IServiceCollection services)
-        {
-            // Add NavigationService for the application.
-            services.AddSingleton<Navigation.AvaloniaNavigationService>();
-
-            // Register all the windows that the applications can display.
-            services.AddTransient<MainWindow>();
-            services.AddTransient<GlobalOptions>();
-            services.AddTransient<QuestOptions>();
-            services.AddTransient<ManageVotes>();
-            services.AddTransient<ReorderTasks>();
-        }
-        #endregion Hosting/DI setup
-
-        #region Avalonia
-        /// <summary>
-        /// Initializes the Application Framework, loading resources and the like from the XAML.
-        /// </summary>
-        public override void Initialize()
-        {
-            AvaloniaXamlLoader.Load(this);
-        }
-
-        public override void OnFrameworkInitializationCompleted()
-        {
-            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
-            {
-                // Line below is needed to remove Avalonia data validation.
-                // Without this line you will get duplicate validations from both Avalonia and CT
-                BindingPlugins.DataValidators.RemoveAt(0);
-
-                // Add event handler for when the program exits.
-                desktop.Exit += Desktop_Exit;
-
-                Views.MainWindow mainWindow = AppX.Services.GetRequiredService<Views.MainWindow>();
-
-                desktop.MainWindow = mainWindow;
-                desktop.ShutdownMode = ShutdownMode.OnMainWindowClose;
-            }
-
-            base.OnFrameworkInitializationCompleted();
-        }
-
-        private void Desktop_Exit(object? sender, ControlledApplicationLifetimeExitEventArgs e)
-        {
-            // Save settings on exit.
-            AppX.SaveConfiguration();
-
-            logger.LogDebug("Application exit.");
-        }
-        #endregion Avalonia
-
-        #region Error Handling
-        /// <summary>
-        /// Special handlers if an exception isn't handled by the program.
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
-        {
-            logger.LogCritical((Exception)e.ExceptionObject, "Unhandled exception");
-        }
-        #endregion Error Handling
+        logger.LogDebug("Application constructor completed.");
     }
+
+    #region Hosting/DI setup
+    /// <summary>
+    /// Register UI views and navigation service.
+    /// </summary>
+    /// <param name="services">The DI service collection being built at startup.</param>
+    private void SetupUIServices(IServiceCollection services)
+    {
+        // Add NavigationService for the application.
+        services.AddSingleton<Navigation.AvaloniaNavigationService>();
+
+        // Register all the windows that the applications can display.
+        services.AddTransient<MainWindow>();
+        services.AddTransient<GlobalOptions>();
+        services.AddTransient<QuestOptions>();
+        services.AddTransient<ManageVotes>();
+        services.AddTransient<ReorderTasks>();
+    }
+    #endregion Hosting/DI setup
+
+    #region Avalonia
+    /// <summary>
+    /// Initializes the Application Framework, loading resources and the like from the XAML.
+    /// </summary>
+    public override void Initialize()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+
+    public override void OnFrameworkInitializationCompleted()
+    {
+        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        {
+            // Line below is needed to remove Avalonia data validation.
+            // Without this line you will get duplicate validations from both Avalonia and CT
+            BindingPlugins.DataValidators.RemoveAt(0);
+
+            // Add event handler for when the program exits.
+            desktop.Exit += Desktop_Exit;
+
+            Views.MainWindow mainWindow = AppX.Services.GetRequiredService<Views.MainWindow>();
+
+            desktop.MainWindow = mainWindow;
+            desktop.ShutdownMode = ShutdownMode.OnMainWindowClose;
+        }
+
+        base.OnFrameworkInitializationCompleted();
+    }
+
+    private void Desktop_Exit(object? sender, ControlledApplicationLifetimeExitEventArgs e)
+    {
+        // Save settings on exit.
+        AppX.SaveConfiguration();
+
+        logger.LogDebug("Application exit.");
+    }
+    #endregion Avalonia
+
+    #region Error Handling
+    /// <summary>
+    /// Special handlers if an exception isn't handled by the program.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+    {
+        logger.LogCritical((Exception)e.ExceptionObject, "Unhandled exception");
+    }
+    #endregion Error Handling
 }
--- NetTally.Avalonia/Navigation/NavigationService.cs
+++ NetTally.Avalonia/Navigation/NavigationService.cs
@@ -5,71 +5,70 @@ using Avalonia.Controls;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace NetTally.Avalonia.Navigation
+namespace NetTally.Avalonia.Navigation;
+
+/// <summary>
+/// An IoC service to allow creating and showing windows via the standard
+/// service provider.
+/// </summary>
+public class AvaloniaNavigationService(IServiceProvider serviceProvider, ILogger<AvaloniaNavigationService> logger)
 {
+    private readonly IServiceProvider serviceProvider = serviceProvider;
+    private readonly ILogger<AvaloniaNavigationService> logger = logger;
+
     /// <summary>
-    /// An IoC service to allow creating and showing windows via the standard
-    /// service provider.
+    /// Show a non-modal window.
     /// </summary>
-    public class AvaloniaNavigationService(IServiceProvider serviceProvider, ILogger<AvaloniaNavigationService> logger)
+    /// <typeparam name="T">The type of window being requested.</typeparam>
+    public void Show<T>() where T : Window
     {
-        private readonly IServiceProvider serviceProvider = serviceProvider;
-        private readonly ILogger<AvaloniaNavigationService> logger = logger;
+        logger.LogDebug("Showing Window {type}", typeof(T));
 
-        /// <summary>
-        /// Show a non-modal window.
-        /// </summary>
-        /// <typeparam name="T">The type of window being requested.</typeparam>
-        public void Show<T>() where T : Window
+        try
         {
-            logger.LogDebug("Showing Window {type}", typeof(T));
-
-            try
-            {
-                T window = serviceProvider.GetRequiredService<T>();
-                
-                window.Show();
-            }
-            catch (Exception e)
-            {
-                logger.LogError(e, "Failed to create window");
-            }
+            T window = serviceProvider.GetRequiredService<T>();
+            
+            window.Show();
         }
-
-        /// <summary>
-        /// Show a modal window.
-        /// </summary>
-        /// <remarks>
-        /// Not sure if this should get rewritten to possibly catch any exceptions, or if they should bubble up.
-        /// </remarks>
-        /// <typeparam name="T">The type of window being requested.</typeparam>
-        /// <param name="parentWindow">The parent Window for this dialog.</param>
-        /// <param name="parameters">List of paramaters to pass along to the dialog window.</param>
-        /// <returns>Returns the dialog result.</returns>
-        public async Task<bool?> ShowDialogAsync<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] T>
-            (Window parentWindow, params object[] parameters)
-            where T : Window
+        catch (Exception e)
         {
-            logger.LogDebug("Showing Dialog Window {type}", typeof(T));
-
-            parameters ??= [];
-
-            // Get the service provider to resolve our dependencies and call our window. If we got passed a
-            // parameter that isn't (or at least, shouldn't be) a dependency we can resolve, then we pass
-            // that along as well.
-
-            // ActivatorUtilities seems to (sometimes) call the default constructor if we call it with no
-            // extra parameters instead of resolving our dependencies. I'm not sure if this is a bug or
-            // intended behavior, but we avoid this with this.
-            // Note: Most likely this is due to a service that was never properly registered. Check that.
-
-            //T window = (parameters.Length > 0)
-            //    ? ActivatorUtilities.CreateInstance<T>(serviceProvider, parameters)
-            //    : serviceProvider.GetRequiredService<T>();
-
-            T window = ActivatorUtilities.CreateInstance<T>(serviceProvider, parameters);
-            var result = await window.ShowDialog<bool?>(parentWindow);
-            return result;
+            logger.LogError(e, "Failed to create window");
         }
+    }
+
+    /// <summary>
+    /// Show a modal window.
+    /// </summary>
+    /// <remarks>
+    /// Not sure if this should get rewritten to possibly catch any exceptions, or if they should bubble up.
+    /// </remarks>
+    /// <typeparam name="T">The type of window being requested.</typeparam>
+    /// <param name="parentWindow">The parent Window for this dialog.</param>
+    /// <param name="parameters">List of paramaters to pass along to the dialog window.</param>
+    /// <returns>Returns the dialog result.</returns>
+    public async Task<bool?> ShowDialogAsync<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] T>
+        (Window parentWindow, params object[] parameters)
+        where T : Window
+    {
+        logger.LogDebug("Showing Dialog Window {type}", typeof(T));
+
+        parameters ??= [];
+
+        // Get the service provider to resolve our dependencies and call our window. If we got passed a
+        // parameter that isn't (or at least, shouldn't be) a dependency we can resolve, then we pass
+        // that along as well.
+
+        // ActivatorUtilities seems to (sometimes) call the default constructor if we call it with no
+        // extra parameters instead of resolving our dependencies. I'm not sure if this is a bug or
+        // intended behavior, but we avoid this with this.
+        // Note: Most likely this is due to a service that was never properly registered. Check that.
+
+        //T window = (parameters.Length > 0)
+        //    ? ActivatorUtilities.CreateInstance<T>(serviceProvider, parameters)
+        //    : serviceProvider.GetRequiredService<T>();
+
+        T window = ActivatorUtilities.CreateInstance<T>(serviceProvider, parameters);
+        var result = await window.ShowDialog<bool?>(parentWindow);
+        return result;
     }
 }
