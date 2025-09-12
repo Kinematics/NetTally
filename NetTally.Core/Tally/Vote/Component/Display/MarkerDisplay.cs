388909dab6bb0ab5cf13aea4f3ac8dbd2c3a1351
David Smith <dsmith@datasync.com>
2025-09-11 22:08:00 -0500

    Split Marker utility classes into separate files.

---
+++ NetTally.Core/Tally/Vote/Component/Display/MarkerDisplay.cs
@@ -0,0 +1,21 @@
+﻿using NetTally.Utility;
+
+namespace NetTally.Tally.Vote.Component;
+public static class MarkerDisplay
+{
+    extension(Marker marker)
+    {
+        /// <summary>
+        /// Get a string value to use for display purposes for a marker.
+        /// </summary>
+        /// <param name="marker">The marker to get a value for.</param>
+        /// <returns>Returns a string value based on the marker's derived class and state.</returns>
+        public string Display() => marker.Map(
+            voteMarker => "X",
+            rankMarker => $"#{rankMarker.Rank}",
+            scoreMarker => $"{scoreMarker.Score}%",
+            approvalMarker => approvalMarker.Approve ? "+" : "-",
+            planMarker => Strings.PlanNameMarker,
+            noMarker => "");
+    }
+}
--- NetTally.Core/Tally/Vote/Component/Utility/MarkerUtility.cs
+++ NetTally.Core/Tally/Vote/Component/Utility/MarkerUtility.cs
@@ -1,5 +1,4 @@
 ﻿using NetTally.Enums;
-using NetTally.Utility;
 
 namespace NetTally.Tally.Vote.Component;
 
@@ -11,19 +10,6 @@ public static partial class MarkerUtility
 {
     extension(Marker marker)
     {
-        /// <summary>
-        /// Get a string value to use for display purposes for a marker.
-        /// </summary>
-        /// <param name="marker">The marker to get a value for.</param>
-        /// <returns>Returns a string value based on the marker's derived class and state.</returns>
-        public string Display() => marker.Map(
-            voteMarker => "X",
-            rankMarker => $"#{rankMarker.Rank}",
-            scoreMarker => $"{scoreMarker.Score}%",
-            approvalMarker => approvalMarker.Approve ? "+" : "-",
-            planMarker => Strings.PlanNameMarker,
-            noMarker => "");
-
         /// <summary>
         /// Get a string value to use for display purposes for a marker.
         /// </summary>
@@ -63,49 +49,5 @@ public static partial class MarkerUtility
             approvalMarker => approvalMarker.Approve,
             planMarker => null,
             noMarker => null);
-    }
-}
-
-/// <summary>
-/// Extension class containing mapping function for subclasses of <see cref="Marker"/> objects.
-/// </summary>
-public static class MarkerMapping
-{
-    extension(Marker marker)
-    {
-        /// <summary>
-        /// Map function that defines how to implement a function that can apply to different
-        /// subclasses of <see cref="Marker"/>.
-        /// </summary>
-        /// <typeparam name="T">The function return type.</typeparam>
-        /// <param name="origin">The <see cref="Origin"/> that this extension method applies to.</param>
-        /// <param name="voteMap">What to do if the <see cref="Marker"/> is a <see cref="VoteMarker"/></param>
-        /// <param name="rankMap">What to do if the <see cref="Marker"/> is a <see cref="RankMarker"/></param>
-        /// <param name="scoreMap">What to do if the <see cref="Marker"/> is a <see cref="ScoreMarker"/></param>
-        /// <param name="approvalMap">What to do if the <see cref="Marker"/> is a <see cref="ApprovalMarker"/></param>
-        /// <param name="planMap">What to do if the <see cref="Marker"/> is a <see cref="PlanMarker"/></param>
-        /// <param name="emptyMap">What to do if the <see cref="Marker"/> is a <see cref="NoMarker"/></param>
-        /// <returns>The result of whichever function got applied.</returns>
-        /// <exception cref="InvalidOperationException">Will trigger if another subclass is
-        /// ever created, but this function hasn't been updated.</exception>
-        public T Map<T>(
-            Func<VoteMarker, T> voteMap,
-            Func<RankMarker, T> rankMap,
-            Func<ScoreMarker, T> scoreMap,
-            Func<ApprovalMarker, T> approvalMap,
-            Func<PlanMarker, T> planMap,
-            Func<NoMarker, T> emptyMap)
-        {
-            return marker switch
-            {
-                VoteMarker voteMarker => voteMap(voteMarker),
-                RankMarker rankMaker => rankMap(rankMaker),
-                ScoreMarker scoreMarker => scoreMap(scoreMarker),
-                ApprovalMarker approvalMarker => approvalMap(approvalMarker),
-                PlanMarker planMarker => planMap(planMarker),
-                NoMarker noMarker => emptyMap(noMarker),
-                _ => throw new InvalidOperationException("Unknown Marker type.")
-            };
-        }
     }
 }
