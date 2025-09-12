a5fbd6c079001d5f51df83dba6b330721c1ba4eb
David Smith <dsmith@datasync.com>
2025-09-11 22:04:47 -0500

    Rename file and class.

--- NetTally.Core/Tally/Vote/Component/Utility/VoteContentUtility.cs
+++ NetTally.Core/Tally/Vote/Component/Utility/VoteContentUtility.cs
@@ -5,7 +5,7 @@ namespace NetTally.Tally.Vote.Component;
 /// <summary>
 /// Extension class for handling trimming of vote content.
 /// </summary>
-public static partial class VoteContentTrimming
+public static partial class VoteContentUtility
 {
     extension(VoteContent content)
     {
