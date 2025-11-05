dbd7bbd0e57cfd20daf1167b0ff8b6e56610e31c
Kinematics <dsmith@datasync.com>
2025-11-05 13:51:22 -0600

    Tidy. Comments.

--- NetTally.Core/Models/Posts/Origin.cs
+++ NetTally.Core/Models/Posts/Origin.cs
@@ -1,10 +1,27 @@
 ﻿namespace NetTally.Models;
 
+/// <summary>
+/// The identification information of a post or plan.
+/// </summary>
 public abstract record Origin();
 
+/// <summary>
+/// A type indicating that there is no origin.
+/// </summary>
 public sealed record NoOrigin() : Origin;
 
+/// <summary>
+/// The identification of a post.
+/// </summary>
+/// <param name="UserName">The use who made the post.</param>
+/// <param name="Source">The location of the post.</param>
 public sealed record UserOrigin(Author UserName, Source Source) : Origin;
 
+/// <summary>
+/// The identification of a plan.
+/// </summary>
+/// <param name="PlanName">The name of the plan.</param>
+/// <param name="Author">The user who wrote the plan.</param>
+/// <param name="Source">The location of the post the plan was posted in.</param>
 public sealed record PlanOrigin(Author PlanName, Author Author, Source Source) : Origin;
 
--- NetTally.Core/Models/Votes/Marker.cs
+++ NetTally.Core/Models/Votes/Marker.cs
@@ -1,15 +1,39 @@
 ﻿namespace NetTally.Models;
 
+/// <summary>
+/// A marker on a vote line that's used to indicate how a person voted.
+/// </summary>
 public abstract record Marker();
 
+/// <summary>
+/// A standard vote marker, to indicate a person is voting for this option.
+/// </summary>
 public sealed record VoteMarker() : Marker;
 
+/// <summary>
+/// A vote marker indicating either approval or disapproval for the vote option.
+/// </summary>
+/// <param name="Approve">Whether the voter approves of the vote line or doesn't.</param>
 public sealed record ApprovalMarker(bool Approve) : Marker;
 
+/// <summary>
+/// A vote marker indicating the score a voter gave to the vote option.
+/// </summary>
+/// <param name="Score">A score between 0 (worst) and 100 (best), indicating percent approval.</param>
 public sealed record ScoreMarker(int Score) : Marker;
 
+/// <summary>
+/// A vote marker indicating the rank a voter gave to this vote option.
+/// </summary>
+/// <param name="Rank">A rank between 1 (best) and 99 (worst) for the voter's preference for this option.</param>
 public sealed record RankMarker(int Rank) : Marker;
 
+/// <summary>
+/// A type representing the lack of a vote marker.
+/// </summary>
 public sealed record NoMarker() : Marker;
 
+/// <summary>
+/// A type indicating that this vote line is part of a defined plan, rather than a user vote.
+/// </summary>
 public sealed record PlanMarker() : Marker;
